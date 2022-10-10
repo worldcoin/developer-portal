7f2012eb9582469892bc8ce274e4e559885b0ebc
Igor Osipov <igor.osipov@ottofeller.com>
2022-10-10 11:44:12 +0300

    chore: hide scaling params at web stack

--- infrastructure/src/hasura/parameters/index.ts
+++ infrastructure/src/hasura/parameters/index.ts
@@ -4,17 +4,9 @@ import { production } from './production'
 import { staging } from './staging'
 import { ContextEnv } from 'common/types'
 
-type UndefinedKeys<T> = {
-  [P in keyof T]: undefined extends T[P] ? P : never;
-}[keyof T]
-
-// @ANCHOR Makes the key optional if the value of this key has undefined type in the declaration
-type OptionalUndefined<T> = Partial<Pick<T, UndefinedKeys<T>>> &
-  Omit<T, UndefinedKeys<T>>
-
-type Envs = OptionalUndefined<ReturnType<typeof staging>['envs']> &
-  OptionalUndefined<ReturnType<typeof dev>['envs']> &
-  OptionalUndefined<ReturnType<typeof production>['envs']>
+type Envs = Partial<ReturnType<typeof staging>['envs']> &
+  Partial<ReturnType<typeof dev>['envs']> &
+  Partial<ReturnType<typeof production>['envs']>
 
 export const parameters = (params: {
   environment: ContextEnv
