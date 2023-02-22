eb91568febcb3d9895c28572b707901ec04157f2
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-02-21 19:11:14 -0800

    fix: app table defaults

---
+++ hasura/migrations/default/1677035369959_app_defaults/down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "public"."app" ALTER COLUMN "description" drop default;
+ALTER TABLE "public"."app" ALTER COLUMN "user_interfaces" drop default;
+ALTER TABLE "public"."app" ALTER COLUMN "logo_url" drop default;
