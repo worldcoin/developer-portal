2ec7192b299adc6864c72f7e524789d78fc51905
Max Petretta <git@maxpetretta.com>
2023-02-23 10:34:43 -0800

    Address comments, rename 'type' to 'credential_type' everywhere

---
+++ hasura/migrations/default/1677035369960_create_table_public_revocation/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."revocation" ("id" varchar(50) NOT NULL DEFAULT gen_random_friendly_id('rvk'), "identity_commitment" text NOT NULL, "credential_type" text NOT NULL, "revoked_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , UNIQUE ("id"), UNIQUE ("identity_commitment"));
--- hasura/migrations/default/1677035369960_create_table_public_revoke/up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE "public"."revocation" ("id" varchar(50) NOT NULL DEFAULT gen_random_friendly_id('rvk'), "identity_commitment" text NOT NULL, "type" text NOT NULL, "revoked_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , UNIQUE ("id"), UNIQUE ("identity_commitment"));
