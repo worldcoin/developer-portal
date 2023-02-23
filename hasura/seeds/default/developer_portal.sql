2e24a804ee01ee2fa7b0ecf922f5c724b47af903
Igor Osipov <igor.osipov@ottofeller.com>
2023-02-23 14:59:23 +0100

    remove creating app from seeds

--- hasura/seeds/default/developer_portal.sql
+++ hasura/seeds/default/developer_portal.sql
@@ -1,3 +1,2 @@
 SET check_function_bodies = false;
 INSERT INTO public.team (id, name) VALUES ('team_a2bdc68383167c9a0a9f416352fde6c8', 'Worldcoin');
-INSERT INTO public.app (id, created_at, updated_at, team_id, name, logo_url, verified_at, description, is_staging, engine, status, user_interfaces, is_archived) VALUES ('app_developer_portal', '2023-02-21 11:46:10.844546+00', '2023-02-21 11:46:10.844546+00', 'team_a2bdc68383167c9a0a9f416352fde6c8', 'Developer Portal App', '''''::text', NULL, 'Default app for the signing in with World ID', true, 'cloud', 'active', '{}', false);
