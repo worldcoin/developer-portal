b603da1c8793d592c368fe81d7383425e28487a8
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-02-27 19:06:20 -0800

    normal mode

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
   images: {
     domains: ["world-id-public.s3.amazonaws.com"],
   },
