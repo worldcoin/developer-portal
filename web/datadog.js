11ebd1aad0fdc502c63ce292881d75e5395a8491
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-02-27 17:36:44 -0800

    fix: start fresh (#134)

--- web/datadog.js
+++
@@ -1,9 +0,0 @@
-const ddTrace = require("dd-trace");
-
-/** @type {import('dd-trace').Tracer} */
-const tracer = ddTrace.init({});
-tracer.use("http", {
-  blocklist: ["/api/health"],
-});
-
-module.exports = tracer;
