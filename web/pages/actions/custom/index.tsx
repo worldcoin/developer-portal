b77d1bfc718a00f7e5aff264d6a351ddbcb4c8a7
Max Petretta <git@maxpetretta.com>
2023-02-28 12:08:47 -0800

    Delete index.tsx

--- web/pages/actions/custom/index.tsx
+++
@@ -1,9 +0,0 @@
-import { GetServerSideProps } from "next";
-import { Actions } from "scenes/actions/actions";
-export default Actions;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {},
-  };
-};
