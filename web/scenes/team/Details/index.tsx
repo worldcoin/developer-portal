da2aa2781b469b6edbdb4d9b07e1a088a6721ce6
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-03-02 02:52:57 +0400

    feat: team details (#144)

--- web/scenes/team/Details/index.tsx
+++ web/scenes/team/Details/index.tsx
@@ -1,5 +1,15 @@
+import { FieldGroup } from "common/FieldGroup";
+import { FieldInput } from "common/FieldInput";
 import { memo } from "react";
 
 export const Details = memo(function Details() {
-  return <div>Details</div>;
+  return (
+    <div className="space-y-12">
+      <h2 className="font-semibold font-sora">My Team Details</h2>
+
+      <FieldGroup label="Team name">
+        <FieldInput value="Worldcoin" />
+      </FieldGroup>
+    </div>
+  );
 });
