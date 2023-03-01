40c968797cc856518018e19bd178de0c36ddc9ce
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-03-01 15:50:08 +0400

    feat: team header

--- web/scenes/team/Header/index.tsx
+++ web/scenes/team/Header/index.tsx
@@ -1,5 +1,18 @@
+import { Illustration } from "common/Auth/Illustration";
 import { memo } from "react";
 
 export const Header = memo(function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex items-center gap-6">
+      <Illustration icon="team" />
+
+      <div className="space-y-1">
+        <h1 className="text-20 font-sora font-semibold">My Team</h1>
+
+        <span className="text-neutral-secondary text-14">
+          Update your team settings and invite others to collaborate.
+        </span>
+      </div>
+    </div>
+  );
 });
