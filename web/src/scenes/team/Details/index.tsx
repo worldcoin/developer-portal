790113f1474e4ab5c4cb789bfe2b910f9779dff2
Igor Osipov <igor.osipov@ottofeller.com>
2023-03-02 15:01:05 +0100

    fix: details input

--- web/src/scenes/team/Details/index.tsx
+++ web/src/scenes/team/Details/index.tsx
@@ -1,14 +1,16 @@
 import { FieldGroup } from "@/components/FieldGroup";
 import { FieldInput } from "@/components/FieldInput";
-import { memo } from "react";
+import { memo, useState } from "react";
 
 export const Details = memo(function Details() {
+  const [name, setName] = useState("Worldcoin");
+
   return (
     <div className="grid gap-y-12">
       <h2 className="font-semibold font-sora">My Team Details</h2>
 
       <FieldGroup label="Team name">
-        <FieldInput value="Worldcoin" />
+        <FieldInput value={name} onChange={(e) => setName(e.target.value)} />
       </FieldGroup>
     </div>
   );
