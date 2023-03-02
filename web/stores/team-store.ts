f065aef73823a1e7248730150ceee8e511d35042
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-03-02 13:53:54 +0400

    chore: move tmp data

--- web/stores/team-store.ts
+++ web/stores/team-store.ts
@@ -1,3 +1,4 @@
+import { tempTeam } from "common/Layout/temp-data";
 import { Dispatch, SetStateAction } from "react";
 import { create } from "zustand";
 
@@ -28,43 +29,6 @@ export enum RemoveMembersState {
   SUCCESS,
   ERROR,
 }
-
-const tempTeam = {
-  name: "Team 1",
-
-  members: [
-    {
-      image: "",
-      name: "Steven Wilks",
-      email: "steven.w34s@email.com",
-      verified: true,
-    },
-    {
-      image: "",
-      name: "Christina Goldsmith",
-      email: "christina.goldsmith@email.com",
-      verified: true,
-    },
-    {
-      image: "",
-      name: "Bessie Cooper",
-      email: "bess.cooper@email.com",
-      verified: false,
-    },
-    {
-      image: "",
-      name: "Esther Howard",
-      email: "esther.h@email.com",
-      verified: true,
-    },
-    {
-      image: "",
-      name: "Darlene Robertson",
-      email: "darlene.robertson@email.com",
-      verified: false,
-    },
-  ],
-};
 
 type TeamStore = {
   team: Team | null;
