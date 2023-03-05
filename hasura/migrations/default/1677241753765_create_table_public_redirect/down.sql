43ddea2dddc08a6ace6e232220f735ac9dc43e12
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-03-05 17:46:23 -0600

    wip

--- hasura/migrations/default/1677241753765_create_table_public_redirect/down.sql
+++ hasura/migrations/default/1677241753765_create_table_public_redirect/down.sql
@@ -1,1 +1,4 @@
 DROP TABLE "public"."redirect";
+DROP TRIGGER IF EXISTS "set_public_redirect_updated_at";
+DROP TRIGGER IF EXISTS "validate_redirect_uri_on_redirect";
+DROP FUNCTION IF EXISTS "validate_url";
--- hasura/migrations/default/1677241753765_create_table_public_redirect/up.sql
+++ hasura/migrations/default/1677241753765_create_table_public_redirect/up.sql
@@ -1,17 +1,43 @@
-CREATE TABLE "public"."redirect" ("id" varchar(50) NOT NULL DEFAULT gen_random_friendly_id('redirect'), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "action_id" varchar(50) NOT NULL, "uri" jsonb NOT NULL DEFAULT jsonb_build_array(), PRIMARY KEY ("id") , UNIQUE ("id"), UNIQUE ("action_id"));
-CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
-RETURNS TRIGGER AS $$
-DECLARE
-  _new record;
-BEGIN
-  _new := NEW;
-  _new."updated_at" = NOW();
-  RETURN _new;
-END;
-$$ LANGUAGE plpgsql;
+CREATE TABLE "public"."redirect" (
+  "id" varchar(50) NOT NULL DEFAULT gen_random_friendly_id('uri'), 
+  "created_at" timestamptz NOT NULL DEFAULT now(), 
+  "updated_at" timestamptz NOT NULL DEFAULT now(), 
+  "action_id" varchar(50) NOT NULL, 
+  "redirect_uri" varchar(256) NOT NULL, 
+  PRIMARY KEY ("id"),
+  FOREIGN KEY ("action_id") REFERENCES "public"."action" ("id") ON UPDATE RESTRICT ON DELETE CASCADE,
+  UNIQUE ("id")
+);
+
 CREATE TRIGGER "set_public_redirect_updated_at"
 BEFORE UPDATE ON "public"."redirect"
 FOR EACH ROW
 EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
 COMMENT ON TRIGGER "set_public_redirect_updated_at" ON "public"."redirect" 
 IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
+
+CREATE OR REPLACE FUNCTION validate_url()
+RETURNS TRIGGER AS $$
+DECLARE
+  url text;
+BEGIN
+  url := NEW.redirect_uri;
+  IF url IS NOT NULL AND url != '' THEN
+    IF strpos(url, 'http://localhost') = -1 THEN
+      IF strpos(url, 'https://') = 0 THEN
+        RAISE EXCEPTION 'URL must use HTTPS protocol unless it is localhost.';
+      END IF;
+    END IF;
+    IF NOT (url ~* '^https?://([[:alnum:]_-]+\.)+[[:alnum:]_-]+(/[[:alnum:]_\-./?%&=]*)?$') THEN
+      RAISE EXCEPTION 'Invalid URL format.';
+    END IF;
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "validate_redirect_uri_on_redirect"
+BEFORE INSERT OR UPDATE ON "public"."redirect"
+FOR EACH ROW
+EXECUTE FUNCTION validate_url();
