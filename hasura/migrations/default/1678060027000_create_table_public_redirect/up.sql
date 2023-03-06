ca7a271e5870e50491601edce103f57769c6393f
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-03-05 20:29:49 -0600

    fix with tests

--- hasura/migrations/default/1678060027000_create_table_public_redirect/up.sql
+++ hasura/migrations/default/1678060027000_create_table_public_redirect/up.sql
@@ -25,17 +25,17 @@ BEGIN
   url := NEW.redirect_uri;
   IF url IS NOT NULL AND url != '' THEN
     
-    IF strpos(url, 'http://localhost') = 1 THEN
+    IF strpos(url, 'http://localhost') = 1 OR strpos(url, 'https://localhost') = 1 THEN
       IF NOT (url ~* '^https?://localhost(:[0-9]+)?(/[^\s?]*)(\\?[^\s]*)?$') THEN
-        RAISE EXCEPTION 'Invalid localhost URL format.';
+        RAISE EXCEPTION USING ERRCODE= '22000', MESSAGE= 'Invalid localhost URL format.';
       END IF;
     ELSE
+      IF NOT (url ~* '^https?://([[:alnum:]_-]+\.)+[[:alnum:]_-]+(/[[:alnum:]_\-./?%&=]*)?$') THEN
+        RAISE EXCEPTION USING ERRCODE= '22000', MESSAGE= 'Invalid URL format.';
+      END IF;
       -- if url is not localhost, it must use https protocol
       IF strpos(url, 'https://') = 0 THEN
-        RAISE EXCEPTION 'URL must use HTTPS protocol unless it is localhost.';
-      END IF;
-      IF NOT (url ~* '^https://([[:alnum:]_-]+\.)+[[:alnum:]_-]+(/[[:alnum:]_\-./?%&=]*)?$') THEN
-        RAISE EXCEPTION 'Invalid URL format.';
+        RAISE EXCEPTION USING ERRCODE= '22000', MESSAGE= 'URL must use HTTPS protocol unless it is localhost.';
       END IF;
     END IF;
    
