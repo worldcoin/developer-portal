396f77a1330adc41dd0c04d2a0db65de209ea3e4
igorosip0v <89008845+igorosip0v@users.noreply.github.com>
2023-03-07 23:14:38 +0100

    feat: security headers (#109)
    Co-authored-by: Paolo D'Amico <paolodamico@users.noreply.github.com>

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,39 @@
+// @ts-check
+
+/** @type {import('next-safe').nextSafe} */
+// @ts-ignore
+const nextSafe = require("next-safe");
+const isDev = process.env.NODE_ENV !== "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: nextSafe({
+          isDev,
+          contentSecurityPolicy: {
+            mergeDefaultDirectives: true,
+            "img-src": "world-id-public.s3.amazonaws.com",
+            "font-src": [
+              "https://fonts.googleapis.com",
+              "https://fonts.gstatic.com",
+            ],
+            "style-src": [
+              "https://fonts.googleapis.com",
+              "https://fonts.gstatic.com",
+            ],
+            "prefetch-src": [
+              "https://fonts.googleapis.com",
+              "https://fonts.googleapis.com",
+            ],
+          },
+        }),
+      },
+    ];
+  },
+
   reactStrictMode: true,
   images: {
     domains: ["world-id-public.s3.amazonaws.com"],
