114badf4775715a4f493803c98d5e88ebd494701
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-03-09 17:29:47 +0400

    chore: comment default auth link

--- web/src/scenes/sign-in-with-world-id/index.tsx
+++ web/src/scenes/sign-in-with-world-id/index.tsx
@@ -2,7 +2,6 @@ import { memo } from "react";
 import { AuthRequired } from "src/components/AuthRequired";
 import { Layout } from "src/components/Layout";
 import { Credentials } from "./Credentials";
-import { DefaultAuthorizationLink } from "./DefaultAuthorizationLink";
 import { Header } from "./Header";
 import { Redirects } from "./Redirects";
 import { Status } from "./Status";
@@ -15,7 +14,8 @@ export const SignIn = memo(function SignIn() {
         <Status />
         <Credentials />
         <Redirects />
-        <DefaultAuthorizationLink />
+        {/* NOTE: https://linear.app/worldcoin/issue/WID-370#comment-d47da43e  */}
+        {/* <DefaultAuthorizationLink /> */}
       </Layout>
     </AuthRequired>
   );
