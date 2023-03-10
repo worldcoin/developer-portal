24bddd7b8f16fbcf0c650c4e5e3106ce1360c3d6
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-03-10 16:45:19 -0500

    fix tests

--- web/tests/integration/test-utils.ts
+++ web/tests/integration/test-utils.ts
@@ -62,10 +62,14 @@ export const getAPIUserClient = async (): Promise<
   const authLink = setContext(async (_, { headers }) => ({
     headers: {
       ...headers,
-      authorization: `Bearer ${await generateUserJWT(
-        "usr_a78f59e547fa5bd3d76bc1a1817c6d89",
-        "team_d7cde14f17eda7e0ededba7ded6b4467" // cspell: disable-line
-      )}`,
+      authorization: `Bearer ${
+        (
+          await generateUserJWT(
+            "usr_a78f59e547fa5bd3d76bc1a1817c6d89",
+            "team_d7cde14f17eda7e0ededba7ded6b4467" // cspell: disable-line
+          )
+        ).token
+      }`,
     },
   }));
 
