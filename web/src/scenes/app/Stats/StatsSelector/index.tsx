0eb73f21f962c912922d36926b4930da7932268a
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-03-11 03:29:35 +0400

    feat: app stats (#174)
    Co-authored-by: Paolo D'Amico <paolodamico@users.noreply.github.com>

--- web/src/scenes/app/Stats/StatsSelector/index.tsx
+++ web/src/scenes/app/Stats/StatsSelector/index.tsx
@@ -1,14 +1,12 @@
-import { Dispatch, Fragment, memo, SetStateAction, useState } from "react";
+import { Fragment, memo } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { Icon } from "src/components/Icon";
-import cn from "classnames";
-
-export type Option = { label: string; value: string };
+import { TimeSpan } from "src/stores/appStatsStore";
 
 export const Selector = memo(function Selector(props: {
-  options: Array<Option>;
-  selected: Option;
-  setOption: Dispatch<SetStateAction<Option>>;
+  options: Readonly<Array<TimeSpan>>;
+  selected: TimeSpan;
+  setOption: (value: TimeSpan) => void;
 }) {
   return (
     <div className="flex">
@@ -40,7 +38,7 @@ export const Selector = memo(function Selector(props: {
                     {({ active }) => (
                       <button
                         onClick={() => props.setOption(option)}
-                        className="bg-ffffff py-3 px-4 w-full text-start min-w-[140px]"
+                        className="bg-ffffff py-3 px-4 w-full text-start min-w-[140px] rounded-md hover:bg-f9fafb"
                       >
                         {option.label}
                       </button>
