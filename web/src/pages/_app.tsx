dd73e4ae1552ae4885e34492aeb7dd071c60f6e1
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-03-13 20:42:17 +0400

    fix: add jsx, global to style tag also add ignore rule for eslint

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -99,7 +99,7 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
 
       <Component {...pageProps} />
 
-      <style>{`
+      <style jsx global>{`
         :root {
           --font-sora: ${sora.style.fontFamily};
           --font-rubik: ${rubik.style.fontFamily};
