6939a8e6730faa2864f1ec88b21ca764afbeb40a
Max Petretta <git@maxpetretta.com>
2023-03-23 05:40:49 -0400

    feat: login is behind a button (#211)

--- web/src/scenes/invite/invite.tsx
+++ web/src/scenes/invite/invite.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Auth } from "src/components/Auth";
 import { Typography } from "src/components/Auth/Typography";
+import { Button } from "src/components/Button";
 import { Icon } from "src/components/Icon";
-import { urls } from "src/lib/urls";
 import { IInvitePageProps } from "src/pages/invite";
 
 export function Invite({ loginUrl, nonce }: IInvitePageProps) {
@@ -18,15 +19,6 @@ export function Invite({ loginUrl, nonce }: IInvitePageProps) {
     }
   }, [router]);
 
-  useEffect(() => {
-    const invite_token = localStorage.getItem("invite_token");
-    if (invite_token) {
-      router.push(loginUrl ?? "");
-    } else {
-      router.push(urls.waitlist());
-    }
-  }, [loginUrl, router]);
-
   return (
     <Auth pageTitle="Invite" pageUrl="invite">
       <div className="flex flex-col items-center max-w-[544px] p-12">
@@ -34,22 +26,34 @@ export function Invite({ loginUrl, nonce }: IInvitePageProps) {
           <>
             <Icon name="spinner" className="h-8 w-8 animate-spin" noMask />
             <Typography className="mt-8" variant="title">
-              Please wait while we process your invite.
+              Hold on, validating your invite....
             </Typography>
           </>
         )}
-        {/* {!loading && (
+        {!loading && (
           <>
             <Typography className="mt-8" variant="title">
-              Welcome, please sign in to create your account.
+              Welcome, please continue with Worldcoin to create your account.
             </Typography>
-            <SignInWithWorldID
-              app_id={process.env.SIGN_IN_WITH_WORLD_ID_APP_ID as string}
-              nonce={nonce}
-              onSuccess={() => console.log("success")}
-            />
+            <div className="mt-4 text-neutral-secondary">
+              If you don&apos;t have a World ID yet, download the{" "}
+              <a
+                href="https://worldcoin.org/download"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                World App
+              </a>{" "}
+              first.
+            </div>
+            <Link href={loginUrl ?? ""}>
+              <Button variant="primary" className="py-3 px-8 mt-8">
+                <Icon name="wld-logo" className="h-6 w-6 mr-2" />
+                Sign In with Worldcoin
+              </Button>
+            </Link>
           </>
-        )} */}
+        )}
       </div>
     </Auth>
   );
