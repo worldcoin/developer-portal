ff58b2e0bc71827a96b9e80535b088e6587b6882
Max Petretta <git@maxpetretta.com>
2023-03-23 19:23:39 -0400

    fix: error thrown by link component (#221)

--- web/src/components/Link/index.tsx
+++ web/src/components/Link/index.tsx
@@ -1,11 +1,21 @@
-import { AnchorHTMLAttributes, Fragment, memo, ReactNode } from "react";
-import NextLink, { LinkProps } from "next/link";
 import cn from "classnames";
+import NextLink, { LinkProps } from "next/link";
+import {
+  AnchorHTMLAttributes,
+  forwardRef,
+  Fragment,
+  memo,
+  ReactNode,
+} from "react";
 
 // NOTE Use this component every time you need a link
 // if "external" prop passed component will return usual <a> element
 // if there is no "external" prop, component returns "next/link" but with div wrapper around children
 // this allows you to pass styles with "className" to next/link
+
+type RefProp = {
+  ref?: React.Ref<HTMLAnchorElement>;
+};
 
 export type LinkInterface = {
   children: ReactNode;
@@ -13,41 +23,45 @@ export type LinkInterface = {
 } & (
   | ({
       external?: never | false;
-    } & LinkProps)
-  | ({ external: true } & AnchorHTMLAttributes<HTMLAnchorElement>)
+    } & LinkProps &
+      RefProp)
+  | ({ external: true } & AnchorHTMLAttributes<HTMLAnchorElement> & RefProp)
 );
 
-export const Link = memo(function Link(props: LinkInterface) {
-  const { external, href, ...restProps } = props;
-  let parsedUrl;
+const Link = memo(
+  forwardRef<HTMLAnchorElement, LinkInterface>(function Link(props, ref) {
+    const { external, href, ...restProps } = props;
+    let parsedUrl;
 
-  const className = cn("cursor-pointer text-primary", props.className);
+    const className = cn("cursor-pointer text-primary", props.className);
 
-  try {
-    parsedUrl = props.external ? new URL(props.href as string) : undefined;
-  } catch (error) {}
+    try {
+      parsedUrl = props.external ? new URL(props.href as string) : undefined;
+    } catch (error) {}
 
-  return (
-    <Fragment>
-      {external && (
-        <a
-          {...restProps}
-          href={href}
-          className={className}
-          target={props.target || "_blank"}
-          rel={
-            props.rel || parsedUrl?.hostname.includes("worldcoin.org")
-              ? "noopener" // If the link is in a worldcoin.org domain/subdomain, don't send `noreferrer` to properly track analytics
-              : "noopener noreferrer"
-          }
-        >
-          {props.children}
-        </a>
-      )}
+    return (
+      <Fragment>
+        {external && (
+          <a
+            {...restProps}
+            href={href}
+            className={className}
+            target={props.target || "_blank"}
+            rel={
+              props.rel || parsedUrl?.hostname.includes("worldcoin.org")
+                ? "noopener" // If the link is in a worldcoin.org domain/subdomain, don't send `noreferrer` to properly track analytics
+                : "noopener noreferrer"
+            }
+            ref={ref}
+          >
+            {props.children}
+          </a>
+        )}
 
-      {!external && (
-        <NextLink {...restProps} className={className} href={href} />
-      )}
-    </Fragment>
-  );
-});
+        {!external && <NextLink {...restProps} href={href} passHref />}
+      </Fragment>
+    );
+  })
+);
+
+export { Link };
