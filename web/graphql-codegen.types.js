46858265144b4c996a37d79d2d5ea00ab34107bf
igorosip0v <89008845+igorosip0v@users.noreply.github.com>
2023-03-24 02:52:56 +0100

    refactor: update profile modal (#223)
    * refactor: update profile modal
    
    * fix: prettier

--- web/graphql-codegen.types.js
+++ web/graphql-codegen.types.js
@@ -18,6 +18,7 @@ module.exports = {
         "typescript",
       ],
     },
+
     "src/api": {
       documents: "src/api/**/*.graphql",
       preset: "near-operation-file",
@@ -37,6 +38,7 @@ module.exports = {
         "typescript-graphql-request",
       ],
     },
+
     "src/scenes": {
       documents: "src/scenes/**/*.graphql",
       preset: "near-operation-file",
@@ -58,7 +60,30 @@ module.exports = {
         withMutationFn: true,
       },
     },
+
+    "src/components": {
+      documents: "src/components/**/*.graphql",
+      preset: "near-operation-file",
+      presetConfig: {
+        baseTypesPath: "~@/graphql/graphql",
+        extension: ".generated.ts",
+      },
+      plugins: [
+        {
+          add: {
+            placement: "prepend",
+            content: "/* eslint-disable */",
+          },
+        },
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withMutationFn: true,
+      },
+    },
   },
+
   hooks: {
     afterAllFileWrite: [
       (...filePaths) => {
---
+++ web/src/components/Layout/LoggedUserDisplay/graphql/fetch-user.generated.ts
@@ -0,0 +1,80 @@
+/* eslint-disable */
+import * as Types from "@/graphql/graphql";
+
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
+const defaultOptions = {} as const;
+export type FetchUserQueryVariables = Types.Exact<{
+  id: Types.Scalars["String"];
+}>;
+
+export type FetchUserQuery = {
+  __typename?: "query_root";
+  user: Array<{
+    __typename?: "user";
+    id: string;
+    email: string;
+    name: string;
+    team: { __typename?: "team"; id: string; name?: string | null };
+  }>;
+};
+
+export const FetchUserDocument = gql`
+  query FetchUser($id: String!) {
+    user(where: { id: { _eq: $id } }) {
+      id
+      email
+      name
+      team {
+        id
+        name
+      }
+    }
+  }
+`;
+
+/**
+ * __useFetchUserQuery__
+ *
+ * To run a query within a React component, call `useFetchUserQuery` and pass it any options that fit your needs.
+ * When your component renders, `useFetchUserQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useFetchUserQuery({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useFetchUserQuery(
+  baseOptions: Apollo.QueryHookOptions<FetchUserQuery, FetchUserQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<FetchUserQuery, FetchUserQueryVariables>(
+    FetchUserDocument,
+    options
+  );
+}
+export function useFetchUserLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    FetchUserQuery,
+    FetchUserQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<FetchUserQuery, FetchUserQueryVariables>(
+    FetchUserDocument,
+    options
+  );
+}
+export type FetchUserQueryHookResult = ReturnType<typeof useFetchUserQuery>;
+export type FetchUserLazyQueryHookResult = ReturnType<
+  typeof useFetchUserLazyQuery
+>;
+export type FetchUserQueryResult = Apollo.QueryResult<
+  FetchUserQuery,
+  FetchUserQueryVariables
+>;
