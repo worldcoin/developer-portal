dc89c977590ea3d7beaaa40fc9bc94943445c460
michaelfurutin <104859710+michaelfurutin@users.noreply.github.com>
2023-03-27 19:36:01 +0300

    fix: app not found state (#236)

--- web/src/scenes/sign-in-with-world-id/index.tsx
+++ web/src/scenes/sign-in-with-world-id/index.tsx
@@ -6,24 +6,21 @@ import useSignInAction from "src/hooks/useSignInAction";
 import { Credentials } from "./Credentials";
 import { Header } from "./Header";
 import { Redirects } from "./Redirects";
+import { NotFound } from "@/components/NotFound";
 
 export const SignIn = memo(function SignIn(props: { user_id?: string }) {
   const { currentApp, isLoading: appIsLoading } = useApps();
   const { actionIsLoading } = useSignInAction();
 
   return (
-    <Layout userId={props.user_id} title="Sign in">
+    <Layout userId={props.user_id} title="Sign in" mainClassName="grid">
       {(appIsLoading || actionIsLoading) && (
         <div className="w-full h-full flex justify-center items-center">
           <Preloader className="w-20 h-20" />
         </div>
       )}
 
-      {!appIsLoading && !currentApp && (
-        <div className="w-full h-full flex justify-center items-center">
-          <h1 className="text-20 font-sora font-semibold">App not found</h1>
-        </div>
-      )}
+      {!appIsLoading && !currentApp && <NotFound className="self-center" />}
 
       {!appIsLoading && !actionIsLoading && currentApp && (
         <div className="grid gap-y-12 content-start">
