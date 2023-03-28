e3c85d5bc2fe317dd8f7299b87af877dff74a44a
Max Petretta <git@maxpetretta.com>
2023-03-28 15:25:31 -0400

    feat: temporary phone credential fallback for new sequencer

--- web/src/pages/api/v1/clients/inclusion_proof.ts
+++ web/src/pages/api/v1/clients/inclusion_proof.ts
@@ -5,7 +5,10 @@ import {
   errorRequiredAttribute,
   errorValidation,
 } from "src/backend/errors";
-import { getAPIServiceClient } from "src/backend/graphql";
+import {
+  getAPIServiceClient,
+  getWLDAppBackendServiceClient,
+} from "src/backend/graphql";
 import {
   PHONE_SEQUENCER,
   PHONE_SEQUENCER_STAGING,
@@ -17,6 +20,14 @@ const existsQuery = gql`
   query IdentityCommitmentExists($identity_commitment: String!) {
     revocation(where: { identity_commitment: { _eq: $identity_commitment } }) {
       identity_commitment
+    }
+  }
+`;
+
+const phoneVerifiedQuery = gql`
+  query PhoneNumberVerified($identity_commitment: String!) {
+    user(where: { phoneIdComm: { _eq: $identity_commitment } }) {
+      phoneIsVerified # TODO: Need to check this is present
     }
   }
 `;
@@ -36,6 +47,59 @@ const EXPECTED_ERRORS: Record<string, ISimplifiedError> = {
     detail: "This identity is not verified for the relevant credential.",
   },
 };
+
+// Temporary function to handle missing production commitments from the phone signup sequencer
+async function checkConsumerBackend(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  isStaging: boolean
+) {
+  const consumerClient = await getWLDAppBackendServiceClient(isStaging);
+  // const phoneVerifiedResponse = await consumerClient.query({
+  //   query: phoneVerifiedQuery,
+  //   variables: { identity_commitment: req.body.identity_commitment },
+  // });
+  const phoneVerifiedResponse = { data: { user: { phoneIsVerified: true } } }; // DEBUG
+
+  if (phoneVerifiedResponse.data.user.phoneIsVerified) {
+    console.info(
+      `User's phone number is verified, but not on-chain. Inserting identity: ${req.body.identity_commitment}`
+    );
+    const insertResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_APP_URL}/api/v1/clients/insert_identity`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.CONSUMER_BACKEND_SECRET}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req.body),
+      }
+    );
+
+    console.log("Insert response: ", await insertResponse.text()); // DEBUG
+
+    // Commitment inserted, return a pending inclusion error
+    if (insertResponse.status === 200) {
+      return res.status(400).json({
+        code: "inclusion_pending",
+        detail:
+          "This identity is in progress of being included on-chain. Please wait a few minutes and try again.",
+      });
+    }
+
+    // Commitment not inserted, return generic error
+    else {
+      console.error(
+        `Error inserting identity: ${req.body.identity_commitment}`
+      );
+      return res.status(503).json({
+        code: "server_error",
+        detail: "Something went wrong. Please try again.",
+      });
+    }
+  }
+}
 
 /**
  * Checks if the given identity commitment is in the revocation table, and if false,
@@ -77,10 +141,11 @@ export default async function handleInclusionProof(
     );
   }
 
-  const client = await getAPIServiceClient();
+  const apiClient = await getAPIServiceClient();
+  const isStaging = req.body.env === "staging" ? true : false;
 
   // ANCHOR: Check if the identity commitment has been revoked
-  const identityCommitmentExistsResponse = await client.query({
+  const identityCommitmentExistsResponse = await apiClient.query({
     query: existsQuery,
     variables: { identity_commitment: req.body.identity_commitment },
   });
@@ -103,9 +168,9 @@ export default async function handleInclusionProof(
   const headers = new Headers();
   headers.append(
     "Authorization",
-    req.body.env === "production"
-      ? `Basic ${process.env.PHONE_SEQUENCER_KEY}`
-      : `Basic ${process.env.PHONE_SEQUENCER_STAGING_KEY}`
+    isStaging
+      ? `Basic ${process.env.PHONE_SEQUENCER_STAGING_KEY}`
+      : `Basic ${process.env.PHONE_SEQUENCER_KEY}`
   );
   headers.append("Content-Type", "application/json");
   const body = JSON.stringify([
@@ -124,18 +189,32 @@ export default async function handleInclusionProof(
     }
   );
 
+  // Commitment found, return the proof
   if (response.status === 200) {
     res.status(200).json({
       inclusion_proof: await response.json(),
     });
-  } else if (response.status === 202) {
+  }
+
+  // Commitment is still pending inclusion, return an error
+  else if (response.status === 202) {
     res.status(400).json({
       code: "inclusion_pending",
       detail:
         "This identity is in progress of being included on-chain. Please wait a few minutes and try again.",
     });
-  } else if (response.status === 400) {
+  }
+
+  // Commitment not found by the sequencer
+  else if (response.status === 400) {
     const errorBody = await response.text();
+
+    console.log("errorBody:", errorBody); // DEBUG
+
+    // Check if the user's phone number is verified on the consumer backend, temporary fix
+    await checkConsumerBackend(req, res, isStaging);
+
+    // Phone was not verified, proceed as normal
     if (Object.keys(EXPECTED_ERRORS).includes(errorBody)) {
       return res.status(400).json(EXPECTED_ERRORS[errorBody]);
     } else {
