bbd8dc04793d1447c1b8e4e86c04c448b448c628
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-04-05 19:12:08 +0900

    update

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -6,11 +6,7 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const customJestConfig: Config = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>"],
   testEnvironment: "node",
   setupFiles: ["./jest.setup.ts"],
