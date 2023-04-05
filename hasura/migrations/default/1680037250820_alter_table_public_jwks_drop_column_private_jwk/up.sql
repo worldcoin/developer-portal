c3aaf67648bf815b83de6f2e4010d8c2a06a1844
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-04-05 20:37:22 +0900

    fix: migrations on JWKs

--- hasura/migrations/default/1680037250820_alter_table_public_jwks_drop_column_private_jwk/up.sql
+++ hasura/migrations/default/1680037250820_alter_table_public_jwks_drop_column_private_jwk/up.sql
@@ -3,5 +3,4 @@ alter table "public"."jwks" drop column "private_jwk" cascade;
 
 alter table "public"."jwks" drop column "alg";
 
-alter table "public"."jwks" add column "kms_id" text
- not null unique;
+alter table "public"."jwks" add column "kms_id" text null default null unique;
