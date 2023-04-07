8a2183120ba6506a8cdc7513744b233949dd4e64
igorosip0v <igor.osipov@ottofeller.com>
2023-04-07 13:33:42 +0200

    fix: fonts

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/globals.css";
 import { usePostHog } from "@/hooks/usePostHog";
 import { client } from "@/services/apollo";
 import { ApolloProvider } from "@apollo/client";
-import { IBM_Plex_Mono, Rubik, Sora } from "@next/font/google";
+import { IBM_Plex_Mono, Rubik, Sora } from "next/font/google";
 import { NextSeo } from "next-seo";
 import type { AppContext, AppProps } from "next/app";
 import Head from "next/head";
