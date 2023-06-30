665575ba6613918f44e2627c74a1d164fed7a620
igorosip0v <igor.osipov@ottofeller.com>
2023-06-30 18:32:13 +0200

    fix: pass missing invite id

--- web/src/pages/login-with-invite.tsx
+++ web/src/pages/login-with-invite.tsx
@@ -1,9 +1,11 @@
+import { setCookie } from "cookies-next";
 import { GetServerSidePropsContext } from "next";
 import {
   generateLoginNonce,
   generateLoginUrl,
 } from "src/backend/login-internal";
 import { Login } from "src/scenes/login-with-invite";
+
 export default Login;
 
 export interface ILoginPageProps {
@@ -15,8 +17,18 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     // If `id_token` is passed, it means that user has been redirected back from the IdP.
     return { props: {} };
   }
+
+  if (ctx.query?.invite) {
+    setCookie("invite_id", ctx.query.invite as string, {
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+      secure: process.env.NODE_ENV === "production",
+      req: ctx.req,
+      res: ctx.res,
+    });
+  }
+
   const nonce = await generateLoginNonce();
-  const loginUrl = generateLoginUrl(nonce, "/login-with-invite");
+  const loginUrl = generateLoginUrl(nonce, `/login-with-invite`);
 
   return {
     props: {
