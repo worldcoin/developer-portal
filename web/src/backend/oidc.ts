0aaa26c04203c1fa419503c32726b7055eb26f40
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-07-06 14:34:46 +0200

    wip

--- web/src/backend/oidc.ts
+++ web/src/backend/oidc.ts
@@ -7,13 +7,7 @@ import {
   OIDCResponseType,
 } from "src/lib/types";
 import { getAPIServiceClient } from "./graphql";
-
-const GENERAL_SECRET_KEY = process.env.GENERAL_SECRET_KEY;
-if (!GENERAL_SECRET_KEY) {
-  throw new Error(
-    "Improperly configured. `GENERAL_SECRET_KEY` env var must be set!"
-  );
-}
+import { verifyHashedSecret } from "./utils";
 
 export const OIDCResponseTypeMapping = {
   code: OIDCResponseType.Code,
@@ -219,6 +213,7 @@ type FetchAppSecretResult = {
   >;
 };
 
+// TODO: Hash secrets as passwords (e.g. `PBKDF2`) instead of using HMAC encryption
 export const authenticateOIDCEndpoint = async (
   auth_header: string
 ): Promise<string | null> => {
@@ -249,11 +244,7 @@ export const authenticateOIDCEndpoint = async (
   }
 
   // ANCHOR: Verify client secret
-  const hmac = crypto.createHmac("sha256", GENERAL_SECRET_KEY);
-  hmac.update(`${app_id}.${client_secret}`);
-  const candidate_secret = hmac.digest("hex");
-
-  if (hmac_secret !== candidate_secret) {
+  if (!verifyHashedSecret(app_id, client_secret, hmac_secret)) {
     console.warn("authenticateOIDCEndpoint - Invalid client secret.");
     return null;
   }
