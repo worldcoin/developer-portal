d9cd7c8f887047c8593abf239487df21f8fabd7c
Denis Bushaev <denis.bushaev@ottofeller.com>
2023-07-19 22:56:23 +0400

    feat: posthog pageview (#333)

--- web/src/hooks/usePostHog.tsx
+++ web/src/hooks/usePostHog.tsx
@@ -2,6 +2,15 @@ import { useRouter } from "next/router";
 import posthog from "posthog-js";
 import { useEffect } from "react";
 
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
+
 export const usePostHog = (): void => {
   const router = useRouter();
 
