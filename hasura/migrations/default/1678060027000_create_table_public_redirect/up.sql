58c6eddba6f0f0db38b4dc7dd3103b1acecddfa6
0xPenryn <penryn@pm.me>
2023-08-07 15:55:19 -0500

    fix: properly escape backslash in SQL string

--- hasura/migrations/default/1678060027000_create_table_public_redirect/up.sql
+++ hasura/migrations/default/1678060027000_create_table_public_redirect/up.sql
@@ -26,11 +26,11 @@ BEGIN
   IF url IS NOT NULL AND url != '' THEN
     
     IF strpos(url, 'http://localhost') = 1 OR strpos(url, 'https://localhost') = 1 THEN
-      IF NOT (url ~* '^https?://localhost(:[0-9]+)?(/[^\s?]*)(\\?[^\s]*)?$') THEN
+      IF NOT (url ~* '^https?://localhost(:[0-9]+)?(/[^\\s?]*)(\\?[^\\s]*)?$') THEN
         RAISE EXCEPTION USING ERRCODE= '22000', MESSAGE= 'Invalid localhost URL format.';
       END IF;
     ELSE
-      IF NOT (url ~* '^https?://([[:alnum:]_-]+\.)+[[:alnum:]_-]+(/[[:alnum:]_\-./?%&=]*)?$') THEN
+      IF NOT (url ~* '^https?://([[:alnum:]_-]+\\.)+[[:alnum:]_-]+(/[[:alnum:]_\\-./?%&=]*)?$') THEN
         RAISE EXCEPTION USING ERRCODE= '22000', MESSAGE= 'Invalid URL format.';
       END IF;
       -- if url is not localhost, it must use https protocol
