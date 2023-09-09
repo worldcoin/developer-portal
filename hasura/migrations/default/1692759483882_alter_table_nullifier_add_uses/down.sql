d2b413c2660304393d5354729258d668dedceef0
Miguel Piedrafita <github@miguel.build>
2023-09-09 01:56:26 +0100

    improve migrations

--- hasura/migrations/default/1692759483882_alter_table_nullifier_add_uses/down.sql
+++ hasura/migrations/default/1692759483882_alter_table_nullifier_add_uses/down.sql
@@ -1,3 +1,21 @@
+BEGIN;
+
+ALTER TABLE "public"."nullifier" DROP CONSTRAINT unique_nullifier_hash;
+
+WITH duplicates AS (
+  SELECT id, nullifier_hash, uses
+  FROM "public"."nullifier"
+  WHERE uses > 1
+)
+INSERT INTO "public"."nullifier" (action_id, created_at, updated_at, nullifier_hash, merkle_root, credential_type)
+SELECT y.action_id, y.created_at, y.updated_at, y.nullifier_hash, y.merkle_root, y.credential_type
+FROM duplicates x
+JOIN "public"."nullifier" y ON x.id = y.id, generate_series(1, x.uses - 1);
+
+UPDATE "public"."nullifier" SET uses = 1;
+
 ALTER TABLE "public"."nullifier"
     DROP COLUMN "uses",
     DROP CONSTRAINT "unique_nullifier_hash";
+
+COMMIT;
--- hasura/migrations/default/1692759483882_alter_table_nullifier_add_uses/up.sql
+++ hasura/migrations/default/1692759483882_alter_table_nullifier_add_uses/up.sql
@@ -1,4 +1,33 @@
+BEGIN;
+
 ALTER TABLE "public"."nullifier"
     DROP COLUMN "merkle_root",
-    ADD COLUMN "uses" INT DEFAULT 1,
-    ADD CONSTRAINT "unique_nullifier_hash" UNIQUE (nullifier_hash);
+    ADD COLUMN uses INT DEFAULT 1;
+
+WITH duplicates AS (
+  SELECT nullifier_hash, MIN(id) AS min_id, COUNT(*) AS cnt
+  FROM "public"."nullifier"
+  GROUP BY nullifier_hash
+  HAVING COUNT(*) > 1
+)
+UPDATE "public"."nullifier"
+SET uses = duplicates.cnt
+FROM duplicates
+WHERE "public"."nullifier"."nullifier_hash" = duplicates.nullifier_hash
+AND "public"."nullifier"."id" = duplicates.min_id;
+
+WITH duplicates AS (
+  SELECT nullifier_hash, MIN(id) AS min_id
+  FROM "public"."nullifier"
+  GROUP BY nullifier_hash
+  HAVING COUNT(*) > 1
+)
+DELETE FROM "public"."nullifier"
+WHERE (nullifier_hash, id) NOT IN (
+  SELECT duplicates.nullifier_hash, duplicates.min_id
+  FROM duplicates
+);
+
+ALTER TABLE "public"."nullifier" ADD CONSTRAINT unique_nullifier_hash UNIQUE(nullifier_hash);
+
+COMMIT;
