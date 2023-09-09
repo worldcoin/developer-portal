1474813f8f2e8254cdea886c7cdfc69245dabbdf
Miguel Piedrafita <github@miguel.build>
2023-09-09 02:08:52 +0100

    fix down migration

--- hasura/migrations/default/1692759483882_alter_table_nullifier_add_uses/down.sql
+++ hasura/migrations/default/1692759483882_alter_table_nullifier_add_uses/down.sql
@@ -1,21 +1,28 @@
 BEGIN;
 
-ALTER TABLE "public"."nullifier" DROP CONSTRAINT unique_nullifier_hash;
-
-WITH duplicates AS (
-  SELECT id, nullifier_hash, uses
-  FROM "public"."nullifier"
-  WHERE uses > 1
-)
-INSERT INTO "public"."nullifier" (action_id, created_at, updated_at, nullifier_hash, merkle_root, credential_type)
-SELECT y.action_id, y.created_at, y.updated_at, y.nullifier_hash, y.merkle_root, y.credential_type
-FROM duplicates x
-JOIN "public"."nullifier" y ON x.id = y.id, generate_series(1, x.uses - 1);
-
-UPDATE "public"."nullifier" SET uses = 1;
-
 ALTER TABLE "public"."nullifier"
-    DROP COLUMN "uses",
-    DROP CONSTRAINT "unique_nullifier_hash";
+    DROP CONSTRAINT unique_nullifier_hash,
+    ADD COLUMN "merkle_root" TEXT;
+
+DO $$
+DECLARE
+    row_record RECORD;
+BEGIN
+
+FOR row_record IN (SELECT * FROM "public"."nullifier" WHERE uses > 1)
+    LOOP
+        -- Duplicate each row (uses - 1) times
+        FOR i IN 1..(row_record.uses - 1)
+        LOOP
+            INSERT INTO "public"."nullifier" (action_id, created_at, updated_at, nullifier_hash, merkle_root, credential_type)
+            VALUES (row_record.action_id, row_record.created_at, row_record.updated_at, row_record.nullifier_hash, row_record.merkle_root, row_record.credential_type);
+        END LOOP;
+    END LOOP;
+
+    UPDATE "public"."nullifier" SET uses = 1;
+
+END $$;
+
+ALTER TABLE "public"."nullifier" DROP COLUMN "uses";
 
 COMMIT;
