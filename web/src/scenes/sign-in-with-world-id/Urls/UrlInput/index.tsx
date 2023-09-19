eb650861bc5c541586ebb877bb3da2e9dcab0c8b
Mickh F <michael.furutin.ext@toolsforhumanity.com>
2023-09-19 16:32:27 +0300

    refactor: use hook-form with yup resolver for url input

--- web/src/scenes/sign-in-with-world-id/Urls/UrlInput/index.tsx
+++ web/src/scenes/sign-in-with-world-id/Urls/UrlInput/index.tsx
@@ -1,44 +1,47 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import cn from "classnames";
-import { ChangeEvent, KeyboardEvent, useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
 import { FieldInput } from "src/components/FieldInput";
 import { validateUrl } from "src/lib/utils";
+import * as yup from "yup";
 
 interface UrlInputProps {
   value: string;
   onChange: (value: string) => void;
 }
 
+const schema = yup.object({
+  url: yup.string().required().test("is-url", "Must be a valid URL", (value) => {
+    return value != null ? validateUrl(value) : true;
+  }),
+});
+
+type UrlFormValues = yup.InferType<typeof schema>;
+
 export function UrlInput(props: UrlInputProps) {
-  const [valid, setValid] = useState(true);
-  const [value, setValue] = useState(props.value);
-
-  const submit = useCallback(() => {
-    if (valid) {
-      props.onChange(value);
-    }
-  }, [props, valid, value]);
-
-  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    setValid(validateUrl(e.target.value));
-  }, []);
-
-  const handleKeyboard = useCallback(
-    (e: KeyboardEvent<HTMLInputElement>) => {
-      if (e.key === "Enter") {
-        submit();
-      }
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, isValid, errors },
+  } = useForm<UrlFormValues>({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+    shouldFocusError: false,
+    defaultValues: {
+      url: props.value ?? "",
     },
-    [submit]
-  );
+  });
+
+  const handleSave = handleSubmit((data, event) => {
+    props.onChange(data.url);
+  });
 
   return (
-    <FieldInput
-      className={cn("w-full", { "border-danger/75": !valid })}
-      value={value}
-      onChange={handleChange}
-      onBlur={submit}
-      onKeyDown={handleKeyboard}
-    />
+    <form onSubmit={handleSave}>
+      <FieldInput
+        className={cn("w-full", { "border-danger/75": !isValid })}
+        {...register("url", { onBlur:handleSave })}
+      />
+    </form>
   );
 }
