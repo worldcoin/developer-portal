24fc112bf9f4b4b56fc37a5695686282de93b998
michaelfurutin <104859710+michaelfurutin@users.noreply.github.com>
2023-09-21 20:35:17 +0300

    feat: improve redirect adding (#384)
    * feat: improve redirect adding
    
    * feat: optimistic update when redirect added
    
    * feat: add error toast on redirect adding error
    
    ---------
    
    Co-authored-by: Igor Osipov <igor.osipov@toolsforhumanity.com>

--- web/src/scenes/sign-in-with-world-id/Redirects/RedirectInput/index.tsx
+++ web/src/scenes/sign-in-with-world-id/Redirects/RedirectInput/index.tsx
@@ -1,8 +1,5 @@
-import cn from "classnames";
-import { ChangeEvent, KeyboardEvent, useCallback, useState } from "react";
-import { FieldInput } from "src/components/FieldInput";
 import { Icon } from "src/components/Icon";
-import { validateUrl } from "src/lib/utils";
+import { UrlInput } from "src/scenes/sign-in-with-world-id/Urls/UrlInput";
 
 interface RedirectInputProps {
   value: string;
@@ -11,38 +8,9 @@ interface RedirectInputProps {
 }
 
 export function RedirectInput(props: RedirectInputProps) {
-  const [valid, setValid] = useState(true);
-  const [value, setValue] = useState(props.value);
-
-  const submit = useCallback(() => {
-    if (valid) {
-      props.onChange(value);
-    }
-  }, [props, valid, value]);
-
-  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    setValid(validateUrl(e.target.value));
-  }, []);
-
-  const handleKeyboard = useCallback(
-    (e: KeyboardEvent<HTMLInputElement>) => {
-      if (e.key === "Enter") {
-        submit();
-      }
-    },
-    [submit]
-  );
-
   return (
     <div className="grid grid-cols-1fr/auto gap-x-2 items-center">
-      <FieldInput
-        className={cn("w-full", { "border-danger/75": !valid })}
-        value={value}
-        onChange={handleChange}
-        onBlur={submit}
-        onKeyDown={handleKeyboard}
-      />
+      <UrlInput value={props.value} onChange={props.onChange} />
 
       <button type="button" onClick={props.onDelete} className="text-0 group">
         <Icon
