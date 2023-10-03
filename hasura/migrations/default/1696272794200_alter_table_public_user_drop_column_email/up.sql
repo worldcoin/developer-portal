029d03cb38a00d28be309d2feeb354002daadb9d
Igor Osipov <igor.osipov@toolsforhumanity.com>
2023-10-03 14:32:56 +0200

    refactor: revert email field

--- hasura/migrations/default/1696272794200_alter_table_public_user_drop_column_email/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."user" drop column "email" cascade;
--- hasura/seeds/default/1676722088705_userSeed.sql
+++ hasura/seeds/default/1676722088705_userSeed.sql
@@ -1,2 +1,2 @@
 SET check_function_bodies = false;
-INSERT INTO public."user" (id, created_at, updated_at, team_id, world_id_nullifier, is_subscribed, ironclad_id) VALUES ('usr_a78f59e547fa5bd3d76bc1a1817c6d89', '2023-02-18 10:44:58.085661+00', '2023-02-18 10:44:58.085661+00', 'team_d7cde14f17eda7e0ededba7ded6b4467', '0x123', false, 'ironclad_123');
+INSERT INTO public."user" (id, created_at, updated_at, email, team_id, world_id_nullifier, is_subscribed, ironclad_id) VALUES ('usr_a78f59e547fa5bd3d76bc1a1817c6d89', '2023-02-18 10:44:58.085661+00', '2023-02-18 10:44:58.085661+00', 'test@worldcoin.org', 'team_d7cde14f17eda7e0ededba7ded6b4467', '0x123', false, 'ironclad_123');
