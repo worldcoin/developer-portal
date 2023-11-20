d6587349ebc27dae0b18705469c4aab6131b40fc
pdtfh <149602456+pdtfh@users.noreply.github.com>
2023-11-19 21:15:16 -0600

    Update web/src/pages/api/v1/precheck/[app_id].ts

--- web/src/pages/api/v1/precheck/[app_id].ts
+++ web/src/pages/api/v1/precheck/[app_id].ts
@@ -153,7 +153,6 @@ export default async function handlePrecheck(
     value: req.body,
   });
 
-  console.log("parsedParams:", parsedParams);
 
   if (!isValid) {
     return handleError(req, res);
