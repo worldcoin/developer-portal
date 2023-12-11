de1b29fa17f86f7e5862b73728ec8e110b63d93a
0xPenryn <penryn@pm.me>
2023-12-11 01:01:20 +0100

    fix: don't proxy/redirect v2 endpoints, it's too much trouble

---
+++ web/src/pages/api/v2/contracts.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { gql } from "@apollo/client";
+import { getAPIServiceClient } from "src/backend/graphql";
+import { errorNotAllowed } from "src/backend/errors";
+
+/**
+ * Fetches the World ID contract addresses (Semaphore) from the local cache.
+ * @param req
+ * @param res
+ */
+export default async function handleContracts(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (!req.method || !["GET", "OPTIONS"].includes(req.method)) {
+    return errorNotAllowed(req.method, res, req);
+  }
+
+  const query = gql`
+    query ENS {
+      cache(where: { key: { _iregex: "^[a-z.-]*id.worldcoin.eth$" } }) {
+        key
+        value
+      }
+    }
+  `;
+
+  const client = await getAPIServiceClient();
+  const response = await client.query({
+    query,
+  });
+
+  res.status(200).json(response.data.cache);
+}
