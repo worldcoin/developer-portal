1302824a9e17aec9172f3709624ef8f1e7c964b6
Igor Osipov <igor.osipov@toolsforhumanity.com>
2023-12-11 15:56:38 +0100

    refactor: sql phone -> device credentials migration

--- hasura/migrations/default/1701452651132_alter_table_public_nullifier_add_check_constraint_credential_type_choices/down.sql
+++
@@ -1,2 +0,0 @@
-alter table "public"."nullifier" drop constraint "credential_type_choices";
-alter table "public"."nullifier" add constraint "credential_type_choices" check (CHECK (credential_type::text = ANY (ARRAY['orb'::text, 'phone'::text])));
--- hasura/migrations/default/1702306472486_replace_phone_credentials_with_device/up.sql
+++ hasura/migrations/default/1702306472486_replace_phone_credentials_with_device/up.sql
@@ -1,2 +1,7 @@
 alter table "public"."nullifier" drop constraint "credential_type_choices";
+
+UPDATE public.nullifier
+SET credential_type = 'device'
+WHERE credential_type = 'phone';
+
 alter table "public"."nullifier" add constraint "credential_type_choices" check (credential_type::text = ANY (ARRAY['orb'::text, 'device'::text]));
