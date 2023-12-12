64a455bae3df0b62a0bf13c6158325352c457dfe
Paolo <149602456+pdtfh@users.noreply.github.com>
2023-12-12 08:41:29 +0100

    update to verification_level

---
+++ hasura/migrations/default/1702365006773_alter_table_public_auth_code_alter_column_credential_type/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."auth_code" rename column "verification_level" to "credential_type";
---
+++ hasura/migrations/default/1702365006773_alter_table_public_auth_code_alter_column_credential_type/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."auth_code" rename column "credential_type" to "verification_level";
