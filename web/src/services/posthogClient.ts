de4342c15752db8defb2e2c3538c4b5fa4ecf29b
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-05 13:33:22 -0800

    initializing the posthog

--- web/src/services/posthogClient.ts
+++ web/src/services/posthogClient.ts
@@ -1,25 +1,36 @@
 import { PostHog } from "posthog-node";
-
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
 interface CaptureEventParams {
   event: string;
   distinctId: string;
   properties: object;
 }
 
-// Initialize PostHog only if it hasn't been initialized yet
-let posthogClient: PostHog | null = null;
+// Function to initialize PostHog
+function initializePostHog(): PostHog | null {
+  if (!publicRuntimeConfig.NEXT_PUBLIC_POSTHOG_API_KEY) {
+    // API Key not available
+    return null;
+  }
 
-if (!posthogClient) {
-  posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_API_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+  return new PostHog(publicRuntimeConfig.NEXT_PUBLIC_POSTHOG_API_KEY, {
+    host:
+      publicRuntimeConfig.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
   });
 }
+
+// Initialize PostHog only if it hasn't been initialized yet
+let posthogClient: PostHog | null = null;
 
 export function captureEvent({
   event,
   distinctId,
   properties,
 }: CaptureEventParams): void {
+  if (!posthogClient) {
+    posthogClient = initializePostHog();
+  }
   if (posthogClient) {
     posthogClient.capture({
       distinctId,
