fb252473407eb245da75fddbb6d9e06d6f846114
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-05 12:14:05 -0800

    migration file

---
+++ hasura/migrations/default/1704485495003_alter_table_public_user_add_column_posthog_id/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."user" add column "posthog_id" varchar
+--  null unique default gen_random_friendly_id('posthog_id'::character varying);
