1540682e5cdaba2ff671e2aa8ed68ab6dbb5c365
pdtfh <149602456+pdtfh@users.noreply.github.com>
2024-01-09 12:57:21 -0600

    fix: allow undefined signals (#437)

--- web/tests/integration/verify.test.ts
+++ web/tests/integration/verify.test.ts
@@ -224,7 +224,7 @@ describe("/api/v1/verify/[app_id]", () => {
       })
     );
 
-    // Send both requests at the same time
+    // Send all requests at the same time
     await Promise.all(requests.map(({ req, res }) => handleVerify(req, res)));
 
     // Only one of the requests should be successful (this test action allows two uses)
@@ -265,5 +265,71 @@ describe("/api/v1/verify/[app_id]", () => {
       `SELECT uses FROM nullifier WHERE nullifier_hash = '${validRequestPayload.nullifier_hash}';`
     );
     expect(nullifierQuery.rows[0].uses).toBe(2);
+  });
+
+  test("handles race conditions with multiple insertions", async () => {
+    // This test case covers the case of the nullifier record being inserted first and then being updated
+    const appQuery = await integrationDBExecuteQuery(
+      "SELECT * FROM app where name = 'Multi-claim App' limit 1;"
+    );
+    const app_id = appQuery.rows[0].id;
+    const actionQuery = await integrationDBExecuteQuery(
+      `SELECT * FROM action where name = 'Multi-claim action' limit 1;`
+    );
+    expect(actionQuery.rows[0].max_verifications).toBe(2); // Sanity check
+    const action = actionQuery.rows[0].action;
+
+    const validRequestPayload = validParams(app_id, action);
+    validRequestPayload.nullifier_hash =
+      "0x011111b95a5a808a36d3966216404ff4d522f1e66ecddf9c22439393f0011111";
+
+    // User verifies for the first time
+    const { req, res } = createMocks({
+      method: "POST",
+      query: { app_id },
+      body: validRequestPayload,
+    });
+    await handleVerify(req, res);
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toEqual(
+      expect.objectContaining({
+        action,
+        uses: 1,
+        max_uses: 2,
+        success: true,
+        created_at: expect.any(String),
+        nullifier_hash:
+          "0x011111b95a5a808a36d3966216404ff4d522f1e66ecddf9c22439393f0011111",
+      })
+    );
+
+    const requests = [1, 2, 3, 4, 5].map(() =>
+      createMocks({
+        method: "POST",
+        query: { app_id },
+        body: validRequestPayload,
+      })
+    );
+
+    // Send all requests at the same time
+    await Promise.all(requests.map(({ req, res }) => handleVerify(req, res)));
+
+    // Only one of the requests should be successful (this test action allows two uses)
+    const successResponses = requests.filter(
+      ({ res }) => res._getStatusCode() === 200
+    );
+    expect(successResponses.length).toBe(1); // Only one is allowed to reach the maximum of 2 verifications
+    expect(successResponses[0].res._getJSONData()).toEqual(
+      expect.objectContaining({ max_uses: 2, uses: 2 })
+    );
+
+    const errorResponses = requests.filter(
+      ({ res }) => res._getStatusCode() !== 200
+    );
+    expect(errorResponses.length).toBe(4);
+    const updatedNullifierHash = await integrationDBExecuteQuery(
+      `SELECT uses FROM nullifier WHERE nullifier_hash = '${validRequestPayload.nullifier_hash}';`
+    );
+    expect(updatedNullifierHash.rows[0].uses).toBe(2);
   });
 });
