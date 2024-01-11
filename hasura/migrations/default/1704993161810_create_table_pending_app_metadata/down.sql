911d99378f24bc703f2c5395a8e0c8a384766a03
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-01-11 12:17:21 -0800

    Feat: Add Pending App Metadata table to Hasura (#443)
    * rebase on main
    
    * fix the rebase
    
    * update fields, revoke id permissions and create app table relationship
    
    * add url check trigger
    
    * added down migration
    
    * change table name
    
    * update permissions and fix down migrations
    
    * save permissions
    
    * re-order down migration to be inverse of up
    
    ---------
    
    Co-authored-by: Andy Wang <andy.wang@toolsforhumanity.com>

---
+++ hasura/migrations/default/1704993161810_create_table_pending_app_metadata/down.sql
@@ -0,0 +1,9 @@
+DROP TRIGGER IF EXISTS "validate_metadata_uris" ON "public"."pending_app_metadata";
+
+DROP FUNCTION IF EXISTS "validate_metadata_urls";
+
+DROP FUNCTION IF EXISTS "validate_single_url";
+
+DROP TRIGGER IF EXISTS "set_public_pending_app_metadata_updated_at" ON "public"."pending_app_metadata";
+
+DROP TABLE "public"."pending_app_metadata";
