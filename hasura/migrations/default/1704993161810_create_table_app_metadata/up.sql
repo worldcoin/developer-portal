176f4d6e6fec6ca7efb1c5042031d49a7bdadb7c
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-12 10:44:52 -0800

    permission changes and file updates

--- hasura/migrations/default/1704993161810_create_table_app_metadata/up.sql
+++ hasura/migrations/default/1704993161810_create_table_app_metadata/up.sql
@@ -2,22 +2,22 @@ CREATE TABLE "public"."app_metadata" (
   "id" varchar NOT NULL UNIQUE DEFAULT gen_random_friendly_id('update'),
   "app_id" varchar NOT NULL,
   "name" varchar NOT NULL,
-  "logo_img_url" varchar NULL,
+  "logo_img_url" varchar NOT NULL DEFAULT '',
   "showcase_img_urls" text[] NULL,
-  "hero_image_url" varchar NULL,
-  "description" text NOT NULL,
-  "world_app_description" varchar NULL,
-  "category" varchar NULL,
+  "hero_image_url" varchar NOT NULL DEFAULT '',
+  "description" text NOT NULL DEFAULT '',
+  "world_app_description" varchar NOT NULL DEFAULT '',
+  "category" varchar NOT NULL DEFAULT '',
   "is_developer_allow_listing" bool NOT NULL DEFAULT false,
-  "integration_url" varchar NULL,
-  "app_website_url" varchar NULL,
-  "source_code_url" varchar NULL,
+  "integration_url" varchar NOT NULL DEFAULT '',
+  "app_website_url" varchar NOT NULL DEFAULT '',
+  "source_code_url" varchar NOT NULL DEFAULT '',
   "verified_at" timestamptz NULL,
-  "reviewed_by" varchar NULL,
+  "reviewed_by" varchar NOT NULL DEFAULT '',
   "is_reviewer_app_store_approved" bool NOT NULL DEFAULT false,
   "is_reviewer_world_app_approved" bool NOT NULL DEFAULT false,
-  "review_message" text NULL,
-  "status" varchar NULL,
+  "review_message" text NOT NULL DEFAULT '',
+  "status" varchar NOT NULL DEFAULT 'unverified',
   "created_at" timestamptz NOT NULL DEFAULT now(),
   "updated_at" timestamptz NOT NULL DEFAULT now(),
   primary key ("id")
@@ -85,14 +85,14 @@ DECLARE
   item text;
 BEGIN
   IF NEW.logo_img_url IS NOT NULL AND NEW.logo_img_url != '' THEN
-    IF NEW.logo_img_url !~* '^(logo_img_url\.png|logo_img_url\.jpg)$' THEN
-      RAISE EXCEPTION 'logo_img_url must be either logo_img_url.png or logo_img_url.jpg';
+    IF NEW.logo_img_url !~* '^(logo_img\.png|logo_img\.jpg)$' THEN
+      RAISE EXCEPTION 'logo_img path must be either logo_img.png or logo_img.jpg';
     END IF;
   END IF;
 
   IF NEW.hero_image_url IS NOT NULL AND NEW.hero_image_url != '' THEN
-    IF NEW.hero_image_url !~* '^(hero_image_url\.png|hero_image_url\.jpg)$' THEN
-      RAISE EXCEPTION 'hero_image_url must be either hero_image_url.png or hero_image_url.jpg';
+    IF NEW.hero_image_url !~* '^(hero_image\.png|hero_image\.jpg)$' THEN
+      RAISE EXCEPTION 'hero_image value must be either hero_image.png or hero_image.jpg';
     END IF;
   END IF;
 
@@ -103,8 +103,8 @@ BEGIN
 
     FOREACH item IN ARRAY NEW.showcase_img_urls
     LOOP
-      IF item !~* '^(showcase_img_urls_[1-3]\.(png|jpg))$' THEN
-        RAISE EXCEPTION 'Each item in showcase_img_urls must follow the pattern showcase_img_urls_n.png or showcase_img_urls_n.jpg where n is 1, 2, or 3';
+      IF item !~* '^(showcase_img_[1-3]\.(png|jpg))$' THEN
+        RAISE EXCEPTION 'Each item in showcase_img_urls must follow the pattern showcase_img_n.jpg where n is 1, 2, or 3';
       END IF;
     END LOOP;
   END IF;
