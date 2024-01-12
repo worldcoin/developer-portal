d4c1137edcba90c8688616017c8817080e75a681
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-12 13:27:44 -0800

    squashed migration

---
+++ hasura/migrations/default/1705087600730_alter_table_app_drop_metadata/up.sql
@@ -0,0 +1,11 @@
+alter table "public"."app"
+drop column "name" cascade;
+
+alter table "public"."app"
+drop column "description_internal" cascade;
+
+alter table "public"."app"
+drop column "logo_url" cascade;
+
+alter table "public"."app"
+drop column "verified_at" cascade;
