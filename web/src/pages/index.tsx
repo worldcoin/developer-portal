423c45996b5914299434714ca90150c9f85bd072
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-01-16 22:01:58 +0100

    feat: prefetch teams when on '/'

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { getFirstTeamIdByUserId } from "@/helpers/server/get-first-team-id-by-user-id";
 import { getSession } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
 import { urls } from "src/lib/urls";
@@ -7,6 +8,7 @@ export default page;
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession(req, res);
+  const user_id = session?.user.hasura.id;
 
   if (!session?.user) {
     return {
@@ -17,9 +19,20 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     };
   }
 
+  const team_id = await getFirstTeamIdByUserId(user_id);
+
+  if (!team_id) {
+    return {
+      redirect: {
+        destination: "/404",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     redirect: {
-      destination: urls.app(),
+      destination: urls.app({ team_id }),
       permanent: false,
     },
   };
