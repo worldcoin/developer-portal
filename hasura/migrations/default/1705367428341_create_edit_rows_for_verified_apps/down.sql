650fbfb1ac8c057aeabc5c9794df8a09951578c4
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-16 09:54:23 -0800

    remove app prefill but keep function change, update front end code to accomodate new structure

--- hasura/migrations/default/1705367428341_create_edit_rows_for_verified_apps/down.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM "public"."app_metadata"
-WHERE verification_status = 'unverified'
-AND app_id IN (
-    SELECT app_id
-    FROM "public"."app_metadata"
-    WHERE verification_status = 'verified'
-);
