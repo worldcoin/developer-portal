824374d307259bd448af76534d8356338d7ad923
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-01-16 10:57:31 +0100

    feat: improve seeds

--- hasura/seeds/default/1704901197527_membershipsSeed.sql
+++ hasura/seeds/default/1704901197527_membershipsSeed.sql
@@ -1,2 +1,4 @@
 SET check_function_bodies = false;
 INSERT INTO public.membership (id, created_at, updated_at, user_id, role, team_id) VALUES ('memb_522f3b81b0ad15c2366f328f99360b18', '2024-01-10 15:38:40.132843+00', '2024-01-10 15:38:40.132843+00', 'usr_a78f59e547fa5bd3d76bc1a1817c6d89', 'OWNER', 'team_d7cde14f17eda7e0ededba7ded6b4467');
+INSERT INTO public.membership (id, created_at, updated_at, user_id, role, team_id) VALUES ('memb_522f3b81b0ad15c2366f328f99360b19', '2024-01-11 15:38:40.132843+00', '2024-01-11 15:38:40.132843+00', 'usr_a78f59e547fa5bd3d76bc1a1817c6d90', 'ADMIN', 'team_d7cde14f17eda7e0ededba7ded6b4467');
+INSERT INTO public.membership (id, created_at, updated_at, user_id, role, team_id) VALUES ('memb_522f3b81b0ad15c2366f328f99360b20', '2024-01-12 15:38:40.132843+00', '2024-01-12 15:38:40.132843+00', 'usr_a78f59e547fa5bd3d76bc1a1817c6d91', 'MEMBER', 'team_d7cde14f17eda7e0ededba7ded6b4467');
