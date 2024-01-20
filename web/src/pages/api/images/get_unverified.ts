fc7ec2e0094daadcf3952fea1b44a85a6088a543
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-19 19:45:34 -0800

    remove file

--- web/src/pages/api/images/get_unverified.ts
+++
@@ -1,3 +0,0 @@
-// NOTE: use src/api when you need to make gql request using codegen
-import { handleGetAllUnverifiedImages } from "@/api/images/get_unverified_images";
-export default handleGetAllUnverifiedImages;
