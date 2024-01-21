2fe0032ff0dfa4e1d261165372412f9e207162f8
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-20 19:15:15 -0800

    add conditional to fix api test

--- web/src/pages/api/v1/precheck/[app_id].ts
+++ web/src/pages/api/v1/precheck/[app_id].ts
@@ -206,7 +206,7 @@ export default async function handlePrecheck(
   }
   const app_metadata = rawAppValues.app_metadata[0];
   const verified_app_metadata = rawAppValues.verified_app_metadata[0];
-  const logo_img_url = verified_app_metadata.logo_img_url
+  const logo_img_url = verified_app_metadata?.logo_img_url
     ? `${process.env.NEXT_PUBLIC_VERIFIED_CDN_URL}/verified/${rawAppValues.id}/${verified_app_metadata?.logo_img_url}`
     : "";
   const app: _App = {
