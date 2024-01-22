3eb20dc12d25679f8023a41f2e1e63ca84e873f0
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-01-22 18:55:26 +0100

    chore: update seeds for tests

--- hasura/seeds/default/1678146259008_appSeed.sql
+++ hasura/seeds/default/1678146259008_appSeed.sql
@@ -1,3 +1,4 @@
+-- team_d7cde14f17eda7e0ededba7ded6b4467
 WITH
     inserted_app1 AS (
         INSERT INTO
@@ -208,3 +209,216 @@ SELECT
     'unverified'
 FROM
     inserted_app5;
+
+
+-- team_d7cde14f17eda7e0ededba7ded6b4468
+WITH
+    inserted_app6 AS (
+        INSERT INTO
+            "public"."app" (
+                is_archived,
+                is_staging,
+                name,
+                team_id,
+                engine,
+                status,
+                created_at,
+                updated_at
+            )
+        VALUES
+            (
+                false,
+                false,
+                'Sign In App_T2',
+                'team_d7cde14f17eda7e0ededba7ded6b4468',
+                'cloud',
+                'active',
+                '2023-02-18T11:20:39.530041+00:00',
+                '2023-03-06T23:00:15.992529+00:00'
+            ) RETURNING id
+    )
+INSERT INTO
+    "public"."app_metadata" (
+        app_id,
+        name,
+        description,
+        logo_img_url,
+        verification_status
+    )
+SELECT
+    id,
+    'Sign In App_T2',
+    'This app is for Sign In with World ID',
+    'logo_img.png',
+    'unverified'
+FROM
+    inserted_app6;
+
+WITH
+    inserted_app7 AS (
+        INSERT INTO
+            "public"."app" (
+                is_archived,
+                is_staging,
+                name,
+                team_id,
+                engine,
+                status,
+                created_at,
+                updated_at
+            )
+        VALUES
+            (
+                false,
+                true,
+                'Multi-claim App_T2',
+                'team_d7cde14f17eda7e0ededba7ded6b4468',
+                'cloud',
+                'active',
+                '2023-02-18T11:20:39.530041+00:00',
+                '2023-03-06T23:00:15.995455+00:00'
+            ) RETURNING id
+    )
+INSERT INTO
+    "public"."app_metadata" (
+        app_id,
+        name,
+        description,
+        logo_img_url,
+        verification_status
+    )
+SELECT
+    id,
+    'Multi-claim App',
+    'This app has a multi-claim custom action',
+    '',
+    'unverified'
+FROM
+    inserted_app7;
+
+WITH
+    inserted_app8 AS (
+        INSERT INTO
+            "public"."app" (
+                is_archived,
+                is_staging,
+                name,
+                team_id,
+                engine,
+                status,
+                created_at,
+                updated_at
+            )
+        VALUES
+            (
+                false,
+                false,
+                'On-chain App_T2',
+                'team_d7cde14f17eda7e0ededba7ded6b4468',
+                'on-chain',
+                'active',
+                '2023-02-18T11:20:39.530041+00:00',
+                '2023-03-06T23:00:15.998795+00:00'
+            ) RETURNING id
+    )
+INSERT INTO
+    "public"."app_metadata" (
+        app_id,
+        name,
+        description,
+        logo_img_url,
+        verification_status
+    )
+SELECT
+    id,
+    'On-chain App_T2',
+    'This app is on-chain and in production',
+    '',
+    'unverified'
+FROM
+    inserted_app8;
+
+WITH
+    inserted_app9 AS (
+        INSERT INTO
+            "public"."app" (
+                is_archived,
+                is_staging,
+                name,
+                team_id,
+                engine,
+                status,
+                created_at,
+                updated_at
+            )
+        VALUES
+            (
+                false,
+                true,
+                'Custom Action App_T2',
+                'team_d7cde14f17eda7e0ededba7ded6b4468',
+                'cloud',
+                'active',
+                '2023-02-18T11:20:39.530041+00:00',
+                '2023-03-06T23:00:16.00117+00:00'
+            ) RETURNING id
+    )
+INSERT INTO
+    "public"."app_metadata" (
+        app_id,
+        name,
+        description,
+        logo_img_url,
+        verification_status
+    )
+SELECT
+    id,
+    'Custom Action App_T2',
+    'This app has a one-time custom action',
+    '',
+    'unverified'
+FROM
+    inserted_app9;
+
+WITH
+    inserted_app10 AS (
+        INSERT INTO
+            "public"."app" (
+                is_archived,
+                is_staging,
+                name,
+                team_id,
+                engine,
+                status,
+                created_at,
+                updated_at
+            )
+        VALUES
+            (
+                true,
+                true,
+                'Archived App_T2',
+                'team_d7cde14f17eda7e0ededba7ded6b4468',
+                'cloud',
+                'inactive',
+                '2023-02-18T11:20:39.530041+00:00',
+                '2023-03-06T23:00:15.98657+00:00'
+            ) RETURNING id
+    )
+    -- Insert into the app_metadata table using the returned id from the app table
+INSERT INTO
+    "public"."app_metadata" (
+        app_id,
+        name,
+        description,
+        logo_img_url,
+        verification_status
+    )
+SELECT
+    id,
+    'Archived App_T2',
+    'This app is inactive and archived',
+    'logo_img.jpg',
+    'unverified'
+FROM
+    inserted_app10;
