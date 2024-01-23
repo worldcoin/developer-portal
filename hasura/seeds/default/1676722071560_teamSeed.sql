5dc640bb61bc014b208dafd938f245d237dc6596
Igor Osipov <89008845+igorosip0v@users.noreply.github.com>
2024-01-23 20:27:44 +0100

    M2m test (#468)
    * chore: update seeds for tests
    
    * fix: action test
    
    * feat: add m2m test for team
    
    * feat: add m2m test for app
    
    * feat: add m2m test for user
    
    * refactor: getAPIUserClient
    
    * update integration tests
    
    * test-integration
    
    * permission issue
    
    * test the integration test
    
    * removed an update that was breaking
    
    * updated tests with paolos comments
    
    * clean up app tests api key
    
    * fix tests
    
    * add ext null to all auth
    
    * app id
    
    ---------
    
    Co-authored-by: Andy Wang <andy.wang@toolsforhumanity.com>

--- hasura/seeds/default/1676722071560_teamSeed.sql
+++ hasura/seeds/default/1676722071560_teamSeed.sql
@@ -1,2 +1,22 @@
-SET check_function_bodies = false;
-INSERT INTO public.team (id, name, created_at, updated_at) VALUES ('team_d7cde14f17eda7e0ededba7ded6b4467', 'Testing Co', '2023-02-18 10:44:27.212563+00', '2023-02-18 10:44:27.212563+00');
+SET
+    check_function_bodies = false;
+
+INSERT INTO
+    public.team (id, name, created_at, updated_at)
+VALUES
+    (
+        'team_d7cde14f17eda7e0ededba7ded6b4467',
+        'Testing Co',
+        '2023-02-18 10:44:27.212563+00',
+        '2023-02-18 10:44:27.212563+00'
+    );
+
+INSERT INTO
+    public.team (id, name, created_at, updated_at)
+VALUES
+    (
+        'team_2222214f17eda7e0ededba7ded6b4222',
+        'Testing Co #2',
+        '2024-01-01 10:44:27.212563+00',
+        '2024-01-01 10:44:27.212563+00'
+    );
