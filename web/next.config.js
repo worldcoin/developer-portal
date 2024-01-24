199ce8d924fc0436523d312f3b64d6b7501baf54
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-24 11:11:34 -0800

    update the hostname to be more generic

--- web/next.config.js
+++ web/next.config.js
@@ -61,6 +61,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "s3.us-east-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: `s3.${process.env.ASSETS_S3_REGION}.amazonaws.com`,
         pathname: `/${process.env.ASSETS_S3_BUCKET_NAME}/unverified/**`,
       },
     ],
