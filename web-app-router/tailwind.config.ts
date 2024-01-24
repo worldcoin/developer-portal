ac1057613d98e9c3136a54a6cd91999c8fe676b3
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-24 14:33:58 -0800

    reset state

--- web-app-router/tailwind.config.ts
+++ web-app-router/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./scenes/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {
