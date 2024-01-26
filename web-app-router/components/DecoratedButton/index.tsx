76e83a85aac6b6b8cae8b0cacd5b6008d19cbfc3
Michael Furutin <michael.furutin@toolsforhumanity.com>
2024-01-26 17:49:31 +0300

    feat: example tailwind variants

--- web-app-router/components/DecoratedButton/index.tsx
+++ web-app-router/components/DecoratedButton/index.tsx
@@ -1,7 +1,115 @@
 import { Button, CommonButtonProps } from "@/components/Button";
 import { memo } from "react";
-import clsx from "clsx";
 import { ArrowRightIcon } from "@/components/Icons/ArrowRightIcon";
+import { tv } from 'tailwind-variants'
+
+const decoratedButton = tv({
+  slots: {
+    base: 'px-6 py-2.5 rounded-xl border font-medium relative',
+    inner: 'gap-2 flex items-center justify-center',
+  },
+  variants: {
+    variant: {
+      primary: {
+        base: '',
+        inner: 'before:absolute before:inset-0 before:rounded-[11px] before:shadow-[0_0_0_1px_rgba(255,255,255,.1)_inset]',
+      },
+      secondary: {
+        base: '',
+      },
+      danger: {
+        base: '',
+      }
+    },
+    loading: {
+      true: {},
+      false: {},
+    },
+    disabled: {
+      true: {},
+      false: {},
+    },
+  },
+  compoundVariants: [
+    {
+      variant: 'primary',
+      disabled: false,
+      loading: false,
+      class: {
+        base: 'bg-grey-900 text-white bg-gradient-to-b border-grey-900 from-white/15 to-transparent shadow-button hover:bg-gradient-to-b hover:from-white/20 hover:to-transparent',
+      }
+    },
+    {
+      variant: 'primary',
+      disabled: true,
+      loading: false,
+      class: {
+        base: 'bg-grey-100 text-grey-300 pointer-events-none'
+      },
+    },
+    {
+      variant: 'primary',
+      disabled: false,
+      loading: true,
+      class: {
+        base: 'bg-grey-100 text-grey-400 pointer-events-none'
+      },
+    },
+    {
+      variant: 'secondary',
+      disabled: false,
+      loading: false,
+      class: {
+        base: 'bg-grey-0 text-grey-700 border-grey-200 shadow-button hover:bg-grey-100 hover:text-grey-900',
+      }
+    },
+    {
+      variant: 'secondary',
+      disabled: true,
+      loading: false,
+      class: {
+        base: 'bg-grey-0 text-grey-300 border-grey-100 pointer-events-none'
+      },
+    },
+    {
+      variant: 'secondary',
+      disabled: false,
+      loading: true,
+      class: {
+        base: 'bg-grey-0 text-grey-400 border-grey-200 pointer-events-none'
+      },
+    },
+    {
+      variant: 'danger',
+      disabled: false,
+      loading: false,
+      class: {
+        base: 'bg-grey-0 text-system-error-600 border-system-error-400 inset-0 hover:bg-system-error-50',
+      }
+    },
+    {
+      variant: 'danger',
+      disabled: true,
+      loading: false,
+      class: {
+        base: 'border-system-error-200 text-system-error-300 pointer-events-none'
+      },
+    },
+    {
+      variant: 'danger',
+      disabled: false,
+      loading: true,
+      class: {
+        base: 'border-system-error-300 text-system-error-400 pointer-events-none'
+      },
+    }
+  ],
+  defaultVariants: {
+    variant: 'primary',
+    disabled: false,
+    loading: false,
+  }
+});
 
 type DecoratedButtonProps = CommonButtonProps & {
   icon?: React.ReactElement;
@@ -21,54 +129,20 @@ export const DecoratedButton = memo(function DecoratedButton(
     loading,
     disabled,
     className,
-    variant = "primary",
+    variant,
     ...restProps
   } = props;
 
-  const buttonStyles = {
-    primary: {
-      normal:
-        "bg-grey-900 text-white bg-gradient-to-b border-grey-900 from-white/15 to-transparent shadow-button",
-      hover: "hover:bg-gradient-to-b hover:from-white/20 hover:to-transparent ",
-      disabled: "bg-grey-100 text-grey-300 pointer-events-none",
-      loading: "bg-grey-100 text-grey-400 pointer-events-none",
-    },
-    secondary: {
-      normal: "bg-grey-0 text-grey-700 border-grey-200 shadow-button",
-      hover: "hover:bg-grey-100 hover:text-grey-900 ",
-      disabled: "bg-grey-0 text-grey-300 border-grey-100 pointer-events-none",
-      loading: "bg-grey-0 text-grey-400 border-grey-200 pointer-events-none",
-    },
-    danger: {
-      normal: "bg-grey-0 text-system-error-600 border-system-error-400 inset-0",
-      hover: "hover:bg-system-error-50 ",
-
-      disabled:
-        "border-system-error-200 text-system-error-300 pointer-events-none",
-
-      loading:
-        "border-system-error-300 text-system-error-400 pointer-events-none",
-    },
-  };
+  const { base, inner } = decoratedButton({ variant, disabled, loading })
 
   return (
     <Button
       disabled={disabled}
-      className={clsx(
-        className,
-        "px-6 py-2.5 rounded-xl border font-medium relative",
-        { [buttonStyles[variant].normal]: !disabled && !loading },
-        { [buttonStyles[variant].hover]: !disabled && !loading },
-        disabled && buttonStyles[variant].disabled,
-        loading && buttonStyles[variant].loading
-      )}
+      className={base({ className })}
       {...restProps}
     >
       <div
-        className={clsx("gap-2 flex items-center justify-center", {
-          "before:absolute before:inset-0 before:rounded-[11px] before:shadow-[0_0_0_1px_rgba(255,255,255,.1)_inset]":
-            variant === "primary",
-        })}
+        className={inner()}
       >
         {icon}
         {props.children}
