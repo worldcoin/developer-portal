62012b27373045e720a300a39d758eee1eb68372
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-26 15:35:41 -0800

    initial actions page

---
+++ web-app-router/components/Icons/IncognitoActionIcon.tsx
@@ -0,0 +1,59 @@
+export const IncognitoActionIcon = (props: { className?: string }) => {
+  return (
+    <svg
+      className={props.className}
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1421_28134)">
+        <rect width="40" height="40" rx="20" fill="#9D50FF" />
+        <rect
+          opacity="0.3"
+          width="40"
+          height="40"
+          rx="20"
+          fill="url(#paint0_radial_1421_28134)"
+        />
+        <path
+          d="M20.0898 11.7584C24.6929 11.7584 28.4232 15.4899 28.4232 20.0918C28.4232 24.6936 24.6929 28.4251 20.0898 28.4251"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M17.5902 28.0084C16.5999 27.6711 15.6696 27.1839 14.8594 26.5468M14.8594 13.6366C15.6696 13.0089 16.5999 12.5123 17.5902 12.175M11.7568 18.6301C11.9369 17.6652 12.307 16.7283 12.8372 15.8757M11.7568 21.5533C11.9369 22.5183 12.307 23.4552 12.8372 24.3077"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M16.7568 23.8418C18.484 22.0102 21.6778 21.9239 23.4235 23.8418M21.9384 18.2168C21.9384 19.2523 21.1081 20.0918 20.0839 20.0918C19.0597 20.0918 18.2294 19.2523 18.2294 18.2168C18.2294 17.1813 19.0597 16.3418 20.0839 16.3418C21.1081 16.3418 21.9384 17.1813 21.9384 18.2168Z"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+        />
+      </g>
+      <defs>
+        <radialGradient
+          id="paint0_radial_1421_28134"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(9.09091) rotate(63.4349) scale(44.7214 44.5285)"
+        >
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="white" stop-opacity="0" />
+        </radialGradient>
+        <clipPath id="clip0_1421_28134">
+          <rect width="40" height="40" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
@@ -1,5 +1,45 @@
+import { DecoratedButton } from "@/components/DecoratedButton";
+import { IncognitoActionIcon } from "@/components/Icons/IncognitoActionIcon";
+import { LogoLinesIcon } from "@/components/Icons/LogoLines";
+import { WorldcoinBlueprintIcon } from "@/components/Icons/WorldcoinBlueprintIcon";
+
+// TODO: Ad TWK Lausanne font
 export const ActionsPage = () => {
-  <div>
-    <h1>Actions</h1>
-  </div>;
+  return (
+    <div className="w-full h-full min-h-[100dvh] flex flex-col items-center mt-24">
+      <div className="grid gap-y-4 place-items-center max-w-[600px]">
+        <div className="relative">
+          <LogoLinesIcon className="z-0" />
+          <WorldcoinBlueprintIcon className="absolute inset-0 m-auto z-10 w-[60px] h-[60px] rounded-2xl" />
+        </div>
+        <div className="grid place-items-center gap-y-2 grid-cols-1">
+          <h1 className="text-2xl font-[550]">
+            Create your first incognito action
+          </h1>
+          <p className="text-grey-500 font-[400] text-sm">
+            Allow users to verify as a unique person without revealing their
+            identity
+          </p>
+        </div>
+        <div className="grid grid-cols-auto/1fr/auto gap-x-4 w-full mt-5 border-grey-200 border p-5 rounded-2xl shadow-button">
+          <IncognitoActionIcon />
+          <div className="grid grid-cols-1">
+            <p className="text-grey-900 text-base font-[500]">
+              Create an incognito action
+            </p>
+            <p className="text-xs text-grey-500 font-[400]">
+              Verify users as unique humans
+            </p>
+          </div>
+          <DecoratedButton
+            variant="primary"
+            type="button"
+            className="text-sm px-5 py-1 rounded-[.7rem]"
+          >
+            Create
+          </DecoratedButton>
+        </div>
+      </div>
+    </div>
+  );
 };
