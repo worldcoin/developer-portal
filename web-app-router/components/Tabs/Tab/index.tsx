688183732be27f41e2fbcb5c4046b457b1e7b38f
michaelfurutin <104859710+michaelfurutin@users.noreply.github.com>
2024-01-26 22:08:35 +0300

    feat: tabs component (#491)
    * feat: tabs component
    
    * fix: links highlighting
    
    ---------
    
    Co-authored-by: Igor Osipov <igor.osipov@toolsforhumanity.com>

---
+++ web-app-router/components/Tabs/Tab/index.tsx
@@ -0,0 +1,46 @@
+import { HTMLAttributes, useMemo } from "react";
+import { tv } from "tailwind-variants";
+import Link, { LinkProps } from "next/link";
+import { useSelectedLayoutSegment, useSearchParams } from "next/navigation";
+
+const tab = tv({
+  base: "block px-1 py-3 leading-4",
+  variants: {
+    active: {
+      true: "text-grey-900 cursor-default",
+      false: "text-grey-500",
+    },
+    underlined: {
+      true: "",
+    },
+  },
+  compoundVariants: [
+    {
+      active: true,
+      underlined: true,
+      class: "shadow-[0_1px_0_0]",
+    },
+  ],
+});
+
+type TabProps = HTMLAttributes<HTMLAnchorElement> &
+  LinkProps & {
+    underlined?: boolean;
+    segment: string | null;
+  };
+
+export const Tab = (props: TabProps) => {
+  const { className, children, underlined, ...otherProps } = props;
+  const selectedLayoutSegment = useSelectedLayoutSegment();
+
+  const active = useMemo(
+    () => props.segment === selectedLayoutSegment,
+    [props.segment, selectedLayoutSegment]
+  );
+
+  return (
+    <Link className={tab({ active, underlined, className })} {...otherProps}>
+      {children}
+    </Link>
+  );
+};
