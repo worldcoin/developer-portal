449967242c9a16bb54bc0c524efc333f983bc37d
Michael Furutin <michael.furutin@toolsforhumanity.com>
2024-01-26 20:04:26 +0300

    feat: tabs component

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Profile/layout/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Profile/layout/index.tsx
@@ -1,5 +1,52 @@
+"use client";
 import { ReactNode } from "react";
+import { Tabs, Tab } from "@/components/Tabs";
+import { useParams } from "next/navigation";
 
 export const AppProfileLayout = (props: { children: ReactNode }) => {
-  return <div>{props.children}</div>;
+  const params = useParams<{ teamId: string; appId: string }>();
+  return (
+    <div>
+      <div className="bg-grey-50 border-b border-grey-100">
+        <Tabs className="max-w-[1180px] m-auto">
+          <Tab
+            className="py-4"
+            href={`/teams/${params!.teamId}/apps/${params!.appId}/profile`}
+          >
+            Overview
+          </Tab>
+
+          <Tab
+            className="py-4"
+            href={`/teams/${params!.teamId}/apps/${params!.appId}/profile/store-info`}
+          >
+            Store info
+          </Tab>
+
+          <Tab
+            className="py-4"
+            href={`/teams/${params!.teamId}/apps/${params!.appId}/profile/gallery`}
+          >
+            Gallery
+          </Tab>
+
+          <Tab
+            className="py-4"
+            href={`/teams/${params!.teamId}/apps/${params!.appId}/profile/links`}
+          >
+            Links
+          </Tab>
+
+          <Tab
+            className="py-4"
+            href={`/teams/${params!.teamId}/apps/${params!.appId}/profile/danger`}
+          >
+            Danger zone
+          </Tab>
+        </Tabs>
+      </div>
+
+      {props.children}
+    </div>
+  );
 };
--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/layout/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/layout/index.tsx
@@ -1,7 +1,38 @@
-export const AppIdLayout = () => {
+"use client";
+import { ReactNode } from "react";
+import { Tabs, Tab } from "@/components/Tabs";
+import { useParams } from "next/navigation";
+
+export const AppIdLayout = (props: { children: ReactNode }) => {
+  const params = useParams<{ teamId: string; appId: string }>();
   return (
     <div>
-      <h1>AppId</h1>
+      <div className="border-b border-grey-100">
+        <Tabs className="max-w-[1180px] m-auto">
+          <Tab
+            href={`/teams/${params!.teamId}/apps/${params!.appId}`}
+            underlined
+          >
+            Dashboard
+          </Tab>
+
+          <Tab
+            href={`/teams/${params!.teamId}/apps/${params!.appId}/profile`}
+            underlined
+          >
+            App profile
+          </Tab>
+
+          <Tab
+            href={`/teams/${params!.teamId}/apps/${params!.appId}/actions`}
+            underlined
+          >
+            Incognito actions
+          </Tab>
+        </Tabs>
+      </div>
+
+      {props.children}
     </div>
   );
 };
