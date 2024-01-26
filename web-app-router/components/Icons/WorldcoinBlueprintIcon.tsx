b6a7bc58fcc35cb529c6427972cf29f76bfeeb37
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-01-26 22:04:23 +0100

    feat: login screen

---
+++ web-app-router/components/Icons/WorldcoinBlueprintIcon.tsx
@@ -0,0 +1,227 @@
+export const WorldcoinBlueprintIcon = (props: { className?: string }) => {
+  return (
+    <svg
+      className={props.className}
+      width="80"
+      height="80"
+      viewBox="0 0 80 80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="80" height="80" rx="16" fill="#0066FF" />
+      <g
+        style={{ mixBlendMode: "screen" }}
+        opacity="0.5"
+        filter="url(#filter0_i_2043_16602)"
+      >
+        <rect
+          width="80"
+          height="80"
+          rx="16"
+          fill="#0066FF"
+          fillOpacity="0.01"
+        />
+      </g>
+      <g style={{ mixBlendMode: "screen" }}>
+        <rect
+          width="80"
+          height="80"
+          rx="16"
+          fill="url(#paint0_radial_2043_16602)"
+        />
+      </g>
+      <g style={{ mixBlendMode: "overlay" }} opacity="0.7">
+        <line
+          x1="48.2"
+          y1="8.74228e-09"
+          x2="48.2"
+          y2="80"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+        <line
+          x1="64.2"
+          y1="8.74228e-09"
+          x2="64.2"
+          y2="80"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+        <line
+          x1="16.2"
+          y1="8.74206e-09"
+          x2="16.2"
+          y2="80"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+        <line
+          x1="32.2"
+          y1="8.74228e-09"
+          x2="32.2"
+          y2="80"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+      </g>
+      <g style={{ mixBlendMode: "overlay" }} opacity="0.7">
+        <line
+          y1="31.7981"
+          x2="80"
+          y2="31.7981"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+        <line
+          y1="47.799"
+          x2="80"
+          y2="47.799"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+        <line
+          y1="15.799"
+          x2="80"
+          y2="15.799"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+        <line
+          y1="63.799"
+          x2="80"
+          y2="63.799"
+          stroke="white"
+          strokeWidth="0.39999"
+          strokeDasharray="1.6 1.6"
+        />
+      </g>
+      <g filter="url(#filter1_dd_2043_16602)">
+        <path
+          d="M62.1051 30.6586C60.8951 27.7988 59.1658 25.2358 56.9627 23.0325C54.7596 20.8291 52.1912 19.0995 49.3375 17.8894C46.3753 16.6336 43.2361 16 39.9943 16C36.7581 16 33.6133 16.6336 30.6511 17.8894C27.7917 19.0995 25.229 20.8291 23.0259 23.0325C20.8228 25.2358 19.0935 27.8045 17.8835 30.6586C16.6335 33.6154 16 36.7606 16 39.9971C16 43.2337 16.6335 46.3789 17.8892 49.3414C19.0992 52.2012 20.8285 54.7642 23.0316 56.9675C25.2347 59.1709 27.8031 60.9005 30.6568 62.1106C33.619 63.3607 36.7581 64 40 64C43.2361 64 46.381 63.3664 49.3432 62.1106C52.2026 60.9005 54.7653 59.1709 56.9684 56.9675C59.1715 54.7642 60.9008 52.1955 62.1108 49.3414C63.3608 46.3789 64 43.2394 64 39.9971C63.9943 36.7606 63.3551 33.6154 62.1051 30.6586ZM32.0266 37.7424C33.0254 33.9065 36.5184 31.0696 40.6678 31.0696H57.3279C58.401 33.1416 59.0858 35.3964 59.3541 37.7424H32.0266ZM59.3541 42.2519C59.0858 44.5979 58.3952 46.8527 57.3279 48.9247H40.6678C36.5241 48.9247 33.0312 46.0878 32.0266 42.2519H59.3541ZM26.2164 26.2176C29.8977 22.5359 34.7891 20.5095 39.9943 20.5095C45.1995 20.5095 50.0908 22.5359 53.7722 26.2176C53.8863 26.3318 53.9948 26.446 54.1032 26.5601H40.6678C37.0778 26.5601 33.7046 27.9586 31.1648 30.4988C29.1672 32.4966 27.8773 35.0139 27.415 37.7481H20.6402C21.1367 33.3928 23.0716 29.3628 26.2164 26.2176ZM39.9943 59.4905C34.7891 59.4905 29.8977 57.4641 26.2164 53.7824C23.0716 50.6372 21.1367 46.6072 20.6402 42.2576H27.415C27.8716 44.9918 29.1672 47.5091 31.1648 49.507C33.7046 52.0471 37.0778 53.4456 40.6678 53.4456H54.1089C54.0005 53.5598 53.8863 53.6739 53.7779 53.7881C50.0966 57.4584 45.1995 59.4905 39.9943 59.4905Z"
+          fill="#0066FF"
+        />
+      </g>
+      <path
+        d="M62.1051 30.6586C60.8951 27.7988 59.1658 25.2358 56.9627 23.0325C54.7596 20.8291 52.1912 19.0995 49.3375 17.8894C46.3753 16.6336 43.2361 16 39.9943 16C36.7581 16 33.6133 16.6336 30.6511 17.8894C27.7917 19.0995 25.229 20.8291 23.0259 23.0325C20.8228 25.2358 19.0935 27.8045 17.8835 30.6586C16.6335 33.6154 16 36.7606 16 39.9971C16 43.2337 16.6335 46.3789 17.8892 49.3414C19.0992 52.2012 20.8285 54.7642 23.0316 56.9675C25.2347 59.1709 27.8031 60.9005 30.6568 62.1106C33.619 63.3607 36.7581 64 40 64C43.2361 64 46.381 63.3664 49.3432 62.1106C52.2026 60.9005 54.7653 59.1709 56.9684 56.9675C59.1715 54.7642 60.9008 52.1955 62.1108 49.3414C63.3608 46.3789 64 43.2394 64 39.9971C63.9943 36.7606 63.3551 33.6154 62.1051 30.6586ZM32.0266 37.7424C33.0254 33.9065 36.5184 31.0696 40.6678 31.0696H57.3279C58.401 33.1416 59.0858 35.3964 59.3541 37.7424H32.0266ZM59.3541 42.2519C59.0858 44.5979 58.3952 46.8527 57.3279 48.9247H40.6678C36.5241 48.9247 33.0312 46.0878 32.0266 42.2519H59.3541ZM26.2164 26.2176C29.8977 22.5359 34.7891 20.5095 39.9943 20.5095C45.1995 20.5095 50.0908 22.5359 53.7722 26.2176C53.8863 26.3318 53.9948 26.446 54.1032 26.5601H40.6678C37.0778 26.5601 33.7046 27.9586 31.1648 30.4988C29.1672 32.4966 27.8773 35.0139 27.415 37.7481H20.6402C21.1367 33.3928 23.0716 29.3628 26.2164 26.2176ZM39.9943 59.4905C34.7891 59.4905 29.8977 57.4641 26.2164 53.7824C23.0716 50.6372 21.1367 46.6072 20.6402 42.2576H27.415C27.8716 44.9918 29.1672 47.5091 31.1648 49.507C33.7046 52.0471 37.0778 53.4456 40.6678 53.4456H54.1089C54.0005 53.5598 53.8863 53.6739 53.7779 53.7881C50.0966 57.4584 45.1995 59.4905 39.9943 59.4905Z"
+        fill="url(#paint1_linear_2043_16602)"
+      />
+      <defs>
+        <filter
+          id="filter0_i_2043_16602"
+          x="0"
+          y="0"
+          width="80"
+          height="81.6"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1.59996" />
+          <feGaussianBlur stdDeviation="1.59996" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_2043_16602"
+          />
+        </filter>
+        <filter
+          id="filter1_dd_2043_16602"
+          x="9.60016"
+          y="14.4"
+          width="60.7997"
+          height="60.7997"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1.59996" />
+          <feGaussianBlur stdDeviation="1.19997" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0980392 0 0 0 0 0.109804 0 0 0 0 0.12549 0 0 0 0.04 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_2043_16602"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4.79988" />
+          <feGaussianBlur stdDeviation="3.19992" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.0980392 0 0 0 0 0.109804 0 0 0 0 0.12549 0 0 0 0.1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect1_dropShadow_2043_16602"
+            result="effect2_dropShadow_2043_16602"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect2_dropShadow_2043_16602"
+            result="shape"
+          />
+        </filter>
+        <radialGradient
+          id="paint0_radial_2043_16602"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(18.1818 -1.52081e-06) rotate(63.4349) scale(89.4427 89.057)"
+        >
+          <stop stopColor="#66C2FF" />
+          <stop offset="1" stopColor="#66C2FF" stopOpacity="0" />
+        </radialGradient>
+        <linearGradient
+          id="paint1_linear_2043_16602"
+          x1="40"
+          y1="16"
+          x2="40"
+          y2="64"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="white" stopOpacity="0.8" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
--- web-app-router/scenes/Onboarding/Login/page/index.tsx
+++ web-app-router/scenes/Onboarding/Login/page/index.tsx
@@ -1,5 +1,39 @@
+import { DecoratedButton } from "@/components/DecoratedButton";
+import { WorldcoinBlueprintIcon } from "@/components/Icons/WorldcoinBlueprintIcon";
+
 export const LoginPage = () => (
-  <div>
-    <h1>Login</h1>
+  <div className="w-full h-full flex items-center justify-center">
+    <div className="grid gap-y-10 max-w-[360px]">
+      <div className="justify-self-center relative w-[120px] h-[120px] flex justify-center items-center">
+        <div className="absolute inset-0 border opacity-[0.16] border-[#D9E1F6] rounded-[24px] p-2.5 shadow-[0px_4px_8px_0px_#D9E1F6]" />
+        <div className="absolute inset-2.5 border border-[#D9E1F6] opacity-[0.16] rounded-20 p-2.5 shadow-[0px_4px_8px_0px_#D9E1F6]" />
+        <WorldcoinBlueprintIcon />
+      </div>
+
+      <div className="grid gap-y-3">
+        <h1 className="text-2xl text-center font-medium font-twk">
+          World ID is now generally available
+        </h1>
+
+        <p className="font-gta text-grey-400">
+          The Worldcoin Protocol will enable a new class of applications built
+          on top of prof of personhood
+        </p>
+      </div>
+
+      <div className="grid gap-y-4">
+        <DecoratedButton href="#" className="py-3">
+          Create an account
+        </DecoratedButton>
+
+        <DecoratedButton
+          variant="secondary"
+          className="py-3"
+          href="https://docs.worldcoin.org"
+        >
+          Explore Docs
+        </DecoratedButton>
+      </div>
+    </div>
   </div>
 );
