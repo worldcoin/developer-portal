9a00634ecab286ce3ad683e3df8fcb840ec7fa7b
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-27 11:44:20 -0800

    create a table component with example

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
@@ -58,13 +58,24 @@ export const ActionsPage = ({ searchParams }: ActionsPageProps) => {
                 Verify users as unique humans
               </p>
             </div>
-            <DecoratedButton
-              variant="primary"
-              href="?createAction=true"
-              className="text-sm px-5 py-1 rounded-[.7rem]"
-            >
-              Create
-            </DecoratedButton>
+            <div className="grid grid-cols-auto/1fr/auto gap-x-4 w-full mt-5 border-grey-200 border p-5 rounded-2xl shadow-button">
+              <IncognitoActionIcon />
+              <div className="grid grid-cols-1">
+                <p className="text-grey-900 text-base font-[500]">
+                  Create an incognito action
+                </p>
+                <p className="text-xs text-grey-500 font-[400]">
+                  Verify users as unique humans
+                </p>
+              </div>
+              <DecoratedButton
+                variant="primary"
+                href="?createAction=true"
+                className="text-sm px-5 py-1 rounded-[.7rem]"
+              >
+                Create
+              </DecoratedButton>
+            </div>
           </div>
         </div>
       </div>
