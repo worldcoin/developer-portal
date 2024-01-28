bea6a411d76d9853bc0e8eafe2e31e3ae73184a1
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-28 13:07:09 -0800

    adjust height of text

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/page/VerifiedTable.tsx/VerifiedRow.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/page/VerifiedTable.tsx/VerifiedRow.tsx
@@ -16,15 +16,15 @@ export const VerifiedRow = (props: {
       className="flex flex-row items-center gap-x-4 px-2 group py-3"
     >
       {/* // TODO: Images for each human? */}
-      <div className="Capitalize rounded-full flex items-center justify-center h-12 w-12 bg-blue-100 text-blue-500 text-base font-[500]">
+      <div className="Capitalize rounded-full flex items-center justify-center h-10 w-10 bg-blue-100 text-blue-500 text-base font-[500]">
         0x
       </div>
       <div className="text-grey-900 text-sm">{`${nullifier.nullifier_hash.slice(0, 10)}...${nullifier.nullifier_hash.slice(-8)}`}</div>
     </div>,
-    <div key={`nullifier_${key}_2`} className="text-grey-500 w-12 text-sm py-3">
+    <div key={`nullifier_${key}_2`} className="text-grey-500 w-12 text-sm ">
       {nullifier.uses}
     </div>,
-    <div key={`nullifier_${key}_3`} className="text-sm text-grey-500 py-3">
+    <div key={`nullifier_${key}_3`} className="text-sm text-grey-500">
       {timeAgo}
     </div>,
   ];
