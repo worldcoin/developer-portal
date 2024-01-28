d8a479399108deaf2b0a00f2e70ebd772ccef75d
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-27 21:03:35 -0800

    update to table component to make sticky

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/ActionsList/ActionRow.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/ActionsList/ActionRow.tsx
@@ -1,6 +1,4 @@
 import { CopyIcon } from "@/components/Icons/CopyIcon";
-import { ElementsIcon } from "@/components/Icons/ElementsIcon";
-import { useCallback, useMemo } from "react";
 import { toast } from "react-toastify";
 import { DetailsMenu } from "./DetailsMenu";
 import { usePathname } from "next/navigation";
@@ -10,24 +8,23 @@ export const ActionRow = (props: { action: any; key: number }) => {
   const { nullifiers } = action;
   const pathName = usePathname();
 
-  const uses = useMemo(() => {
-    let uses = 0;
-    for (const nullifier of nullifiers) {
-      if (!nullifier.uses) {
-        continue;
-      }
+  let uses = 0;
+  for (const nullifier of nullifiers) {
+    if (nullifier.uses) {
       uses += nullifier.uses;
     }
-    return uses;
-  }, [nullifiers]);
+  }
 
-  const copyAction = useCallback(() => {
+  const copyAction = () => {
     navigator.clipboard.writeText(action.action);
     toast.success("Copied to clipboard");
-  }, [action]);
+  };
 
   return [
-    <div key={key} className="flex flex-row items-center gap-x-4 px-2 group">
+    <div
+      key={`${key}_1`}
+      className="flex flex-row items-center gap-x-4 px-2 group"
+    >
       <div className="uppercase rounded-full flex items-center justify-center h-12 w-12 bg-blue-100 text-blue-500 text-base font-[500]">
         {action.name[0]}
       </div>
@@ -36,7 +33,7 @@ export const ActionRow = (props: { action: any; key: number }) => {
         <div className="text-grey-500 text-xs flex items-center justify-center gap-x-2 ">
           {action.action}
           <button
-            className="invisible group-hover:visible cursor-pointer"
+            className="opacity-0 group-hover:opacity-100 cursor-pointer transition-opacity duration-300"
             onClick={copyAction}
           >
             <CopyIcon className="text-grey-500 w-5 h-5 hover:text-grey-700" />
@@ -44,10 +41,10 @@ export const ActionRow = (props: { action: any; key: number }) => {
         </div>
       </div>
     </div>,
-    <span className="text-sm" key={key}>
+    <span className="text-sm" key={`${key}_2`}>
       {uses}
     </span>,
-    <div key={key} className="w-full flex justify-end px-2">
+    <div key={`${key}_3`} className="w-full flex justify-end px-2">
       <DetailsMenu path={`${pathName}/${action.id}`} />
     </div>,
   ];
--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
@@ -3,8 +3,8 @@ import { IncognitoActionIcon } from "@/components/Icons/IncognitoActionIcon";
 import { LogoLinesIcon } from "@/components/Icons/LogoLines";
 import { WorldcoinBlueprintIcon } from "@/components/Icons/WorldcoinBlueprintIcon";
 import { ActionsList } from "./ActionsList/ActionsList";
-import { CreateActionModal } from "./CreateActionModal";
 import clsx from "clsx";
+import { CreateActionModal } from "./CreateActionModal";
 
 type ActionsPageProps = {
   params: Record<string, string> | null | undefined;
@@ -18,38 +18,46 @@ export const ActionsPage = async ({
 }: ActionsPageProps) => {
   const createAction = searchParams?.createAction;
   const listActions = true; //  Temp will replace with a fetch in later component
-  if (createAction) {
-    return <CreateActionModal />;
-  } else {
-    return (
-      <div className="w-full h-full flex flex-col items-center pt-24">
-        <CreateActionModal className={clsx({ hidden: !createAction })} />
-        <div
-          className={clsx(
-            "w-full h-full flex flex-col justify-center items-center pt-24",
-            {
-              hidden: !listActions || createAction,
-            }
-          )}
-        >
-          <ActionsList />
+
+  return (
+    <div className="w-full h-full flex flex-col items-center pt-24">
+      <CreateActionModal className={clsx({ hidden: !createAction })} />
+      <div
+        className={clsx(
+          "w-full h-full flex flex-col justify-center items-center pt-24",
+          {
+            hidden: !listActions || createAction,
+          }
+        )}
+      >
+        <ActionsList />
+      </div>
+      <div
+        className={clsx("grid gap-y-4 place-items-center max-w-[600px]", {
+          hidden: listActions || createAction,
+        })}
+      >
+        <div className="relative">
+          <LogoLinesIcon className="z-0" />
+          <WorldcoinBlueprintIcon className="absolute inset-0 m-auto z-10 w-[60px] h-[60px] rounded-2xl" />
         </div>
-        <div
-          className={clsx("grid gap-y-4 place-items-center max-w-[600px]", {
-            hidden: listActions || createAction,
-          })}
-        >
-          <div className="relative">
-            <LogoLinesIcon className="z-0" />
-            <WorldcoinBlueprintIcon className="absolute inset-0 m-auto z-10 w-[60px] h-[60px] rounded-2xl" />
-          </div>
-          <div className="grid place-items-center gap-y-2 grid-cols-1">
-            <h1 className="text-2xl font-[550]">
-              Create your first incognito action
-            </h1>
-            <p className="text-grey-500 font-[400] text-sm">
-              Allow users to verify as a unique person without revealing their
-              identity
+        <div className="grid place-items-center gap-y-2 grid-cols-1">
+          <h1 className="text-2xl font-[550]">
+            Create your first incognito action
+          </h1>
+          <p className="text-grey-500 font-[400] text-sm">
+            Allow users to verify as a unique person without revealing their
+            identity
+          </p>
+        </div>
+        <div className="grid grid-cols-auto/1fr/auto gap-x-4 w-full mt-5 border-grey-200 border p-5 rounded-2xl shadow-button">
+          <IncognitoActionIcon />
+          <div className="grid grid-cols-1">
+            <p className="text-grey-900 text-base font-[500]">
+              Create an incognito action
+            </p>
+            <p className="text-xs text-grey-500 font-[400]">
+              Verify users as unique humans
             </p>
           </div>
           <div className="grid grid-cols-auto/1fr/auto gap-x-4 w-full mt-5 border-grey-200 border p-5 rounded-2xl shadow-button">
@@ -62,29 +70,18 @@ export const ActionsPage = async ({
                 Verify users as unique humans
               </p>
             </div>
-            <div className="grid grid-cols-auto/1fr/auto gap-x-4 w-full mt-5 border-grey-200 border p-5 rounded-2xl shadow-button">
-              <IncognitoActionIcon />
-              <div className="grid grid-cols-1">
-                <p className="text-grey-900 text-base font-[500]">
-                  Create an incognito action
-                </p>
-                <p className="text-xs text-grey-500 font-[400]">
-                  Verify users as unique humans
-                </p>
-              </div>
-              <DecoratedButton
-                variant="primary"
-                href="?createAction=true"
-                className="text-sm px-5 py-1 rounded-[.7rem]"
-              >
-                Create
-              </DecoratedButton>
-            </div>
+            <DecoratedButton
+              variant="primary"
+              href="?createAction=true"
+              className="text-sm px-5 py-1 rounded-[.7rem]"
+            >
+              Create
+            </DecoratedButton>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export const getAction = cache(async (appId: string) => {
