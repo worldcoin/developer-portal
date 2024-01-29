959093e0587356f4139dc032f74327427e01434e
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-29 10:12:24 -0800

    adjust hide/show to be done with hidden field

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/index.tsx
@@ -4,6 +4,7 @@ import { LogoLinesIcon } from "@/components/Icons/LogoLines";
 import { WorldcoinBlueprintIcon } from "@/components/Icons/WorldcoinBlueprintIcon";
 import { ActionsList } from "./ActionsList/ActionsList";
 import { CreateActionModal } from "./CreateActionModal";
+import clsx from "clsx";
 
 type ActionsPageProps = {
   searchParams: Record<string, string> | null | undefined;
@@ -12,54 +13,61 @@ type ActionsPageProps = {
 // TODO: Ad TWK Lausanne font
 export const ActionsPage = ({ searchParams }: ActionsPageProps) => {
   const createAction = searchParams?.createAction;
-  const alpha = true;
+  const listActions = false; //  Temp will replace with a fetch in later component
   if (createAction) {
     return <CreateActionModal />;
   } else {
-    if (alpha) {
-      return (
-        <div className="w-full h-full flex flex-col justify-center items-center pt-24">
+    return (
+      <div className="w-full h-full flex flex-col items-center pt-24">
+        <CreateActionModal className={clsx({ hidden: !createAction })} />
+        <div
+          className={clsx(
+            "w-full h-full flex flex-col justify-center items-center pt-24",
+            {
+              hidden: !listActions || createAction,
+            }
+          )}
+        >
           <ActionsList />
         </div>
-      );
-    } else {
-      return (
-        <div className="w-full h-full flex flex-col items-center pt-24">
-          <div className="grid gap-y-4 place-items-center max-w-[600px]">
-            <div className="relative">
-              <LogoLinesIcon className="z-0" />
-              <WorldcoinBlueprintIcon className="absolute inset-0 m-auto z-10 w-[60px] h-[60px] rounded-2xl" />
-            </div>
-            <div className="grid place-items-center gap-y-2 grid-cols-1">
-              <h1 className="text-2xl font-[550]">
-                Create your first incognito action
-              </h1>
-              <p className="text-grey-500 font-[400] text-sm">
-                Allow users to verify as a unique person without revealing their
-                identity
+        <div
+          className={clsx("grid gap-y-4 place-items-center max-w-[600px]", {
+            hidden: listActions || createAction,
+          })}
+        >
+          <div className="relative">
+            <LogoLinesIcon className="z-0" />
+            <WorldcoinBlueprintIcon className="absolute inset-0 m-auto z-10 w-[60px] h-[60px] rounded-2xl" />
+          </div>
+          <div className="grid place-items-center gap-y-2 grid-cols-1">
+            <h1 className="text-2xl font-[550]">
+              Create your first incognito action
+            </h1>
+            <p className="text-grey-500 font-[400] text-sm">
+              Allow users to verify as a unique person without revealing their
+              identity
+            </p>
+          </div>
+          <div className="grid grid-cols-auto/1fr/auto gap-x-4 w-full mt-5 border-grey-200 border p-5 rounded-2xl shadow-button">
+            <IncognitoActionIcon />
+            <div className="grid grid-cols-1">
+              <p className="text-grey-900 text-base font-[500]">
+                Create an incognito action
+              </p>
+              <p className="text-xs text-grey-500 font-[400]">
+                Verify users as unique humans
               </p>
             </div>
-            <div className="grid grid-cols-auto/1fr/auto gap-x-4 w-full mt-5 border-grey-200 border p-5 rounded-2xl shadow-button">
-              <IncognitoActionIcon />
-              <div className="grid grid-cols-1">
-                <p className="text-grey-900 text-base font-[500]">
-                  Create an incognito action
-                </p>
-                <p className="text-xs text-grey-500 font-[400]">
-                  Verify users as unique humans
-                </p>
-              </div>
-              <DecoratedButton
-                variant="primary"
-                href="?createAction=true"
-                className="text-sm px-5 py-1 rounded-[.7rem]"
-              >
-                Create
-              </DecoratedButton>
-            </div>
+            <DecoratedButton
+              variant="primary"
+              href="?createAction=true"
+              className="text-sm px-5 py-1 rounded-[.7rem]"
+            >
+              Create
+            </DecoratedButton>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 };
