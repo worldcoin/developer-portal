dce5c2d46ceaf2870fbc13829d0b79b61cf5aff3
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-29 15:08:51 -0800

    format

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/ActionsList/ActionRow/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/ActionsList/ActionRow/index.tsx
@@ -3,10 +3,13 @@ import { toast } from "react-toastify";
 import { DetailsMenu } from "../DetailsMenu";
 import { usePathname } from "next/navigation";
 
-export const ActionRow = (props: { action: any; key: number }) => {
-  const { action, key } = props;
+export const ActionRow = (props: {
+  action: any;
+  key: number;
+  pathName: string;
+}) => {
+  const { action, key, pathName } = props;
   const { nullifiers } = action;
-  const pathName = usePathname();
 
   let uses = 0;
   for (const nullifier of nullifiers) {
@@ -23,14 +26,14 @@ export const ActionRow = (props: { action: any; key: number }) => {
   return [
     <div
       key={`${key}_1`}
-      className="flex flex-row items-center gap-x-4 px-2 group py-4"
+      className="flex flex-row items-center gap-x-4 px-2 group w-[500px] "
     >
       <div className="uppercase rounded-full flex items-center justify-center h-12 w-12 bg-blue-100 text-blue-500 text-base font-[500]">
         {action.name[0]}
       </div>
       <div>
         <div className="text-grey-900 text-sm">{action.name}</div>
-        <div className="text-grey-500 text-xs flex items-center justify-center gap-x-2 ">
+        <div className="text-grey-500 text-xs flex items-center  gap-x-2 ">
           {action.action}
           <button
             className="opacity-0 group-hover:opacity-100 cursor-pointer transition-opacity duration-300"
@@ -41,9 +44,9 @@ export const ActionRow = (props: { action: any; key: number }) => {
         </div>
       </div>
     </div>,
-    <span className="text-sm" key={`${key}_2`}>
+    <div className="text-sm w-[150px] " key={`${key}_2`}>
       {uses}
-    </span>,
+    </div>,
     <div key={`${key}_3`} className="w-full flex justify-end px-2">
       <DetailsMenu path={`${pathName}/${action.id}`} />
     </div>,
