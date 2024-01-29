310f7917d40a1ce62c037bd256a2fc3c6f881c78
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-01-29 21:46:18 +0100

    feat: join page

---
+++ web-app-router/scenes/Onboarding/Join/page/Logo/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { WorldcoinBlueprintIcon } from "@/components/Icons/WorldcoinBlueprintIcon";
+import clsx from "clsx";
+import Image from "next/image";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+export const Logo = (props: {
+  className?: string;
+  src: string | undefined | null;
+}) => {
+  const [image, setImage] = useState<string | null | undefined>(props.src);
+
+  return (
+    <div
+      className={twMerge(
+        clsx("w-20 h-20 rounded-2xl overflow-hidden", props.className)
+      )}
+    >
+      {image && (
+        // FIXME: update src
+        <Image
+          src={image}
+          alt="Team logo"
+          onError={() => setImage(null)}
+          className="w-full h-full object-contain"
+          width={80}
+          height={80}
+        />
+      )}
+
+      {!image && <WorldcoinBlueprintIcon className="w-full h-full" />}
+    </div>
+  );
+};
--- web-app-router/scenes/Onboarding/Join/page/index.tsx
+++ web-app-router/scenes/Onboarding/Join/page/index.tsx
@@ -1,255 +1,52 @@
-"use client";
-
-import {
-  CircleIconContainer,
-  CircleIconContainerProps,
-} from "@/components/CircleIconContainer";
-
-import { AlertIcon } from "@/components/Icons/AlertIcon";
-import { useEffect, useState } from "react";
+import { LayersIconFrame } from "@/components/LayersIconFrame";
+import { Logo } from "./Logo";
+import { TYPOGRAPHY, Typography } from "@/components/Typography";
 import { DecoratedButton } from "@/components/DecoratedButton";
-import { SuccessIcon } from "@/components/Icons/SuccessIcon";
-import { Input } from "@/components/Input";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import { useCallback } from "react";
-import { Radio } from "@/components/Radio";
-
-const testSchema = yup.object().shape({
-  name: yup
-    .string()
-    .required("Name is required")
-    .max(50, "App name cannot exceed 50 characters"),
-  test: yup
-    .number()
-    .max(1500, "Overview cannot exceed 1500 characters")
-    .required("This section is required"),
-  test2: yup
-    .string()
-    .max(1500, "How it works cannot exceed 1500 characters")
-    .optional(),
-  radio: yup.string().optional(),
-});
-
-export type TestFormValues = yup.Asserts<typeof testSchema>;
+import Link from "next/link";
 
 export const JoinPage = () => {
-  const [variant, setVariant] =
-    useState<CircleIconContainerProps["variant"]>("success");
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setVariant((prev) => {
-        if (prev === "success") return "error";
-        if (prev === "error") return "info";
-        if (prev === "info") return "muted";
-        if (prev === "muted") return "success";
-        return "success";
-      });
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting, dirtyFields },
-  } = useForm<TestFormValues>({
-    resolver: yupResolver(testSchema),
-  });
-  const handleSave = useCallback(async (data: TestFormValues) => {
-    try {
-      console.log("App information saved");
-      console.log(data);
-    } catch (errors: any) {
-      console.log(errors);
-    }
-  }, []);
+  const tempTeamData = {
+    name: "A11.studio",
+    logo: "https://worldcoin.org/icons/logo-small.svg",
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-2 h-screen mt-10">
-      <CircleIconContainer variant={variant}>
-        <AlertIcon />
-      </CircleIconContainer>
-      <div className="flex flex-row gap-1">
-        <DecoratedButton
-          type="button"
-          className="w-72"
-          variant="primary"
-          icon={<SuccessIcon />}
-          showArrowRight
-        >
-          Primary
-        </DecoratedButton>
-        <DecoratedButton
-          type="button"
-          variant="primary"
-          className="w-72"
-          icon={<SuccessIcon />}
-          showArrowRight
-          disabled
-        >
-          Primary: Disabled
-        </DecoratedButton>
-        <DecoratedButton
-          type="button"
-          variant="primary"
-          loading
-          className="w-72"
-          showArrowRight
-          icon={<SuccessIcon />}
-        >
-          Primary: Loading
-        </DecoratedButton>
-      </div>
-      <div className="flex flex-row gap-1">
-        <DecoratedButton
-          type="button"
-          className="w-72"
-          variant="secondary"
-          icon={<SuccessIcon />}
-          showArrowRight
-        >
-          Secondary
-        </DecoratedButton>
-        <DecoratedButton
-          type="button"
-          variant="secondary"
-          className="w-72"
-          icon={<SuccessIcon />}
-          showArrowRight
-          disabled
-        >
-          Secondary: Disabled
-        </DecoratedButton>
-        <DecoratedButton
-          type="button"
-          variant="secondary"
-          loading
-          className="w-72"
-          showArrowRight
-          icon={<SuccessIcon />}
-        >
-          Secondary: Loading
-        </DecoratedButton>
-      </div>
-      <div className="flex flex-row gap-1">
-        <DecoratedButton
-          type="button"
-          className="w-72"
-          variant="danger"
-          icon={<SuccessIcon />}
-          showArrowRight
-        >
-          Danger
-        </DecoratedButton>
-        <DecoratedButton
-          type="button"
-          variant="danger"
-          className="w-72"
-          icon={<SuccessIcon />}
-          showArrowRight
-          disabled
-        >
-          Danger: Disabled
-        </DecoratedButton>
-        <DecoratedButton
-          type="button"
-          variant="danger"
-          loading
-          className="w-72"
-          showArrowRight
-          icon={<SuccessIcon />}
-        >
-          Danger: Loading
-        </DecoratedButton>
-      </div>
-      <div className="flex p-2 w-full items-center justify-center">
-        <form
-          onSubmit={handleSubmit(handleSave)}
-          className="flex flex-col items-center  gap-4  w-[500px]"
-          noValidate
-        >
-          <h1>Test Form</h1>
-          <Input
-            register={register("name")}
-            errors={errors.name}
-            required
-            label="Name"
-            className="w-[500px]"
-            addOnPosition="left"
-            addOn={
-              <button
-                type="button"
-                className="w-16 bg-red-500 px-2 ml-1 py-2 rounded-lg"
-              >
-                Test
-              </button>
-            }
-            placeholder="my_signal"
-          />
-          <Input
-            register={register("test")}
-            required
-            errors={errors.test}
-            label="Ronald"
-            className="w-[500px]"
-            placeholder="my_signal"
-            helperText="The number of verifications the same person can do for this action"
-          />
-          <Input
-            register={register("test2")}
-            label="ALPHABET"
-            errors={errors.test2}
-            addOnPosition="left"
-            disabled
-            addOn={
-              <button type="button">
-                <SuccessIcon />
-              </button>
-            }
-            className="w-[500px]"
-            placeholder="my_signal"
-            helperText="The number of verifications the same person can do for this action"
-          />
-          {errors.test2?.message && (
-            <span className="pt-2 left-0 flex items-center text-12 text-error-500">
-              {errors.test2.message}
-            </span>
-          )}
-          <Radio
-            label="Option 1"
-            value="option1"
-            className="w-32"
-            register={register("radio")}
-            errors={errors.radio}
-          />
-          <Radio
-            label="Option 2"
-            register={register("radio")}
-            value="option2"
-            errors={errors.radio}
-            className="w-32 "
-          />
-          <Radio
-            label="Option 3"
-            register={register("radio")}
-            value="option3"
-            errors={errors.radio}
-            className="w-32 "
-            disabled
-          />
-          <DecoratedButton
-            type="submit"
-            className="w-72"
-            variant="primary"
-            icon={<SuccessIcon />}
-            showArrowRight
+    <div className="w-full min-h-[100dvh] flex justify-center items-center">
+      <div className="grid max-w-[360px] gap-y-6">
+        <LayersIconFrame>
+          <Logo src={tempTeamData.logo} />
+        </LayersIconFrame>
+
+        <div className="grid gap-y-3">
+          <Typography variant={TYPOGRAPHY.H6} className="text-center">
+            Join {tempTeamData.name}
+          </Typography>
+
+          <Typography
+            variant={TYPOGRAPHY.R3}
+            className="text-grey-500 text-center"
           >
-            Danger
-          </DecoratedButton>
-        </form>
+            To join team you need to create account on Worldcoin Developer
+            Portal
+          </Typography>
+        </div>
+
+        {/* FIXME: add href when auth0 PR will be merged */}
+        <DecoratedButton href="#" className="mt-2">
+          Join team
+        </DecoratedButton>
+
+        <p className="text-xs leading-[1.3] font-gta text-grey-500 text-center">
+          By signing up, you are creating Developer Portal account and agree to
+          Worldcoin`s{" "}
+          <Link className="underline" href="#">
+            User terms
+          </Link>{" "}
+          and{" "}
+          <Link className="underline" href="#">
+            Privacy notice
+          </Link>
+        </p>
       </div>
     </div>
   );
