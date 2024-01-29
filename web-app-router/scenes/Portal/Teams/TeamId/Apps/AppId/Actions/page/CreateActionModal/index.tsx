4800514ff12a833d2b750ad1bd2735d6522097e2
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-29 09:51:38 -0800

    revert comma

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/CreateActionModal/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/page/CreateActionModal/index.tsx
@@ -104,14 +104,14 @@ export const CreateActionModal = () => {
           });
 
           return toast.error(
-            "An action with this identifier already exists for this app. Please change the 'action' identifier."
+            "An action with this identifier already exists for this app. Please change the 'action' identifier.",
           );
         }
         return toast.error("Error occurred while creating action.");
       }
       toast.success(`Action "${values.name}" created.`);
     },
-    [appId, insertActionQuery, pathname, router, setError]
+    [appId, insertActionQuery, pathname, router, setError],
   );
 
   const copyAction = useCallback(() => {
