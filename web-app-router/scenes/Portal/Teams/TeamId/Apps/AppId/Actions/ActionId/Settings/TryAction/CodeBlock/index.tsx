66985e856cf289a27022c62f691f51908f60840f
Andy Wang <andy.wang@toolsforhumanity.com>
2024-01-30 10:55:39 -0800

    update code

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/Settings/TryAction/CodeBlock/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/Settings/TryAction/CodeBlock/index.tsx
@@ -10,25 +10,29 @@ export const CodeBlock = (props: CodeBlockProps) => {
   const { appId, action_identifier } = props;
 
   const verifyProofCodeString =
-    "// Note: Verify Proof must be called server side\n" +
+    "// Note: Proof must be verified server side\n" +
+    "// For an end to end example see: https://github.com/worldcoin/world-id-cloud-template \n" +
     "const verifyProof = async (proof) => {\n" +
     "    console.log('proof', proof);\n" +
     "    const response = await fetch(\n" +
-    `   'https://developer.worldcoin.org/api/v1/verify/${appId}',\n` +
+    `      'https://developer.worldcoin.org/api/v1/verify/${appId}',\n` +
     "      {\n" +
     "        method: 'POST',\n" +
     "        headers: {\n" +
     "          'Content-Type': 'application/json',\n" +
     "        },\n" +
-    `        body: JSON.stringify({ ...proof, action: ${action_identifier}}),\n` +
+    `        body: JSON.stringify({ ...proof, action: "${action_identifier}"}),\n` +
     "      }\n" +
     "    );\n" +
     "    if (response.ok) {\n" +
     "      const { verified } = await response.json();\n" +
     "      return verified;\n" +
-    "    }\n" +
+    "    } else {\n" +
+    "      const { code, detail } = await response.json();\n" +
+    "      return new Error(`Error Code ${code}: ${detail}`);\n" +
+    "    }" +
     "\n" +
-    "    return false;\n" +
+    `    return new Error("Proof did not verify")\n` +
     "  };";
 
   return (
@@ -42,11 +46,12 @@ export const CodeBlock = (props: CodeBlockProps) => {
         panelText={
           `"use client" // (Next.JS only - IDKitWidget must be run on client)\n` +
           "import { IDKitWidget, VerificationLevel } from '@worldcoin/idkit'\n\n" +
+          `const verifyProof = async (proof) => throw new Error("TODO: verify proof server route")\n\n` +
           "<IDKitWidget\n" +
           `    app_id="${appId}"\n` +
           `    action="${action_identifier}"\n` +
           `    onSuccess={() => console.log("Success")}\n` +
-          `    handleVerify={verifyProof} // Make sure to copy the callback below\n` +
+          `    handleVerify={verifyProof} // Make sure to use the correct callback\n` +
           "    verification_level={VerificationLevel.Device} // Choose between Orb or Device\n" +
           ">\n" +
           "    {({ open }) => <button onClick={open}>Verify with World ID</button>}\n" +
