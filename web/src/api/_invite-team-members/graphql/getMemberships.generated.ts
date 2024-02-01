2d16cab75212fa3a903e0e00806515be2fdafd3a
Paolo <149602456+pdtfh@users.noreply.github.com>
2024-01-31 16:23:11 -0800

    wip

--- web/src/api/_invite-team-members/graphql/getMemberships.generated.ts
+++ web/src/api/_invite-team-members/graphql/getMemberships.generated.ts
@@ -6,10 +6,24 @@ import { GraphQLClientRequestHeaders } from "graphql-request/build/cjs/types";
 import gql from "graphql-tag";
 export type GetMembershipsQueryVariables = Types.Exact<{
   team_id: Types.Scalars["String"];
+  user_id: Types.Scalars["String"];
 }>;
 
 export type GetMembershipsQuery = {
   __typename?: "query_root";
+  team: Array<{
+    __typename?: "team";
+    id: string;
+    memberships: Array<{
+      __typename?: "membership";
+      user: {
+        __typename?: "user";
+        id: string;
+        name: string;
+        email?: string | null;
+      };
+    }>;
+  }>;
   membership: Array<{
     __typename?: "membership";
     user: { __typename?: "user"; email?: string | null };
@@ -17,7 +31,25 @@ export type GetMembershipsQuery = {
 };
 
 export const GetMembershipsDocument = gql`
-  query GetMemberships($team_id: String!) {
+  query GetMemberships($team_id: String!, $user_id: String!) {
+    team(
+      where: {
+        id: { _eq: $team_id }
+        memberships: {
+          user_id: { _eq: $user_id }
+          role: { _in: [ADMIN, OWNER] }
+        }
+      }
+    ) {
+      id
+      memberships {
+        user {
+          id
+          name
+          email
+        }
+      }
+    }
     membership(where: { team_id: { _eq: $team_id } }) {
       user {
         email
