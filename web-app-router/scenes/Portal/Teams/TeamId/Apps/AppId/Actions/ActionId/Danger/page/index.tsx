cb5a0ae20cfffbb6bc59b7048b519854a7ba4ed5
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-02-08 11:19:51 -0800

    WID-707: Fix incognito actions (#540)
    * Fix incognito actions
    
    Move all fetches to client side
    
    * Add permissions to delete
    
    * format
    
    * Add a clarifying comment for on chain

--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/Danger/page/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/Danger/page/index.tsx
@@ -1,58 +1,68 @@
+"use client";
 import { DecoratedButton } from "@/components/DecoratedButton";
 import { DocsIcon } from "@/components/Icons/DocsIcon";
 import Link from "next/link";
 import { CaretIcon } from "@/components/Icons/CaretIcon";
-import { getSdk as GetActionSdk } from "./graphql/server/get-single-action.generated";
-import { getAPIServiceGraphqlClient } from "@/api/helpers/graphql";
 import { ActionDangerZoneContent } from "../ActionDangerZoneContent";
 import { TYPOGRAPHY, Typography } from "@/components/Typography";
+import ErrorComponent from "next/error";
+import { useGetSingleActionQuery } from "./graphql/client/get-single-action.generated";
 
 type ActionIdDangerPageProps = {
   params: Record<string, string> | null | undefined;
   searchParams: Record<string, string> | null | undefined;
 };
 
-export const ActionIdDangerPage = async ({
-  params,
-}: ActionIdDangerPageProps) => {
-  const actionID = params?.actionId;
-  const client = await getAPIServiceGraphqlClient();
-  const data = await GetActionSdk(client).Action({
-    action_id: actionID ?? "",
+export const ActionIdDangerPage = ({ params }: ActionIdDangerPageProps) => {
+  const actionId = params?.actionId;
+  const teamId = params?.teamId;
+  const { data, loading } = useGetSingleActionQuery({
+    variables: { action_id: actionId ?? "" },
+    context: { headers: { team_id: teamId } },
   });
 
   const action = data?.action[0];
 
-  return (
-    <div className="w-full h-full flex flex-col items-center ">
-      <div className="grid gap-y-2 w-full py-10">
-        <div>
-          <Link href=".." className="flex flex-row items-center gap-x-2">
-            <CaretIcon className="h-3 w-3 text-grey-400 rotate-90" />
-            <Typography variant={TYPOGRAPHY.R5} className="text-grey-700">
-              Back to Incognito Actions
+  if (loading) return <div></div>;
+  else if (!action) {
+    return (
+      <ErrorComponent
+        statusCode={404}
+        title="Action not found"
+      ></ErrorComponent>
+    );
+  } else {
+    return (
+      <div className="w-full h-full flex flex-col items-center ">
+        <div className="grid gap-y-2 w-full py-10">
+          <div>
+            <Link href=".." className="flex flex-row items-center gap-x-2">
+              <CaretIcon className="h-3 w-3 text-grey-400 rotate-90" />
+              <Typography variant={TYPOGRAPHY.R5} className="text-grey-700">
+                Back to Incognito Actions
+              </Typography>
+            </Link>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <Typography
+              variant={TYPOGRAPHY.H6}
+              className="text-grey-900 capitalize"
+            >
+              {action.name}
             </Typography>
-          </Link>
+            <DecoratedButton
+              variant="secondary"
+              href="https://docs.worldcoin.org/id/incognito-actions"
+              className="text-grey-700 py-3 px-7"
+            >
+              <DocsIcon />
+              <Typography variant={TYPOGRAPHY.R3}>Learn more</Typography>
+            </DecoratedButton>
+          </div>
+          <hr className="my-5 w-full text-grey-200 border-dashed" />
+          <ActionDangerZoneContent action={action} teamId={teamId} />
         </div>
-        <div className="w-full flex justify-between items-center">
-          <Typography
-            variant={TYPOGRAPHY.H6}
-            className="text-grey-900 capitalize"
-          >
-            {action.name}
-          </Typography>
-          <DecoratedButton
-            variant="secondary"
-            href="https://docs.worldcoin.org/id/incognito-actions"
-            className="text-grey-700 py-3 px-7"
-          >
-            <DocsIcon />
-            <Typography variant={TYPOGRAPHY.R3}>Learn more</Typography>
-          </DecoratedButton>
-        </div>
-        <hr className="my-5 w-full text-grey-200 border-dashed" />
-        <ActionDangerZoneContent action={action} />
       </div>
-    </div>
-  );
+    );
+  }
 };
--- web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/Settings/TryAction/CodeBlock/index.tsx
+++ web-app-router/scenes/Portal/Teams/TeamId/Apps/AppId/Actions/ActionId/Settings/TryAction/CodeBlock/index.tsx
@@ -29,8 +29,8 @@ const onSuccess = () => {
 <IDKitWidget
     app_id="${appId}"
     action="${action_identifier}"
-    // Choose between Orb or Device
-    verification_level={VerificationLevel.Device}
+    ${engine === EngineType.OnChain ? "// On-chain only accepts Orb" : "// Choose between Orb or Device"}
+    verification_level={VerificationLevel.${engine === EngineType.OnChain ? "Orb" : "Device"}}
     handleVerify={verifyProof}
     onSuccess={onSuccess}>
     {({ open }) => (
