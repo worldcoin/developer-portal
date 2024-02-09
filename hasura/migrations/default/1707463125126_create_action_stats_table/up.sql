8f7700b4cf52e48f4205d48bf47ab777a0ff7bf3
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-02-09 10:42:26 -0800

    WID 745: Hasura Changes for Create Action Stats (#546)
    * Create Action Stats
    
    * update comment

---
+++ hasura/migrations/default/1707463125126_create_action_stats_table/up.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "public"."action_stats_returning" (
+    "action_id" text NOT NULL, 
+    "date" timestamp with time zone NOT NULL, 
+    "verifications" numeric NOT NULL, 
+    "unique_users" Numeric NOT NULL, 
+    PRIMARY KEY ("action_id"), 
+    FOREIGN KEY ("action_id") REFERENCES "public"."action"("id") 
+        ON UPDATE restrict 
+        ON DELETE restrict
+);
+
+COMMENT ON TABLE "public"."action_stats_returning" IS E'Returning value of action_stats function';
+
+CREATE OR REPLACE FUNCTION public.action_stats(
+    "timespan" character varying,
+    "startsAt" timestamp with time zone, 
+    "actionId" character varying
+)
+-- If you changing returning values of this function, please, change action_stats_returning columns too
+ RETURNS SETOF action_stats_returning
+ LANGUAGE sql
+ STABLE
+AS $function$
+SELECT
+    action.id as action_id,
+    DATE_TRUNC("timespan", nullifier.created_at) as date,
+    SUM(COUNT(1)) OVER (ORDER BY DATE_TRUNC("timespan", nullifier.created_at)) as verifications,
+    SUM(COUNT(DISTINCT nullifier_hash)) OVER (ORDER BY DATE_TRUNC("timespan", nullifier.created_at)) as unique_users
+FROM nullifier AS nullifier
+LEFT JOIN action action ON action.id = nullifier.action_id
+WHERE action_id = "actionId" AND nullifier.created_at >= "startsAt"
+GROUP BY DATE_TRUNC("timespan", nullifier.created_at), action.id
+$function$;
