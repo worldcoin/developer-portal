253718607f3b7dd6930288a742152470fcc377d4
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-02-15 21:07:36 +0100

    fix: minor style fixes

--- web/components/LoggedUserNav/TeamSwitch/index.tsx
+++ web/components/LoggedUserNav/TeamSwitch/index.tsx
@@ -1,25 +1,18 @@
 import {
-  autoUpdate,
-  FloatingPortal,
-  size,
-  useFloating,
-} from "@floating-ui/react";
-import { Menu, MenuProps, Transition } from "@headlessui/react";
-import {
   Dropdown,
   DropdownButton,
   DropdownItem,
   DropdownItems,
 } from "@/components/Dropdown";
-import { Fragment } from "react";
-import { useFetchTeamsQuery } from "@/components/LoggedUserNav/TeamSwitch/graphql/client/fetch-teams.generated";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { Auth0SessionUser } from "@/lib/types";
-import { LoginSquareIcon } from "@/components/Icons/LoginSquareIcon";
-import { TeamLogo } from "./TeamLogo";
+
 import { AddCircleIcon } from "@/components/Icons/AddCircleIcon";
-import Link from "next/link";
 import { CheckmarkCircleIcon } from "@/components/Icons/CheckmarkCircleIcon";
+import { LoginSquareIcon } from "@/components/Icons/LoginSquareIcon";
+import { useFetchTeamsQuery } from "@/components/LoggedUserNav/TeamSwitch/graphql/client/fetch-teams.generated";
+import { Auth0SessionUser } from "@/lib/types";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
+import { TeamLogo } from "./TeamLogo";
 
 export const TeamSwitch = (props: { selectedTeamId?: string }) => {
   const { user } = useUser() as Auth0SessionUser;
@@ -34,7 +27,7 @@ export const TeamSwitch = (props: { selectedTeamId?: string }) => {
       <Dropdown placement="left-start">
         <DropdownButton>
           <div className="grid grid-cols-auto/1fr items-center gap-x-2 px-4 py-2.5">
-            <LoginSquareIcon className="h-4 w-4 text-grey-400" /> Switch team
+            <LoginSquareIcon className="size-4 text-grey-400" /> Switch team
           </div>
         </DropdownButton>
 
@@ -67,73 +60,11 @@ export const TeamSwitch = (props: { selectedTeamId?: string }) => {
 
           <DropdownItem>
             <div className="grid grid-cols-auto/1fr items-center gap-x-2">
-              <AddCircleIcon className="h-4 w-4 text-grey-400" /> Create new
-              team
+              <AddCircleIcon className="size-4 text-grey-400" /> Create new team
             </div>
           </DropdownItem>
         </DropdownItems>
       </Dropdown>
     </div>
   );
-  // const { refs, floatingStyles } = useFloating({
-  //   placement: "left-start",
-  //   strategy: "fixed",
-  //   whileElementsMounted: autoUpdate,
-  //   middleware: [
-  //     size({
-  //       apply({ availableWidth, availableHeight, elements, rects }) {
-  //         Object.assign(elements.floating.style, {
-  //           minWidth: `${rects.reference.width}px`,
-  //           maxWidth: `${availableWidth}px`,
-  //           maxHeight: `${availableHeight}px`,
-  //         });
-  //       },
-  //     }),
-  //   ],
-  // });
-  //
-  // return (
-  //   <div className="-mx-4 -my-2.5">
-  //     <Menu>
-  //       <Menu.Button
-  //         ref={refs.setReference}
-  //       >
-  //         <div>
-  //           switch team
-  //         </div>
-  //       </Menu.Button>
-  //       <FloatingPortal>
-  //         <div
-  //           ref={refs.setFloating}
-  //           style={floatingStyles}
-  //           className="z-[1] flex flex-col"
-  //         >
-  //           <Transition
-  //             enter="transition duration-300 ease-out"
-  //             enterFrom="transform opacity-0"
-  //             enterTo="transform opacity-100"
-  //             leave="transition duration-150 ease-out"
-  //             leaveFrom="transform opacity-100"
-  //             leaveTo="transform opacity-0"
-  //             as={Fragment}
-  //           >
-  //             <Menu.Items
-  //               className="min-h-0 overflow-y-auto rounded-12 border border-grey-100 bg-grey-0 py-1 shadow-lg"
-  //             >
-  //               <Menu.Item
-  //
-  //               >
-  //                 <div
-  //                   className="cursor-pointer px-4 py-2.5 text-14 leading-5"
-  //                 >
-  //                   item
-  //                 </div>
-  //               </Menu.Item>
-  //             </Menu.Items>
-  //           </Transition>
-  //         </div>
-  //       </FloatingPortal>
-  //     </Menu>
-  //   </div>
-  // )
 };
