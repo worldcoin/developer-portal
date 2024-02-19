a01288484b53f25d2ca10993e7c9d8b35faa5ff3
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-02-19 02:47:11 +0100

    fix: redirects on root page

--- web/scenes/Root/page/index.tsx
+++ web/scenes/Root/page/index.tsx
@@ -1,20 +1,45 @@
+import { getAPIServiceGraphqlClient } from "@/api/helpers/graphql";
+import { logger } from "@/lib/logger";
 import { Auth0SessionUser } from "@/lib/types";
+import { urls } from "@/lib/urls";
 import { getSession } from "@auth0/nextjs-auth0";
 import { redirect } from "next/navigation";
 
+import {
+  FetchMembershipsQuery,
+  getSdk,
+} from "./graphql/server/fetch-memberships.generated";
+
 export const RootPage = async () => {
   const session = await getSession();
-  const user = session?.user as Auth0SessionUser["user"];
+  const auth0User = session?.user as Auth0SessionUser["user"];
 
-  if (!user) {
-    return redirect("/login");
+  if (!auth0User) {
+    return redirect(urls.login());
   }
 
-  const team_id = user.hasura?.memberships[0].team?.id;
+  const client = await getAPIServiceGraphqlClient();
+  let membership: FetchMembershipsQuery["membership"] | null = null;
 
-  if (!team_id) {
-    return redirect("/profile");
+  try {
+    const data = await getSdk(client).FetchMemberships({
+      userId: auth0User?.hasura.id,
+    });
+
+    membership = data.membership;
+  } catch (error) {
+    logger.error(
+      "Error fetching memberships on root page with active session",
+      { error },
+    );
+
+    return redirect(urls.logout());
   }
 
-  return redirect(`/teams/${team_id}/apps`);
+  if (!membership || membership.length === 0) {
+    return redirect(urls.createTeam());
+  }
+
+  const team_id = membership[0].team_id;
+  return redirect(urls.apps({ team_id }));
 };
