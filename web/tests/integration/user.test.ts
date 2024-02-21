1421c513632019f08dbf6de72ad9eb6b3609c31e
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-02-21 13:43:55 -0800

    add test case

--- web/tests/integration/user.test.ts
+++ web/tests/integration/user.test.ts
@@ -1,11 +1,11 @@
+import { Role_Enum } from "@/graphql/graphql";
+import { gql } from "@apollo/client";
 import {
+  integrationDBExecuteQuery,
   integrationDBSetup,
   integrationDBTearDown,
-  integrationDBExecuteQuery,
 } from "./setup";
 import { getAPIUserClient } from "./test-utils";
-import { gql } from "@apollo/client";
-import { Role_Enum } from "@/graphql/graphql";
 
 // TODO: Consider moving this to a generalized jest environment
 beforeEach(integrationDBSetup);
@@ -84,5 +84,63 @@ describe("user role", () => {
         `SELECT name FROM "public"."user" WHERE id = '${anotherUserFromTeam0?.user_id}'`,
       )) as { rows: Array<{ name: string }> };
     expect(anotherUserFromTeam1AfterUpdate[0].name).not.toBe("new name");
+  });
+
+  test("can't select user from a team you are not a part of", async () => {
+    const { rows: teams } = (await integrationDBExecuteQuery(
+      `SELECT id, name FROM "public"."team"`,
+    )) as { rows: Array<{ id: string; name: string }> };
+
+    const { rows: teamMemberships } = (await integrationDBExecuteQuery(
+      `SELECT id, user_id, team_id, role FROM "public"."membership"`,
+    )) as {
+      rows: Array<{
+        id: string;
+        user_id: string;
+        team_id: string;
+        role: Role_Enum;
+      }>;
+    };
+
+    const query = gql`
+      query FetchUser($id: String!) {
+        user(where: { id: { _eq: $id } }) {
+          id
+          email
+          name
+          auth0Id
+          memberships {
+            id
+            team {
+              id
+              name
+            }
+            role
+          }
+        }
+      }
+    `;
+
+    const userFromTeam0 = teamMemberships.find(
+      (membership) => membership.team_id === teams[0].id,
+    );
+
+    const userFromTeam1 = teamMemberships.find(
+      (membership) => membership.team_id === teams[1].id,
+    );
+
+    const client = await getAPIUserClient({
+      team_id: teams[0].id,
+      user_id: userFromTeam0?.user_id,
+    });
+
+    const response = await client.query({
+      query,
+      variables: {
+        id: userFromTeam1?.user_id,
+      },
+    });
+
+    expect(response.data.user).toEqual([]);
   });
 });
