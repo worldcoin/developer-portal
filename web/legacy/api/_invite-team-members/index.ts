fea5dd0e5d6da5bbde36d315bfd0aada14e98461
Igor Osipov <89008845+igorosip0v@users.noreply.github.com>
2024-02-22 23:35:48 +0100

    fix: duplicated invites after resend (#636)
    * fix: duplicated invites after resend
    
    * fix: mixed sending create/resend invites via modal
    
    fixes the case when user sends already sent and new invites using invite modal

--- web/legacy/api/_invite-team-members/index.ts
+++ web/legacy/api/_invite-team-members/index.ts
@@ -1,17 +1,28 @@
-import { getSdk as getCreateInvitesSdk } from "@/legacy/api/_invite-team-members/graphql/createInvite.generated";
 import { errorHasuraQuery, errorNotAllowed } from "@/legacy/backend/errors";
 import { getAPIServiceGraphqlClient } from "@/legacy/backend/graphql";
 import { protectInternalEndpoint } from "@/legacy/backend/utils";
 import { logger } from "@/legacy/lib/logger";
 import { sendEmail } from "@/legacy/lib/send-email";
+import dayjs from "dayjs";
+import createDOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSdk as getFetchInvitesSdk } from "./graphql/fetchInvites.generated";
+
+import {
+  CreateInvitesMutation,
+  getSdk as getCreateInvitesSdk,
+} from "@/legacy/api/_invite-team-members/graphql/createInvite.generated";
+
 import {
   GetUserAndTeamMembershipsQuery,
   getSdk as getUserAndTeamMembershipsSdk,
 } from "./graphql/getUserAndTeamMemberships.generated";
 
-import createDOMPurify from "dompurify";
-import { JSDOM } from "jsdom";
+import {
+  UpdateInvitesExpirationMutation,
+  getSdk,
+} from "./graphql/updateInvitesExpiration.generated";
 
 export const handleInvite = async (
   req: NextApiRequest,
@@ -106,21 +117,76 @@ export const handleInvite = async (
     });
   }
 
-  const createInvitesRes = await getCreateInvitesSdk(client).CreateInvites({
-    objects: emails.map((email: string) => ({
-      email,
-      team_id: teamId,
-    })),
+  const fetchInvitesResult = await getFetchInvitesSdk(client).FetchInvites({
+    emails,
   });
 
-  if (
-    !createInvitesRes.invites?.returning ||
-    createInvitesRes.invites?.returning?.length !== emails.length
-  ) {
+  const invitesToUpdate = fetchInvitesResult.invite.filter((invite) => {
+    return emails.includes(invite.email);
+  });
+
+  const emailsToCreate = emails.filter((email: string) => {
+    return !invitesToUpdate.some((invite) => invite.email === email);
+  });
+
+  let updatedInvites: NonNullable<
+    UpdateInvitesExpirationMutation["invites"]
+  >["returning"] = [];
+
+  if (invitesToUpdate.length > 0) {
+    const updateExpirationResult = await getSdk(client).UpdateInvitesExpiration(
+      {
+        ids: invitesToUpdate.map((invite) => invite.id),
+        expires_at: dayjs().add(7, "days").toISOString(),
+      },
+    );
+
+    if (!updateExpirationResult.invites?.returning) {
+      return errorHasuraQuery({
+        res,
+        req,
+        detail: "Some invites could not be updated.",
+        code: "invalid_emails",
+      });
+    }
+
+    updatedInvites = updateExpirationResult.invites.returning;
+  }
+
+  let createdInvites: NonNullable<
+    CreateInvitesMutation["invites"]
+  >["returning"] = [];
+
+  if (emailsToCreate.length > 0) {
+    const createInvitesRes = await getCreateInvitesSdk(client).CreateInvites({
+      objects: emailsToCreate.map((email: string) => ({
+        email,
+        team_id: teamId,
+      })),
+    });
+
+    if (
+      !createInvitesRes.invites?.returning ||
+      createInvitesRes.invites?.returning?.length !== emailsToCreate.length
+    ) {
+      return errorHasuraQuery({
+        res,
+        req,
+        detail: "Some emails could not be invited.",
+        code: "invalid_emails",
+      });
+    }
+
+    createdInvites = createInvitesRes.invites.returning;
+  }
+
+  const invites = [...updatedInvites, ...createdInvites];
+
+  if (!invites || invites.length === 0) {
     return errorHasuraQuery({
       res,
       req,
-      detail: "Some emails could not be invited.",
+      detail: "No invites to send.",
       code: "invalid_emails",
     });
   }
@@ -134,7 +200,8 @@ export const handleInvite = async (
   });
 
   const promises = [];
-  for (const invite of createInvitesRes.invites?.returning) {
+
+  for (const invite of invites) {
     const link = `${process.env.NEXT_PUBLIC_APP_URL}/join?invite_id=${invite.id}`;
 
     const inviter = DOMPurify.sanitize(
@@ -142,7 +209,6 @@ export const handleInvite = async (
     );
 
     const team = DOMPurify.sanitize(invitingUser.team?.name || "their team");
-
     const to = DOMPurify.sanitize(invite.email);
 
     promises.push(
