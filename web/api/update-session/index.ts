de47db51b48bd46df88715e7667fc3049ee7b7e8
Igor Osipov <89008845+igorosip0v@users.noreply.github.com>
2024-02-23 20:08:21 +0100

    fix: delete team (#632)
    * fix: delete team
    
    * fix: error text
    
    * feat: protect update session endpoint

---
+++ web/api/update-session/index.ts
@@ -0,0 +1,32 @@
+import {
+  getSession,
+  updateSession,
+  withApiAuthRequired,
+} from "@auth0/nextjs-auth0";
+
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = withApiAuthRequired(async (req: NextRequest) => {
+  const res = NextResponse.json({ success: true });
+  const body = await req.json();
+  const user = body.user as Record<string, any>;
+  let session = await getSession(req, res);
+
+  if (!session) {
+    return NextResponse.json({ success: false }, { status: 500 });
+  }
+
+  const updatedSession = {
+    ...session,
+    user: {
+      ...session.user,
+      hasura: {
+        ...user,
+      },
+    },
+  };
+
+  session = updatedSession;
+  await updateSession(req, res, session);
+  return res;
+});
---
+++ web/app/api/update-session/route.ts
@@ -0,0 +1,1 @@
+export { POST } from "@/api/update-session";
