d9becc22463ad518eee8c8c912d7ef2b2f851a8b
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-02-29 11:05:01 -0800

    fix docs links
    Docs got updated so a bunch of the Urls are now broken

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/SignInWithWorldId/page/index.tsx
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/SignInWithWorldId/page/index.tsx
@@ -52,7 +52,7 @@ export const SignInWithWorldIdPage = async (
             </Typography>
           </DecoratedButton>
           <DecoratedButton
-            href="https://docs.worldcoin.org/id/sign-in"
+            href="https://docs.worldcoin.org/sign-in"
             variant="secondary"
             className="flex h-12 items-center justify-center py-5"
           >
