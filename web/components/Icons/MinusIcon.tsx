b9de7ff6a366ed15a1cdc570d8a4ce06541992f5
Michael Furutin <michael.furutin@toolsforhumanity.com>
2024-03-21 19:08:54 +0300

    refactor: profile teams screen on mobile

--- web/components/Icons/MinusIcon.tsx
+++ web/components/Icons/MinusIcon.tsx
@@ -3,23 +3,20 @@ import { ComponentProps } from "react";
 export const MinusIcon = (props: ComponentProps<"svg">) => {
   return (
     <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g id="minus">
-        <path
-          id="Vector 2437"
-          d="M15 10L5 10"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
+      <path
+        d="M18 12L6 12"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 };
--- web/components/Icons/PlusIcon.tsx
+++ web/components/Icons/PlusIcon.tsx
@@ -3,22 +3,20 @@ import { ComponentProps } from "react";
 export const PlusIcon = (props: ComponentProps<"svg">) => {
   return (
     <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g id="plus">
-        <path
-          id="Union"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M6.625 1C6.625 0.654822 6.34518 0.375 6 0.375C5.65482 0.375 5.375 0.654822 5.375 1V5.375H1C0.654822 5.375 0.375 5.65482 0.375 6C0.375 6.34518 0.654822 6.625 1 6.625H5.375V11C5.375 11.3452 5.65482 11.625 6 11.625C6.34518 11.625 6.625 11.3452 6.625 11V6.625H11C11.3452 6.625 11.625 6.34518 11.625 6C11.625 5.65482 11.3452 5.375 11 5.375H6.625V1Z"
-          fill="currentColor"
-        />
-      </g>
+      <path
+        d="M12 6V18M18 12L6 12"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 };
