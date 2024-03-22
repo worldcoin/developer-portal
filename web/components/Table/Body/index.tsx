f21ab91c0374389849f586f4c95ca7ea3598cbe4
Michael Furutin <michael.furutin@toolsforhumanity.com>
2024-03-22 15:48:59 +0300

    fix

--- web/components/Table/Body/index.tsx
+++ web/components/Table/Body/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const Body = (props: { children: ReactNode }) => {
   return (
-    <div className="tbody divide-y divide-grey-100 overflow-y-scroll bg-white">
+    <div className="divide-y divide-grey-100 overflow-y-scroll bg-white">
       {props.children}
     </div>
   );
