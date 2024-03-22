c58a111bc89354f3f9fdd47e8276e255963d4cf8
Michael Furutin <michael.furutin@toolsforhumanity.com>
2024-03-22 15:57:17 +0300

    formatting

--- web/components/Table/index.tsx
+++ web/components/Table/index.tsx
@@ -8,9 +8,7 @@ type TableProps = {
 export const Table: React.FC<TableProps> = ({ children, footer }) => {
   return (
     <div className="size-full">
-      <div className="min-w-full divide-y divide-grey-100">
-        {children}
-      </div>
+      <div className="min-w-full divide-y divide-grey-100">{children}</div>
       {footer}
     </div>
   );
