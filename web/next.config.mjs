52a66af5577c03cfb2946256dd1681ec61e70fff
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-03-26 10:02:43 -0700

    HSTS Header

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -42,6 +42,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
     ];
   },
 
