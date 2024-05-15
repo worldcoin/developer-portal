dfc1720043abb2d78fb43c5d4e67a62aee5cbfc8
Dmitrii Bormotov <dmitrii.bormotov@ottofeller.com>
2024-05-15 17:13:33 +0400

    WID-893 Add custom test instance Share authentication between test runs

---
+++ web/tests/e2e/helpers/test.ts
@@ -0,0 +1,51 @@
+import { test as base, expect } from "@playwright/test";
+import fs from "fs";
+import path from "path";
+
+export const test = base.extend<{}, { workerStorageState: string }>({
+  storageState: ({ workerStorageState }, use) => use(workerStorageState),
+
+  // Authenticate once per worker with a worker-scoped fixture
+  workerStorageState: [
+    async ({ browser }, use) => {
+      if (!process.env.TEST_USER_EMAIL || !process.env.TEST_USER_PASSWORD) {
+        throw new Error("Missing test user credentials!");
+      }
+
+      // Use parallelIndex as a unique identifier for each worker
+      const id = test.info().parallelIndex;
+      const fileName = path.resolve(
+        test.info().project.outputDir,
+        `.auth/${id}.json`,
+      );
+
+      if (fs.existsSync(fileName)) {
+        // Reuse existing authentication state if any
+        await use(fileName);
+        return;
+      }
+
+      // Authentication steps via Auth0 UI
+      const page = await browser.newPage({ storageState: undefined });
+
+      await page.goto("/login");
+      await page.locator("[data-testid='button-log-in']").click();
+
+      await page.locator("[name='email']").fill(process.env.TEST_USER_EMAIL);
+      await page
+        .locator("[name='password']")
+        .fill(process.env.TEST_USER_PASSWORD);
+      await page.locator("[name='submit']").click();
+
+      await expect(page.getByText("Build your first project")).toBeVisible();
+
+      // Save storage state of the authenticated browser
+      await page.context().storageState({ path: fileName });
+      await page.close();
+      await use(fileName);
+    },
+    { scope: "worker" },
+  ],
+});
+
+export { expect } from "@playwright/test";
