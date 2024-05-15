bcfe437ea0345e83025a93fceada649936403614
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-05-15 14:26:33 -0700

    Improve Transaction Table UI (#749)
    * transaction table
    
    * Update index.tsx
    
    * sort descending

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/Transactions/page/TransactionsTable/TransactionRow/Token/index.tsx
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/Transactions/page/TransactionsTable/TransactionRow/Token/index.tsx
@@ -6,7 +6,7 @@ export const TokenIcon = memo(function TokenIcon(props: { token: string }) {
   const { token } = props;
 
   if (token === "USDCE") {
-    return <USDCIcon className="size-8" />;
+    return <USDCIcon className="size-8 " />;
   } else if (token === "WLD") {
     return <WLDIcon className="size-8" />;
   }
