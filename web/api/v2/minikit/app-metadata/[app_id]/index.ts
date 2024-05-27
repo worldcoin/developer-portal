8cc66ca2bdfc4c79c825cc876be475fd4ad7f610
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-05-27 18:08:30 +0200

    feat: Developer mode endpoint
    Create endpoint to let users query app-metadata if the api key is defined

---
+++ web/api/v2/minikit/app-metadata/[app_id]/index.ts
@@ -0,0 +1,124 @@
+import { errorResponse } from "@/api/helpers/errors";
+import { getAPIServiceGraphqlClient } from "@/api/helpers/graphql";
+import { verifyHashedSecret } from "@/api/helpers/utils";
+import { NextRequest, NextResponse } from "next/server";
+import { getSdk as fetchApiKeySdk } from "./graphql/fetch-api-key.generated";
+import { getSdk as getAppMetadataSdk } from "./graphql/get-app-metadata.generated";
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { app_id: string } },
+) => {
+  const apiKey = req.headers.get("authorization")?.split(" ")[1];
+  const appId = params.app_id;
+
+  if (!apiKey) {
+    return errorResponse({
+      statusCode: 401,
+      code: "unauthorized",
+      detail: "API key is required.",
+      attribute: "api_key",
+      req,
+    });
+  }
+
+  if (!appId) {
+    return errorResponse({
+      statusCode: 400,
+      code: "invalid_request",
+      detail: "App ID is required.",
+      attribute: "app_id",
+      req,
+    });
+  }
+
+  const keyValue = apiKey.replace(/^api_/, "");
+  const base64ApiKey = Buffer.from(keyValue, "base64").toString("utf8");
+  const [id, secret] = base64ApiKey.split(":");
+  const serviceClient = await getAPIServiceGraphqlClient();
+
+  const { api_key_by_pk } = await fetchApiKeySdk(serviceClient).FetchAPIKey({
+    id,
+    appId: appId,
+  });
+
+  if (!api_key_by_pk) {
+    return errorResponse({
+      statusCode: 404,
+      code: "not_found",
+      detail: "API key not found.",
+      attribute: "api_key",
+      req,
+    });
+  }
+
+  if (!api_key_by_pk.is_active) {
+    return errorResponse({
+      statusCode: 400,
+      code: "api_key_inactive",
+      detail: "API key is inactive.",
+      attribute: "api_key",
+      req,
+    });
+  }
+
+  if (!api_key_by_pk.team.apps.some((a) => a.id === appId)) {
+    return errorResponse({
+      statusCode: 403,
+      code: "invalid_app",
+      detail: "API key is not valid for this app.",
+      attribute: "api_key",
+      req,
+    });
+  }
+
+  const isAPIKeyValid = verifyHashedSecret(
+    api_key_by_pk.id,
+    secret,
+    api_key_by_pk.api_key,
+  );
+
+  if (!isAPIKeyValid) {
+    return errorResponse({
+      statusCode: 403,
+      code: "invalid_api_key",
+      detail: "API key is not valid.",
+      attribute: "api_key",
+      req,
+    });
+  }
+
+  const client = await getAPIServiceGraphqlClient();
+
+  // Return the metadata
+  const { app_metadata } = await getAppMetadataSdk(client).GetAppMetadata({
+    app_id: appId,
+  });
+
+  if (!app_metadata || app_metadata.length === 0) {
+    return NextResponse.json({ error: "App not found" }, { status: 404 });
+  }
+  console.log(app_metadata[0].app);
+  const { app, ...appMetadataReturned } = app_metadata[0];
+  const description = JSON.parse(appMetadataReturned.description);
+
+  const dataToReturn = {
+    ...appMetadataReturned,
+    description: {
+      overview: description.description_overview,
+      how_it_works: description.description_how_it_works,
+      how_to_connect: description.description_connect,
+    },
+    logo_img_url: null,
+    hero_image_url: null,
+    showcase_img_urls: null,
+    team_name: app.team.name,
+  };
+
+  return NextResponse.json(
+    { ...dataToReturn },
+    {
+      status: 200,
+    },
+  );
+};
---
+++ web/tests/api/v2/minikit-app-metadata.test.ts
@@ -0,0 +1,227 @@
+import { GET } from "@/api/v2/minikit/app-metadata/[app_id]";
+import { generateHashedSecret } from "@/legacy/backend/utils";
+import { NextRequest } from "next/server";
+
+// #region Mocks
+const FetchAPIKey = jest.fn();
+const GetAppMetadata = jest.fn();
+
+jest.mock("../../../lib/logger", () => ({
+  logger: {
+    error: jest.fn(),
+    warn: jest.fn(),
+  },
+}));
+
+jest.mock(
+  "../../../api/v2/minikit/app-metadata/[app_id]/graphql/fetch-api-key.generated",
+  () => ({
+    getSdk: () => ({
+      FetchAPIKey,
+    }),
+  }),
+);
+
+jest.mock(
+  "../../../api/v2/minikit/app-metadata/[app_id]/graphql/get-app-metadata.generated",
+  () => ({
+    getSdk: () => ({
+      GetAppMetadata,
+    }),
+  }),
+);
+
+// #endregion
+
+const createMockRequest = (params: {
+  url: URL | RequestInfo;
+  api_key: string;
+}) => {
+  const { url, api_key } = params;
+  return new NextRequest(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${api_key}`,
+    },
+  });
+};
+
+const validApiKeyId = "key_667f5fbd4ad943622b4b2d3eb258f89c";
+const testHashedSecret = generateHashedSecret(validApiKeyId);
+
+const apiKeyValue = Buffer.from(`${validApiKeyId}:${testHashedSecret.secret}`)
+  .toString("base64")
+  .replace(/=/g, "");
+
+const validApiKeyResponse = {
+  api_key_by_pk: {
+    id: validApiKeyId,
+    api_key: testHashedSecret.hashed_secret,
+    is_active: true,
+    team: {
+      id: "team_dd2ecd36c6c45f645e8e5d9a31abdee1",
+      apps: [{ id: "app_staging_9cdd0a714aec9ed17dca660bc9ffe72a" }],
+    },
+  },
+};
+
+const validAppMetadataResponse = [
+  {
+    name: "test",
+    app_id: "app_staging_9cdd0a714aec9ed17dca660bc9ffe72a",
+    logo_img_url: "",
+    showcase_img_urls: null,
+    hero_image_url: "",
+    world_app_description: "Random Values",
+    world_app_button_text: "Use Integration",
+    whitelisted_addresses: null,
+    app_mode: "external",
+    description:
+      '{"description_overview":"Random values","description_how_it_works":"Random values","description_connect":"test"}',
+    category: "Social",
+    integration_url: "https://github.com",
+    app_website_url: "",
+    source_code_url: "",
+    app: { team: { name: "test" } },
+  },
+];
+
+const validAppId = validApiKeyResponse.api_key_by_pk.team.apps[0].id;
+const validApiKey = `api_${apiKeyValue}`;
+
+// #endregion
+
+const app_metadata = {
+  name: "test",
+  app_id: "app_staging_9cdd0a714aec9ed17dca660bc9ffe72a",
+  logo_img_url: null,
+  showcase_img_urls: null,
+  hero_image_url: null,
+  world_app_description: "Random Values",
+  world_app_button_text: "Use Integration",
+  whitelisted_addresses: null,
+  app_mode: "external",
+  description: {
+    overview: "Random values",
+    how_it_works: "Random values",
+    how_to_connect: "test",
+  },
+  category: "Social",
+  integration_url: "https://github.com",
+  app_website_url: "",
+  source_code_url: "",
+  team_name: "test",
+};
+
+const getUrl = (app_id: string) =>
+  new URL(`/api/v2/minikit/app-metadata/${app_id}`, "http://localhost:3000");
+
+// #region Success cases tests
+describe("/api/v2/minikit/app-metadata/[app_id] [success cases]", () => {
+  it("can fetch a transaction", async () => {
+    const mockReq = createMockRequest({
+      url: getUrl(validAppId),
+      api_key: validApiKey,
+    });
+
+    FetchAPIKey.mockResolvedValue(validApiKeyResponse);
+    GetAppMetadata.mockResolvedValue({
+      app_metadata: validAppMetadataResponse,
+    });
+
+    const res = await GET(mockReq, { params: { app_id: validAppId } });
+    expect(res.status).toBe(200);
+    const body = await res.json();
+
+    expect(body).toEqual(app_metadata);
+  });
+});
+// #endregion
+
+// #region Error cases tests
+describe("/api/v2/minikit/transaction/transaction_id [error cases]", () => {
+  it("returns 401 if no api key is provided", async () => {
+    const mockReq = createMockRequest({
+      url: getUrl(validAppId),
+      api_key: "",
+    });
+
+    FetchAPIKey.mockResolvedValue(validApiKeyResponse);
+
+    const res = await GET(mockReq, { params: { app_id: validAppId } });
+    expect(res.status).toBe(401);
+  });
+
+  it("returns 404 if api key not exists", async () => {
+    const mockReq = createMockRequest({
+      url: getUrl(validAppId),
+      api_key: (Math.random() * 10 ** 10).toFixed(),
+    });
+
+    FetchAPIKey.mockResolvedValue({});
+
+    const res = await GET(mockReq, { params: { app_id: validAppId } });
+    expect(res.status).toBe(404);
+  });
+
+  it("returns 403 if token is invalid or old", async () => {
+    const testHashedSecretLocal = generateHashedSecret("app_123");
+
+    const apiKeyValueLocal = Buffer.from(
+      `${"app_123"}:${testHashedSecretLocal.secret}`,
+    )
+      .toString("base64")
+      .replace(/=/g, "");
+
+    const mockReq = createMockRequest({
+      url: getUrl(validAppId),
+      api_key: `api_${apiKeyValueLocal}`,
+    });
+
+    FetchAPIKey.mockResolvedValue(validApiKeyResponse);
+
+    const res = await GET(mockReq, { params: { app_id: validAppId } });
+    expect(res.status).toBe(403);
+  });
+
+  it("returns 403 if api key is not valid for the app", async () => {
+    const mockReq = createMockRequest({
+      url: getUrl("app_1234"),
+      api_key: validApiKey,
+    });
+
+    FetchAPIKey.mockResolvedValue(validApiKeyResponse);
+
+    const res = await GET(mockReq, { params: { app_id: "app_1234" } });
+    expect(res.status).toBe(403);
+  });
+
+  it("returns 400 if api key is inactive", async () => {
+    const mockReq = createMockRequest({
+      url: getUrl(validAppId),
+      api_key: validApiKey,
+    });
+
+    FetchAPIKey.mockResolvedValue({
+      ...validApiKeyResponse,
+      api_key_by_pk: { ...validApiKeyResponse.api_key_by_pk, is_active: false },
+    });
+
+    const res = await GET(mockReq, { params: { app_id: validAppId } });
+    expect(res.status).toBe(400);
+  });
+
+  it("returns 400 if appId is empty", async () => {
+    const mockReq = createMockRequest({
+      url: getUrl(""),
+      api_key: validApiKey,
+    });
+
+    FetchAPIKey.mockResolvedValue({});
+
+    const res = await GET(mockReq, { params: { app_id: "" } });
+    expect(res.status).toBe(400);
+  });
+});
+// #endregion
