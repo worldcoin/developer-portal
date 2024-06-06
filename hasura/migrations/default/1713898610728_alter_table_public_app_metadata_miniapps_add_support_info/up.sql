92e385af33c967a35d6e4c4b0a33aeea4d2d5a3a
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-06-05 17:26:59 -0700

    fix deploy

--- hasura/migrations/default/1713898610728_alter_table_public_app_metadata_miniapps_add_support_info/up.sql
+++ hasura/migrations/default/1713898610728_alter_table_public_app_metadata_miniapps_add_support_info/up.sql
@@ -1,5 +1,5 @@
 
-alter table "public"."app_metadata" add column "support_email" varchar NOT NULL DEFAULT '',
+alter table "public"."app_metadata" add column "support_email" varchar not null default '';
 
 alter table "public"."app_metadata" add column "supported_countries" text[]
  null;
