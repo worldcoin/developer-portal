4696ea9bff20e9a2540105bd2f42649c951d7310
Dmitrii Bormotov <dmitrii.bormotov@ottofeller.com>
2024-06-06 16:53:00 +0400

    WID-896 Add Create an App test

--- web/tests/e2e/specs/app.spec.ts
+++ web/tests/e2e/specs/app.spec.ts
@@ -2,9 +2,23 @@ import { expect, test } from "@e2e/helpers";
 
 test.describe("App", () => {
   test("Create an App", async ({ page }) => {
-    await page.goto("/create-team");
+    const appName = "World Test!";
+    await page.goto("/");
+    await expect(page.getByText("Build your first project")).toBeVisible();
+    await page.click("[data-testid='button-create-an-app']");
+
+    await expect(page.getByTestId("radio-build-staging")).toBeChecked();
+    await expect(page.getByTestId("radio-verification-cloud")).toBeChecked();
+    await expect(page.getByTestId("button-create-app")).toBeDisabled();
+
+    await page.fill("[data-testid='input-app-name']", appName);
+    await expect(page.getByTestId("button-create-app")).toBeEnabled();
+    await page.getByTestId("button-create-app").click();
+
     await expect(
-      page.locator("h1", { hasText: "Create a new team" }),
+      page.getByText("Create your first incognito action"),
     ).toBeVisible();
+    await expect(page.getByTestId("title-app-1")).toHaveText(appName);
+    await expect(page.getByTestId("title-app-1")).toBeVisible();
   });
 });
