3731599939be9b979a1bbc49c384125b86713e3e
Dmitrii Bormotov <dmitrii.bormotov@ottofeller.com>
2024-06-06 16:51:36 +0400

    WID-896 Add global setup and teardown

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 
 const config: PlaywrightTestConfig = {
   timeout: 60000,
+  globalSetup: require.resolve('./tests/e2e/global-setup'),
+  globalTeardown: require.resolve('./tests/e2e/global-teardown'),
   use: {
     baseURL: process.env.NEXT_PUBLIC_APP_URL,
     browserName: "chromium",
@@ -16,7 +18,7 @@ const config: PlaywrightTestConfig = {
   projects: [
     {
       name: "dev-portal-stage",
-      testDir: "tests/e2e",
+      testDir: "tests/e2e/specs",
       testMatch: ["tests/e2e/specs/**/*.spec.ts"],
     },
   ],
