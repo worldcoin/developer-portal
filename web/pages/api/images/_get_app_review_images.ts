d55bda45ac473bec61ee2e2e580b69e00c6acfbf
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-06-24 14:57:57 -0700

    remove old routes

--- web/pages/api/images/_get_app_review_images.ts
+++
@@ -1,3 +0,0 @@
-// NOTE: use src/api when you need to make gql request using codegen
-import { handleGetAppReviewImages } from "@/legacy/api/images/get_app_review_images";
-export default handleGetAppReviewImages;
--- web/pages/api/images/_verify_app.ts
+++
@@ -1,4 +0,0 @@
-// NOTE: use src/api when you need to make gql request using codegen
-
-import { handleVerifyApp } from "@/legacy/api/images/verify_app";
-export default handleVerifyApp;
