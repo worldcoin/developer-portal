ece8fa8a576ddc5d0239a219ee3110649df4c23c
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-06-26 12:56:38 -0700

    remove sign up and health (#800)

--- web/pages/api/health.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(
-  _request: NextApiRequest,
-  response: NextApiResponse,
-) {
-  response.statusCode = 200;
-  response.end("OK");
-}
