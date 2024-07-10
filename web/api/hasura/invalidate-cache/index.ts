c8c053d701b35506250bb0cb10bf91c98a7dbbce
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-07-10 09:07:59 +0200

    cache invalidation (#837)

---
+++ web/api/hasura/invalidate-cache/index.ts
@@ -0,0 +1,68 @@
+import { errorHasuraQuery } from "@/api/helpers/errors";
+import { protectInternalEndpoint } from "@/api/helpers/utils";
+import { logger } from "@/lib/logger";
+import {
+  CloudFrontClient,
+  CreateInvalidationCommand,
+} from "@aws-sdk/client-cloudfront";
+import { NextRequest, NextResponse } from "next/server";
+
+// This function serves to invalidate the cache after making changes to app store directory
+export const POST = async (req: NextRequest) => {
+  if (
+    !process.env.ASSETS_S3_REGION ||
+    !process.env.CLOUDFRONT_DISTRIBUTION_ID
+  ) {
+    logger.error("AWS config is not set.");
+    return errorHasuraQuery({
+      req,
+      detail: "AWS config is not set.",
+      code: "invalid_config",
+    });
+  }
+
+  if (!protectInternalEndpoint(req)) {
+    return errorHasuraQuery({
+      req,
+      detail: "Internal endpoint",
+      code: "internal_endpoint",
+    });
+  }
+
+  const body = await req.json();
+  if (body?.action.name !== "invalidate_cache") {
+    return errorHasuraQuery({
+      req,
+      detail: "Invalid action.",
+      code: "invalid_action",
+    });
+  }
+
+  if (
+    !["reviewer", "admin"].includes(body.session_variables["x-hasura-role"])
+  ) {
+    logger.error("Unauthorized access."),
+      { role: body.session_variables["x-hasura-role"] };
+    return errorHasuraQuery({ req });
+  }
+
+  const client = new CloudFrontClient({
+    region: process.env.ASSETS_S3_REGION,
+  });
+
+  const input = {
+    DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
+    InvalidationBatch: {
+      Paths: {
+        Quantity: 1,
+        Items: ["/api/v2/public/apps"],
+      },
+      CallerReference: Date.now().toString(),
+    },
+  };
+
+  const command = new CreateInvalidationCommand(input);
+  await client.send(command);
+
+  return NextResponse.json({ success: true });
+};
