0caaf209789dc7f4a2eca95f3513dad88d3503e6
Dmitrii Bormotov <dmitrii.bormotov@ottofeller.com>
2024-07-11 11:27:28 +0400

    Add playwright-qase-reporter

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -7,13 +7,13 @@ dotenv.config({ path: path.join(__dirname, ".env.test") });
 
 const config: PlaywrightTestConfig = {
   timeout: 60000,
-  globalSetup: require.resolve("./tests/e2e/global-setup"),
-  globalTeardown: require.resolve("./tests/e2e/global-teardown"),
+  globalSetup: require.resolve("tests/e2e/global-setup"),
+  globalTeardown: require.resolve("tests/e2e/global-teardown"),
   workers: 1,
   use: {
     baseURL: process.env.NEXT_PUBLIC_APP_URL,
     browserName: "chromium",
-    screenshot: "off",
+    screenshot: "only-on-failure",
     video: "retain-on-failure",
     trace: "retain-on-failure",
   },
@@ -27,7 +27,23 @@ const config: PlaywrightTestConfig = {
       testMatch: ["tests/e2e/specs/**/*.spec.ts"],
     },
   ],
-  reporter: [["list"], ["html", { open: "never" }]],
+  reporter: [
+    ["list"],
+    ["html", { open: "never" }],
+    [
+      "playwright-qase-reporter",
+      {
+        environment: "dev",
+        testops: {
+          project: "DP",
+          uploadAttachments: true,
+          run: {
+            complete: true,
+          },
+        },
+      },
+    ],
+  ],
   webServer: {
     command: "pnpm dev",
     port: 3000,
