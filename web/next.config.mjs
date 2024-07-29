3de82e2c503a7f20d9d7f81ba033b42c20f33d3c
Paolo <149602456+pdtfh@users.noreply.github.com>
2024-07-29 17:02:13 +0200

    minor fixes

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,10 +1,82 @@
+// @ts-check
+
+const cdnURLObject = new URL(
+  process.env.NEXT_PUBLIC_VERIFIED_IMAGES_CDN_URL ||
+    "https://world-id-assets.com",
+);
+
+const publicAppURL =
+  process.env.NEXT_PUBLIC_APP_URL || "https://developer.worldcoin.org";
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  output: "standalone",
   images: {
-    domains: [
-      "worldcoin.org",
-      new URL(process.env.NEXT_PUBLIC_VERIFIED_IMAGES_CDN_URL || "https://world-id-assets.com").hostname,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: cdnURLObject.hostname,
+      },
+      {
+        protocol: "https",
+        hostname: `${process.env.ASSETS_S3_BUCKET_NAME}.s3.${process.env.ASSETS_S3_REGION}.amazonaws.com`,
+        pathname: `/unverified/**`,
+      },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: publicAppURL,
+          },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/privacy-statement",
+        permanent: false,
+        destination:
+          "https://worldcoin.pactsafe.io/rjd5nsvyq.html#contract-9l-r7n2jt",
+      },
+      {
+        source: "/tos",
+        permanent: false,
+        destination:
+          "https://worldcoin.pactsafe.io/rjd5nsvyq.html#contract-b1q9midy9",
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/openid-configuration",
+        destination: "/api/v1/oidc/openid-configuration",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://app.posthog.com/:path*",
+      },
+    ];
   },
 };
 
