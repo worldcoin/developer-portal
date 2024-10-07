8b0470b68e0e792256ec073943d67a5b4961bbd3
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-06 23:48:54 -0700

    cors and open api (#955)

--- web/api/v2/minikit/transaction/[transaction_id]/index.ts
+++ web/api/v2/minikit/transaction/[transaction_id]/index.ts
@@ -1,27 +1,23 @@
 import { errorResponse } from "@/api/helpers/errors";
-import { getAPIServiceGraphqlClient } from "@/api/helpers/graphql";
-import { verifyHashedSecret } from "@/api/helpers/utils";
 import { TransactionTypes } from "@/lib/types";
 import { createSignedFetcher } from "aws-sigv4-fetch";
 import { NextRequest, NextResponse } from "next/server";
-import { getSdk as fetchApiKeySdk } from "../../graphql/fetch-api-key.generated";
+
+function corsHandler(response: NextResponse) {
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization",
+  );
+  return response;
+}
 
 export const GET = async (
   req: NextRequest,
   { params: routeParams }: { params: { transaction_id: string } },
 ) => {
-  const api_key = req.headers.get("authorization")?.split(" ")[1];
   const { searchParams } = new URL(req.url);
-
-  if (!api_key) {
-    return errorResponse({
-      statusCode: 401,
-      code: "unauthorized",
-      detail: "API key is required.",
-      attribute: "api_key",
-      req,
-    });
-  }
 
   const appId = searchParams.get("app_id");
   const type = searchParams.get("type") ?? TransactionTypes.Payment;
@@ -50,61 +46,6 @@ export const GET = async (
   }
 
   const transactionId = routeParams.transaction_id;
-  const keyValue = api_key.replace(/^api_/, "");
-  const base64ApiKey = Buffer.from(keyValue, "base64").toString("utf8");
-  const [id, secret] = base64ApiKey.split(":");
-  const serviceClient = await getAPIServiceGraphqlClient();
-
-  const { api_key_by_pk } = await fetchApiKeySdk(serviceClient).FetchAPIKey({
-    id,
-    appId: appId,
-  });
-
-  if (!api_key_by_pk) {
-    return errorResponse({
-      statusCode: 404,
-      code: "not_found",
-      detail: "API key not found.",
-      attribute: "api_key",
-      req,
-    });
-  }
-
-  if (!api_key_by_pk.is_active) {
-    return errorResponse({
-      statusCode: 400,
-      code: "api_key_inactive",
-      detail: "API key is inactive.",
-      attribute: "api_key",
-      req,
-    });
-  }
-
-  if (!api_key_by_pk.team.apps.some((a) => a.id === appId)) {
-    return errorResponse({
-      statusCode: 403,
-      code: "invalid_app",
-      detail: "API key is not valid for this app.",
-      attribute: "api_key",
-      req,
-    });
-  }
-
-  const isAPIKeyValid = verifyHashedSecret(
-    api_key_by_pk.id,
-    secret,
-    api_key_by_pk.api_key,
-  );
-
-  if (!isAPIKeyValid) {
-    return errorResponse({
-      statusCode: 403,
-      code: "invalid_api_key",
-      detail: "API key is not valid.",
-      attribute: "api_key",
-      req,
-    });
-  }
 
   const signedFetch = createSignedFetcher({
     service: "execute-api",
@@ -158,7 +99,8 @@ export const GET = async (
 
   if (data?.result?.transactions.length !== 0) {
     const transaction = data?.result?.transactions[0];
-    return NextResponse.json(transaction, { status: 200 });
+    const response = NextResponse.json(transaction, { status: 200 });
+    return corsHandler(response);
   } else {
     return errorResponse({
       statusCode: 404,
