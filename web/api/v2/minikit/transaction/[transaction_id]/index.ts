309bcc3cf12dd2e9bab2d33de62c1c75c7d275d6
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-07 00:13:11 -0700

    cors

--- web/api/v2/minikit/transaction/[transaction_id]/index.ts
+++ web/api/v2/minikit/transaction/[transaction_id]/index.ts
@@ -6,10 +6,6 @@ import { NextRequest, NextResponse } from "next/server";
 function corsHandler(response: NextResponse) {
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Allow-Methods", "GET");
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization",
-  );
   return response;
 }
 
