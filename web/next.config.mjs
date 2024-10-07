d4611e091da1468564b8ea3d6bc6f22e309064c2
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-07 11:21:27 -0700

    remove header

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -29,15 +29,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: publicAppURL,
-          },
-        ],
-      },
-      {
         source: "/(.*)",
         headers: [
           {
