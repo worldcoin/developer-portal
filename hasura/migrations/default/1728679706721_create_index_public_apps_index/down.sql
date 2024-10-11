f6804423fd01f318e5bdc3a1de05025321284486
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-11 15:36:12 -0700

    remove index

--- hasura/migrations/default/1728679706721_create_index_public_apps_index/down.sql
+++
@@ -1,1 +0,0 @@
-DROP INDEX IF EXISTS "public"."public_apps_index";
--- hasura/migrations/default/1728679706721_create_index_public_apps_index/up.sql
+++
@@ -1,2 +0,0 @@
-CREATE  INDEX "public_apps_index" on
-  "public"."app_metadata" using btree ("verification_status", "is_reviewer_app_store_approved");
