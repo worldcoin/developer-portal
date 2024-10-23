0d48d174d5147e386370e2baea18d01838477ec7
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-23 12:50:36 -0400

    fix

--- web/tests/e2e/specs/app.spec.ts
+++ web/tests/e2e/specs/app.spec.ts
@@ -20,6 +20,7 @@ test.describe("App", () => {
     await expect(page.getByTestId("button-create-app")).toBeDisabled();
 
     await page.fill("[data-testid='input-app-name']", appName);
+    await page.fill("[data-testid='category-selector']", "Social");
     await expect(page.getByTestId("button-create-app")).toBeEnabled();
     await page.getByTestId("button-create-app").click();
 
