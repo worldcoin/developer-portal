34a007014816b4249256eb3e023b136b6b8bc09f
Michal Struck <michal.o.struck@gmail.com>
2024-10-23 18:12:44 +0200

    reorder stats, hide all time new users, refactor metrics fetch mapping

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/page/server/index.ts
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/page/server/index.ts
@@ -1,12 +1,12 @@
 "use server";
 
 export type AppMetricsData = {
-  impressions: number | null;
-  impressions_7days: number | null;
-  users: number | null;
-  users_7days: number | null;
+  total_impressions: number | null;
+  total_impressions_last_7_days: number | null;
+  total_users: number | null;
+  total_users_last_7_days: number | null;
   unique_users: number | null;
-  unique_users_7days: number | null;
+  unique_users_last_7_days: number | null;
   new_users_last_7_days: number | null;
   appRanking: `${number} / ${number}` | "-- / --";
 };
@@ -37,12 +37,12 @@ export const getAppMetricsData = async (
 
   if (!appMetrics) {
     return {
-      impressions: 0,
-      impressions_7days: 0,
-      users: 0,
-      users_7days: 0,
+      total_impressions: 0,
+      total_impressions_last_7_days: 0,
+      total_users: 0,
+      total_users_last_7_days: 0,
       unique_users: 0,
-      unique_users_7days: 0,
+      unique_users_last_7_days: 0,
       new_users_last_7_days: 0,
       appRanking: "-- / --",
     };
@@ -53,12 +53,12 @@ export const getAppMetricsData = async (
   const appRanking = `${appIndex + 1} / ${totalApps}` as const;
 
   return {
-    impressions: appMetrics.total_impressions,
-    impressions_7days: appMetrics.total_impressions_last_7_days,
-    users: appMetrics.total_users,
-    users_7days: appMetrics.total_users_last_7_days,
+    total_impressions: appMetrics.total_impressions,
+    total_impressions_last_7_days: appMetrics.total_impressions_last_7_days,
+    total_users: appMetrics.total_users,
+    total_users_last_7_days: appMetrics.total_users_last_7_days,
     unique_users: appMetrics.unique_users,
-    unique_users_7days: appMetrics.unique_users_last_7_days,
+    unique_users_last_7_days: appMetrics.unique_users_last_7_days,
     new_users_last_7_days: appMetrics.new_users_last_7_days,
     appRanking,
   };
