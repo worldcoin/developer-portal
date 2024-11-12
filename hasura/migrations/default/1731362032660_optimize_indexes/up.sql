14b2997a3be6c6bfb4e68d1ed649e2ead009ffe5
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-11-11 21:23:37 -0800

    Index not unique (#1038)
    * index not unique
    
    * revert
    
    * REMOVE UNIQUE

--- hasura/migrations/default/1731362032660_optimize_indexes/up.sql
+++ hasura/migrations/default/1731362032660_optimize_indexes/up.sql
@@ -1,6 +1,6 @@
 CREATE INDEX "team_id_foreign_key" on "public"."app" using btree ("team_id");
 
-CREATE UNIQUE INDEX "app_metadata_id_locale" on "public"."localisations" using btree ("app_metadata_id", "locale");
+CREATE INDEX "app_metadata_id_locale" on "public"."localisations" using btree ("app_metadata_id", "locale");
 
 DROP INDEX IF EXISTS "public"."unique_verified_app_id";
 
