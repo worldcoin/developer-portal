ad058f9e68abddbcf2180764ca3d8f55088e8c74
Michal Struck <michal.o.struck@gmail.com>
2024-11-27 18:11:32 +0100

    logo error oneliner

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/Configuration/PageComponents/AppTopBar/LogoImageUpload/index.tsx
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/Configuration/PageComponents/AppTopBar/LogoImageUpload/index.tsx
@@ -259,7 +259,7 @@ export const LogoImageUpload = (props: LogoImageUploadProps) => {
         className={clsx(
           "absolute -bottom-2 -right-2 rounded-full border-2 border-grey-200 bg-white p-2 text-grey-500 hover:bg-grey-50",
           { hidden: !editable || viewMode === "verified" },
-          { "bottom-6": isError },
+          { "bottom-7": isError },
         )}
       >
         <EditIcon className="size-3" />
@@ -267,7 +267,7 @@ export const LogoImageUpload = (props: LogoImageUploadProps) => {
       {isError && (
         <Typography
           variant={TYPOGRAPHY.R5}
-          className="left-0 top-20 flex max-w-20 shrink text-red-500"
+          className="left-0 top-20 mt-1 flex w-max shrink text-red-500"
         >
           Logo is required.
         </Typography>
