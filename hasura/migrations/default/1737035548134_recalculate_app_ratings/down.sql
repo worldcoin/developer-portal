7c7f5eb95f407354fe557dc3dc376b3277ca98f5
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-01-16 15:52:57 +0200

    Add migration script

---
+++ hasura/migrations/default/1737035548134_recalculate_app_ratings/down.sql
@@ -0,0 +1,14 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- UPDATE app
+-- SET
+--     rating_sum = (
+--       SELECT COALESCE(SUM(rating), 0)
+--       FROM app_reviews
+--       WHERE app_reviews.app_id = app.id
+--     ),
+--     rating_count = (
+--       SELECT COUNT(*)
+--       FROM app_reviews
+--       WHERE app_reviews.app_id = app.id
+--     );
