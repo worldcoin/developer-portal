31b8064c22adb273590a230c69fe5bf676e779a5
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-01-16 15:40:00 +0200

    Add new app categories (#1135)
    * Add new app categories
    
    * Add AI category as well
    
    * Update some translations after checking google translate
    
    * Move other category to the end
    
    * Add name to id category mapping
    
    * Reorder categories and rename to "AI"

--- web/lib/categories.ts
+++ web/lib/categories.ts
@@ -6,6 +6,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finance",
     productivity: "Productivity",
     other: "Other",
+    tokens: "Tokens",
+    external: "External",
+    earn: "Earn",
+    ai: "AI",
   },
   ca: {
     social: "Social",
@@ -14,6 +18,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finances",
     productivity: "Productivitat",
     other: "Altres",
+    tokens: "Tokens",
+    external: "Extern",
+    earn: "Guanyar",
+    ai: "Intel·ligència Artificial",
   },
   zh_CN: {
     social: "社交",
@@ -22,6 +30,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "金融",
     productivity: "生产力",
     other: "其他",
+    tokens: "代币",
+    external: "外部",
+    earn: "赚",
+    ai: "人工智能",
   },
   fr: {
     social: "Social",
@@ -30,6 +42,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finance",
     productivity: "Productivité",
     other: "Autre",
+    tokens: "Jetons",
+    external: "Externe",
+    earn: "Gagner",
+    ai: "Intelligence Artificielle",
   },
   de: {
     social: "Sozial",
@@ -38,6 +54,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finanzen",
     productivity: "Produktivität",
     other: "Andere",
+    tokens: "Token",
+    external: "Extern",
+    earn: "Verdienen",
+    ai: "Künstliche Intelligenz",
   },
   hi: {
     social: "सामाजिक",
@@ -46,6 +66,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "वित्त",
     productivity: "उत्पादकता",
     other: "अन्य",
+    tokens: "टोकन",
+    external: "बाहरी",
+    earn: "कमाना",
+    ai: "कृत्रिम बुद्धिमत्ता",
   },
   ja: {
     social: "ソーシャル",
@@ -54,6 +78,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "ファイナンス",
     productivity: "生産性",
     other: "その他",
+    tokens: "トークン",
+    external: "外部",
+    earn: "稼ぐ",
+    ai: "人工知能",
   },
   ko: {
     social: "소셜",
@@ -62,6 +90,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "재무",
     productivity: "생산성",
     other: "기타",
+    tokens: "토큰",
+    external: "외부",
+    earn: "벌다",
+    ai: "인공지능",
   },
   pl: {
     social: "Społeczność",
@@ -70,6 +102,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finanse",
     productivity: "Produktywność",
     other: "Inne",
+    tokens: "Tokeny",
+    external: "Zewnętrzny",
+    earn: "Zarabiaj",
+    ai: "Sztuczna inteligencja",
   },
   pt: {
     social: "Social",
@@ -78,6 +114,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finanças",
     productivity: "Produtividade",
     other: "Outros",
+    tokens: "Tokens",
+    external: "Externo",
+    earn: "Ganhar",
+    ai: "IA",
   },
   es: {
     social: "Social",
@@ -86,6 +126,10 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finanzas",
     productivity: "Productividad",
     other: "Otro",
+    tokens: "Tokens",
+    external: "Externo",
+    earn: "Ganar",
+    ai: "IA",
   },
   es_419: {
     social: "Social",
@@ -94,21 +138,36 @@ export const CategoryTranslations: Record<string, Record<string, string>> = {
     finance: "Finanzas",
     productivity: "Productividad",
     other: "Otro",
+    tokens: "Tokens",
+    external: "Externo",
+    earn: "Ganar",
+    ai: "IA",
   },
 };
 
 export const Categories = [
+  { name: "AI", id: "ai" },
   { name: "Social", id: "social" },
   { name: "Gaming", id: "gaming" },
   { name: "Business", id: "business" },
   { name: "Finance", id: "finance" },
-  {
-    name: "Productivity",
-    id: "productivity",
-  },
+  { name: "Productivity", id: "productivity" },
+  { name: "Tokens", id: "tokens" },
+  { name: "Earn", id: "earn" },
   { name: "Other", id: "other" },
+  { name: "External", id: "external" },
 ] as const;
+
 export const CategoryNameIterable = Categories.map((category) => category.name);
+
+export const CategoryNameToId = Categories.reduce(
+  (acc, { name, id }) => {
+    acc[name] = id;
+    return acc;
+  },
+  {} as Record<string, string>,
+);
+
 export const getLocalisedCategory = (name: string, locale: string) => {
   if (Object.keys(CategoryTranslations).indexOf(locale) === -1) {
     console.warn("Missing locale, falling back to default: ", { locale });
@@ -116,7 +175,7 @@ export const getLocalisedCategory = (name: string, locale: string) => {
   }
   const defaultLocale = locale || "en";
   const translation = CategoryTranslations[defaultLocale];
-  const id = name.toLowerCase();
+  const id = CategoryNameToId[name];
   return {
     id: id,
     name: translation?.[id] ?? CategoryTranslations["en"][id],
