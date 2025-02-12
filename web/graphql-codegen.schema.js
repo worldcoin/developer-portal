6d853b18ab831433e3cc16586348b99bfc141a7d
Gabe Cohen <gabe.cohen@toolsforhumanity.com>
2025-02-12 13:58:54 -0800

    tmp

--- web/graphql-codegen.schema.js
+++ web/graphql-codegen.schema.js
@@ -7,7 +7,7 @@ dotenv.config({ path: `.env.local`, override: true });
 module.exports = {
   schema: [
     {
-      ["http://localhost:8080/v1/graphql"]: {
+      [process.env.NEXT_PUBLIC_GRAPHQL_API_URL]: {
         headers: {
           "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET,
         },
