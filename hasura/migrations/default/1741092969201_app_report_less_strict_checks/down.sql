64841b52190bb7e88f3dd78bbdce916aed4aa5c4
Michal Struck <michal.o.struck@gmail.com>
2025-03-04 13:58:47 +0100

    less strict check

---
+++ hasura/migrations/default/1741092969201_app_report_less_strict_checks/down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE app_report
+    DROP CONSTRAINT IF EXISTS purpose_violation_details_check;
+    
+ALTER TABLE app_report
+    ADD CONSTRAINT purpose_violation_details_check CHECK (
+        (purpose != 'ILLEGAL_CONTENT' AND violation IS NOT NULL AND details IS NOT NULL) OR 
+        (purpose = 'ILLEGAL_CONTENT')
+    );
