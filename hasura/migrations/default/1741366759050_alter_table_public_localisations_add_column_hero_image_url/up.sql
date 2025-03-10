08ae22919a5947c71af252eb1ee11f496ba53dc5
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-03-10 11:40:31 +0100

    Add images to localisations

---
+++ hasura/migrations/default/1741366759050_alter_table_public_localisations_add_column_hero_image_url/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."localisations" add column "hero_image_url" varchar
+ not null default ''::character varying;
---
+++ hasura/migrations/default/1741366785683_alter_table_public_localisations_add_column_showcase_img_urls/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."localisations" add column "showcase_img_urls" text[]
+ null;
