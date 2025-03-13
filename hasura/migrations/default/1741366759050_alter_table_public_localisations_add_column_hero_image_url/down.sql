3328a793c85f137a1b01d9f6a3dd545da55007a2
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-03-13 13:31:41 +0100

    Add down migrations

--- hasura/migrations/default/1741366759050_alter_table_public_localisations_add_column_hero_image_url/down.sql
+++ hasura/migrations/default/1741366759050_alter_table_public_localisations_add_column_hero_image_url/down.sql
@@ -2,3 +2,6 @@
 -- Please write an appropriate down migration for the SQL below:
 -- alter table "public"."localisations" add column "hero_image_url" varchar
 --  not null default ''::character varying;
+
+ALTER TABLE localisations
+DROP COLUMN hero_image_url;
--- hasura/migrations/default/1741366785683_alter_table_public_localisations_add_column_showcase_img_urls/down.sql
+++ hasura/migrations/default/1741366785683_alter_table_public_localisations_add_column_showcase_img_urls/down.sql
@@ -2,3 +2,6 @@
 -- Please write an appropriate down migration for the SQL below:
 -- alter table "public"."localisations" add column "showcase_img_urls" text[]
 --  null;
+
+ALTER TABLE localisations
+DROP COLUMN showcase_img_urls;
