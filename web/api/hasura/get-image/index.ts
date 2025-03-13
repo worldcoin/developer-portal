3ab3451fd3ee9469cf8db453a7e07e724ab592b1
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-03-13 14:02:58 +0100

    Fix path for english locale

--- web/api/hasura/get-image/index.ts
+++ web/api/hasura/get-image/index.ts
@@ -102,7 +102,7 @@ export const POST = async (req: NextRequest) => {
     }
 
     const bucketName = process.env.ASSETS_S3_BUCKET_NAME;
-    const objectKey = `unverified/${app_id}${locale ? `/${locale}` : ""}/${image_type}.${
+    const objectKey = `unverified/${app_id}${locale && locale !== "en" ? `/${locale}` : ""}/${image_type}.${
       content_type_ending === "jpeg" ? "jpg" : content_type_ending
     }`;
 
