6e46da37a8f9345ec2d059f5953c72e2b6f3392b
Michał Struck <98230431+michalstruck@users.noreply.github.com>
2025-04-01 15:37:33 +0200

    cleanup old records (#1247)
    * cleanup old records
    
    * refactor logs
    
    * fix tests
    
    * add index on created at
    
    * update logic to optimize db load

---
+++ web/api/_delete-expired-notification-logs/graphql/get-expired-notification-log-ids-batch.generated.ts
@@ -0,0 +1,65 @@
+/* eslint-disable import/no-relative-parent-imports -- auto generated file */
+import * as Types from "@/graphql/graphql";
+
+import { GraphQLClient, RequestOptions } from "graphql-request";
+import gql from "graphql-tag";
+type GraphQLClientRequestHeaders = RequestOptions["requestHeaders"];
+export type GetExpiredNotificationLogIdsBatchQueryVariables = Types.Exact<{
+  beforeDate: Types.Scalars["timestamptz"]["input"];
+}>;
+
+export type GetExpiredNotificationLogIdsBatchQuery = {
+  __typename?: "query_root";
+  notification_log: Array<{ __typename?: "notification_log"; id: string }>;
+};
+
+export const GetExpiredNotificationLogIdsBatchDocument = gql`
+  query GetExpiredNotificationLogIdsBatch($beforeDate: timestamptz!) {
+    notification_log(
+      where: { created_at: { _lte: $beforeDate } }
+      order_by: { created_at: asc }
+      limit: 1000
+    ) {
+      id
+    }
+  }
+`;
+
+export type SdkFunctionWrapper = <T>(
+  action: (requestHeaders?: Record<string, string>) => Promise<T>,
+  operationName: string,
+  operationType?: string,
+  variables?: any,
+) => Promise<T>;
+
+const defaultWrapper: SdkFunctionWrapper = (
+  action,
+  _operationName,
+  _operationType,
+  _variables,
+) => action();
+
+export function getSdk(
+  client: GraphQLClient,
+  withWrapper: SdkFunctionWrapper = defaultWrapper,
+) {
+  return {
+    GetExpiredNotificationLogIdsBatch(
+      variables: GetExpiredNotificationLogIdsBatchQueryVariables,
+      requestHeaders?: GraphQLClientRequestHeaders,
+    ): Promise<GetExpiredNotificationLogIdsBatchQuery> {
+      return withWrapper(
+        (wrappedRequestHeaders) =>
+          client.request<GetExpiredNotificationLogIdsBatchQuery>(
+            GetExpiredNotificationLogIdsBatchDocument,
+            variables,
+            { ...requestHeaders, ...wrappedRequestHeaders },
+          ),
+        "GetExpiredNotificationLogIdsBatch",
+        "query",
+        variables,
+      );
+    },
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
---
+++ web/api/_delete-expired-notification-logs/index.ts
@@ -0,0 +1,46 @@
+import { errorNotAllowed } from "@/api/helpers/errors";
+import { getAPIServiceGraphqlClient } from "@/api/helpers/graphql";
+import { protectInternalEndpoint } from "@/api/helpers/utils";
+import { logger } from "@/lib/logger";
+import { NextRequest, NextResponse } from "next/server";
+import { getSdk as deleteExpiredNotificationLogsSdk } from "./graphql/delete-expired-notification-logs-batch.generated";
+import { getSdk as getExpiredNotificationLogIdsBatchSdk } from "./graphql/get-expired-notification-log-ids-batch.generated";
+
+export const POST = async (req: NextRequest) => {
+  if (!protectInternalEndpoint(req)) {
+    return;
+  }
+
+  if (req.method !== "POST") {
+    return errorNotAllowed(req.method, req);
+  }
+
+  try {
+    const serviceClient = await getAPIServiceGraphqlClient();
+    const fourWeeksAgo = new Date(Date.now() - 4 * 7 * 24 * 60 * 60 * 1000);
+
+    const batch = await getExpiredNotificationLogIdsBatchSdk(
+      serviceClient,
+    ).GetExpiredNotificationLogIdsBatch({
+      beforeDate: fourWeeksAgo.toISOString(),
+    });
+
+    // apart from removing these rows, the query also deletes the corresponding notification_log_wallet_address rows
+    await deleteExpiredNotificationLogsSdk(
+      serviceClient,
+    ).DeleteExpiredNotificationLogs({
+      notificationLogIds: batch.notification_log.map((log) => log.id),
+    });
+  } catch (error) {
+    logger.error("Failed to delete expired notification logs", {
+      error,
+    });
+  }
+
+  return NextResponse.json(
+    {
+      success: true,
+    },
+    { status: 200 },
+  );
+};
