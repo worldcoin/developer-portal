7862254930256376b7e9393248dcb8c38c8c908f
Michal Struck <michal.o.struck@gmail.com>
2025-04-02 16:27:05 +0200

    rem migration

--- hasura/migrations/default/1743603595554_create_index_notification_log_wallet_address_notification_log_id_fkey_index/down.sql
+++
@@ -1,1 +0,0 @@
-DROP INDEX IF EXISTS "public"."notification_log_wallet_address_notification_log_id_fkey_index";
--- hasura/migrations/default/1743603595554_create_index_notification_log_wallet_address_notification_log_id_fkey_index/up.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX "notification_log_wallet_address_notification_log_id_fkey_index" on
-  "public"."notification_log_wallet_address" using btree ("notification_log_id");
