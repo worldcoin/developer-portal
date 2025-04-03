859366414983e98df965782ce80276dca7c9f051
Michal Struck <michal.o.struck@gmail.com>
2025-04-03 15:55:49 +0200

    tests and remove unused var

--- web/api/v2/public/apps/index.ts
+++ web/api/v2/public/apps/index.ts
@@ -132,23 +132,7 @@ export const GET = async (request: NextRequest) => {
       req: request,
     });
   }
-  const apps = [
-    {
-      app_id: "1",
-      category: "Social",
-      app_mode: "mini-app",
-    },
-    {
-      app_id: "2",
-      category: "Social",
-      app_mode: "external",
-    },
-    {
-      app_id: "3",
-      category: "External",
-      app_mode: "external",
-    },
-  ];
+
   if (!parsedParams.show_external) {
     topApps = topApps.filter(
       (app) =>
