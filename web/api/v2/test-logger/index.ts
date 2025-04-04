614909b9607e77e00a2c7dc9290dc29953026661
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-04-04 17:38:53 +0200

    Revert "fix: Fix test-logger endpoint (#1282)"
    This reverts commit aa91e2a159845b8a61c829bec28050ae98e762fa.

--- web/api/v2/test-logger/index.ts
+++ web/api/v2/test-logger/index.ts
@@ -7,22 +7,11 @@ function corsHandler(response: NextResponse) {
   return response;
 }
 
-import * as yup from "yup";
-
-const schema = yup.object({
-  test_param: yup.string(),
-});
-
 export const GET = async (req: NextRequest) => {
-  const params = schema.validateSync(req.nextUrl.searchParams);
-  const test_param = params.test_param;
-
-  if (test_param === "error") {
-    logger.error("test", {
-      error: new Error("test"),
-    });
-    throw new Error("test");
-  }
+  logger.error("test", {
+    error: new Error("test"),
+  });
+  throw new Error("test");
   const response = NextResponse.json({ status: 200 });
   return corsHandler(response);
 };
