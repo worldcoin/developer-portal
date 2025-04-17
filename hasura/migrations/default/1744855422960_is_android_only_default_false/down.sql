8d82ca233c5c1218964442361dd1579858f32752
Michal Struck <michal.o.struck@gmail.com>
2025-04-16 19:07:01 -0700

    default to false on flag

---
+++ hasura/migrations/default/1744855422960_is_android_only_default_false/down.sql
@@ -0,0 +1,7 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."app_metadata" alter column "is_android_only" set default 'false';
+-- alter table "public"."app_metadata" alter column "is_android_only" set not null;
+alter table "public"."app_metadata" alter column "is_android_only" drop not null;
+alter table "public"."app_metadata" alter column "is_android_only" drop default;
+
