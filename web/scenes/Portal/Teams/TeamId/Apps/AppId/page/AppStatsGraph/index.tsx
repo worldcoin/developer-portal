67da6953cbc3d382f04b99e0cf8d131d90d040ed
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-04-19 15:23:02 -0700

    format

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/page/AppStatsGraph/index.tsx
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/page/AppStatsGraph/index.tsx
@@ -2,7 +2,6 @@ import { GraphsSection } from "./GraphsSection";
 import { StatCardGlobalRanking } from "./StatCardGlobalRanking";
 import { StatCards } from "./StatCards";
 
-
 export const AppStatsGraph = ({ appId }: { appId: string }) => {
   return (
     <div className="grid gap-y-6">
