7a428932bdc66980b752592baa565b798aeb99af
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-04-21 11:17:28 -0700

    Add clean-up migration

---
+++ hasura/migrations/default/1745259377078_is_android_only_default_false/up.sql
@@ -0,0 +1,5 @@
+update "public"."app_metadata" set "is_android_only" = false;
+ 
+alter table "public"."app_metadata" alter column "is_android_only" set default false;
+ 
+alter table "public"."app_metadata" alter column "is_android_only" set not null;
