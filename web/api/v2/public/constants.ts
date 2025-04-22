e53b11e6358c563cf6efd6e6684b18b593a6c932
Michal Struck <michal.o.struck@gmail.com>
2025-04-22 14:17:05 -0700

    add comment

--- web/api/v2/public/constants.ts
+++ web/api/v2/public/constants.ts
@@ -2,6 +2,7 @@
 export const CONTACTS_APP_AVAILABLE_FROM = "2.8.7803";
 export const STARTER_KIT_APP_AVAILABLE_FROM = "2.8.8100";
 
+// TEMP
 export const OFFICE_IPS = process.env.NEXT_PUBLIC_OFFICE_IPS
   ? process.env.NEXT_PUBLIC_OFFICE_IPS?.split(",")
   : ["158.247.70.32", "158.247.70.38"];
