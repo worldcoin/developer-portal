bcef8e697cf919fba372a02f5ab82ec00cdda96b
Michal Struck <michal.o.struck@gmail.com>
2025-04-22 14:15:17 -0700

    add more office ips, add env var

--- web/api/v2/public/constants.ts
+++ web/api/v2/public/constants.ts
@@ -2,4 +2,6 @@
 export const CONTACTS_APP_AVAILABLE_FROM = "2.8.7803";
 export const STARTER_KIT_APP_AVAILABLE_FROM = "2.8.8100";
 
-export const OFFICE_IP = "158.247.70.32";
+export const OFFICE_IPS = process.env.NEXT_PUBLIC_OFFICE_IPS
+  ? process.env.NEXT_PUBLIC_OFFICE_IPS?.split(",")
+  : ["158.247.70.32", "158.247.70.38"];
