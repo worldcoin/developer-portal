0947344dc7a797b72d659383ed975b64bd0ffa3a
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-04-24 13:50:25 -0700

    remove old log

--- web/api/v2/minikit/transaction/debug/index.ts
+++ web/api/v2/minikit/transaction/debug/index.ts
@@ -143,8 +143,6 @@ export const GET = async (req: NextRequest) => {
   const data = await res.json();
 
   if (!data?.result?.transactions || data.result.transactions.length === 0) {
-    // TODO: Remove this once I see what the response is. Something is wrong with the response from the backend.
-    console.log(JSON.stringify(data, null, 2));
     return corsHandler(
       errorResponse({
         statusCode: 404,
