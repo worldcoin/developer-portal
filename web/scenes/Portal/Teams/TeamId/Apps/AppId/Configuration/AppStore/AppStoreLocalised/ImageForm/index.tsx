117c7c789b970a2aa1386f66978859034748ad0a
Michal Struck <michal.o.struck@gmail.com>
2025-04-24 19:42:16 -0700

    recommendation

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/Configuration/AppStore/AppStoreLocalised/ImageForm/index.tsx
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/Configuration/AppStore/AppStoreLocalised/ImageForm/index.tsx
@@ -686,7 +686,7 @@ export const ImageForm = (props: ImageFormTypes) => {
               </Typography>
             </div>
             <Typography variant={TYPOGRAPHY.R5} className="text-grey-500">
-              {`JPG or PNG (max 500kB), required aspect ratio 2:1. \nRecommended size: ${1080}x${540}px`}
+              {`JPG or PNG (max 500kB), required aspect ratio 2:1. \nRecommended size: ${1200}x${600}px`}
             </Typography>
           </div>
         </ImageDropZone>
