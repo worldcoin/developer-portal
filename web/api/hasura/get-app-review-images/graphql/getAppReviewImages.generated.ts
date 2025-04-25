506326b0c3173325f5aaaa25be69b6cf55bcd01d
Michal Struck <michal.o.struck@gmail.com>
2025-04-25 14:52:52 -0700

    add to review images

--- web/api/hasura/get-app-review-images/graphql/getAppReviewImages.generated.ts
+++ web/api/hasura/get-app-review-images/graphql/getAppReviewImages.generated.ts
@@ -16,11 +16,13 @@ export type GetAppReviewImagesQuery = {
     app_metadata: Array<{
       __typename?: "app_metadata";
       logo_img_url: string;
-      showcase_img_urls?: Array<string> | null;
       hero_image_url: string;
+      meta_tag_image_url: string;
+      showcase_img_urls?: Array<string> | null;
       localisations: Array<{
         __typename?: "localisations";
         hero_image_url: string;
+        meta_tag_image_url: string;
         showcase_img_urls?: Array<string> | null;
       }>;
     }>;
@@ -32,10 +34,12 @@ export const GetAppReviewImagesDocument = gql`
     app(where: { id: { _eq: $app_id } }) {
       app_metadata(where: { verification_status: { _eq: "awaiting_review" } }) {
         logo_img_url
-        showcase_img_urls
         hero_image_url
+        meta_tag_image_url
+        showcase_img_urls
         localisations(where: { locale: { _eq: $locale } }) {
           hero_image_url
+          meta_tag_image_url
           showcase_img_urls
         }
       }
