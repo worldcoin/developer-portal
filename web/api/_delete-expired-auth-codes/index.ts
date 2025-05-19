aafded351a2d52dabdbf0016cb608ee591e9b8f5
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-05-19 14:42:30 +0200

    feat: Migrate Pages Router to App Router - /_delete-expired-auth-codes (#1400)
    * Move OIDC to new directory structure
    
    * Refactor authorize endpoint
    
    * Fix authorize tests
    
    * Add routes back to pages
    
    * Add authorize route
    
    * Add pages tests back
    
    * Fix integration tests
    
    * Fix authorize tests
    
    * Fix integration tests
    
    * And another fix of integration tests
    
    * Add empty JSON body handling
    
    * Reapply "feat: Migrate Pages Router to App Router - /v1/oidc/authorize (#1344)" (#1351)
    
    This reverts commit 1412cb9b211e6ecaf049b34505a1debfe358d3ca.
    
    * Create signal hash
    
    * Add comment about hashing
    
    * Refactor /introspect endpoint
    
    * Migrate /openid-configuration endpoint
    
    * Migrate /token endpoint
    
    * Adjust integration tests
    
    * Fix tests to use JSON
    
    * Revert "Fix tests to use JSON"
    
    This reverts commit 9f18d65fa112e3932cb61d19bd0b8bef59bcc531.
    
    * Fix request body serialization
    
    * Another body fix
    
    * Fix kms mock
    
    * Fix cors handling
    
    * Migrate /userinfo endpoint
    
    * Migrate /validate endpoint
    
    * Fix API tests
    
    * Migrate /precheck endpoint
    
    * Migrate /debugger endpoint
    
    * Remove v1 verify
    
    * Migrate /v1/graphql endpoint
    
    * Remove request from session
    
    * Transforms body before fetch
    
    * Migrate /v1/jwks endpoint
    
    * Refactor CORS
    
    * Migrate /_delete-expired-auth-codes endpoint

---
+++ web/api/_delete-expired-auth-codes/index.ts
@@ -0,0 +1,26 @@
+import { getAPIServiceGraphqlClient } from "@/api/helpers/graphql";
+import { protectInternalEndpoint } from "@/api/helpers/utils";
+import { logger } from "@/lib/logger";
+import { NextRequest, NextResponse } from "next/server";
+import { getSdk as deleteExpiredAuthCodesSdk } from "./graphql/delete-expired-auth-codes.generated";
+
+export async function POST(request: NextRequest) {
+  if (!protectInternalEndpoint(request)) {
+    return;
+  }
+
+  logger.info("Starting deletion of expired auth codes.");
+
+  const client = await getAPIServiceGraphqlClient();
+  const sdk = deleteExpiredAuthCodesSdk(client);
+
+  const response = await sdk.DeleteExpiredAuthCodes({
+    now: new Date().toISOString(),
+  });
+
+  logger.info(
+    `Deleted ${response.delete_auth_code?.affected_rows} expired auth codes.`,
+  );
+
+  return new NextResponse(null, { status: 204 });
+}
