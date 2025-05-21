05e4ed134c6fb9e79f8b542da6de3c97eab5c237
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-05-21 14:20:38 +0300

    Update down migration

--- hasura/migrations/default/1747818197472_alter_table_public_nullifier_add_column_nullifier_hash_int/down.sql
+++ hasura/migrations/default/1747818197472_alter_table_public_nullifier_add_column_nullifier_hash_int/down.sql
@@ -1,4 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."nullifier" add column "nullifier_hash_int" text
---  null;
+alter table "public"."nullifier" drop column "nullifier_hash_int";
