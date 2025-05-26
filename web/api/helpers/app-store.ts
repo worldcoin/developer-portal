6faf9d4689bae3338764fa3eef4fce283eacb637
Michal Struck <michal.o.struck@gmail.com>
2025-05-26 23:04:42 +0200

    move function closer to other related ones

--- web/api/helpers/app-store.ts
+++ web/api/helpers/app-store.ts
@@ -13,7 +13,6 @@ import {
   AppStoreMetadataFields,
 } from "@/lib/types";
 import { getCDNImageUrl, getLogoImgCDNUrl, tryParseJSON } from "@/lib/utils";
-import { getNotificationPermissions } from "./notifications";
 
 export const formatAppMetadata = async (
   appData: AppStoreMetadataFields,
@@ -176,6 +175,48 @@ const isDefaultPinnedNoGrants = (appId: string) => {
     appId === NATIVE_MAPPED_APP_ID.network ||
     appId === NATIVE_MAPPED_APP_ID.invites
   );
+};
+
+// logic responsible for setting this is in api/evaluate-app-notification-permissions
+export const getNotificationPermissions = (
+  appMetadata: Pick<
+    AppStoreMetadataFields,
+    | "is_allowed_unlimited_notifications"
+    | "max_notifications_per_day"
+    | "notification_state"
+    | "notification_state_changed_date"
+  >,
+): {
+  is_allowed_unlimited_notifications: boolean | null | undefined;
+  max_notifications_per_day: number | null | undefined;
+} => {
+  if (appMetadata.notification_state === "normal") {
+    return {
+      is_allowed_unlimited_notifications:
+        appMetadata.is_allowed_unlimited_notifications,
+      max_notifications_per_day: appMetadata.max_notifications_per_day,
+    };
+  }
+  if (appMetadata.notification_state === "paused") {
+    return {
+      is_allowed_unlimited_notifications: false,
+      max_notifications_per_day: 0,
+    };
+  }
+  if (appMetadata.notification_state === "enabled_after_pause") {
+    return {
+      is_allowed_unlimited_notifications:
+        appMetadata.is_allowed_unlimited_notifications,
+      max_notifications_per_day: appMetadata.max_notifications_per_day,
+    };
+  }
+
+  // default
+  return {
+    is_allowed_unlimited_notifications:
+      appMetadata.is_allowed_unlimited_notifications,
+    max_notifications_per_day: appMetadata.max_notifications_per_day,
+  };
 };
 
 // Cached thus this is not that expensive
