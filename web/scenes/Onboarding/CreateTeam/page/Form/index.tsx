b527e1fd5ddcc2f11bdd8010eaab68688e749513
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-05-27 18:16:34 +0200

    Move update team name to server side

--- web/scenes/Onboarding/CreateTeam/page/Form/index.tsx
+++ web/scenes/Onboarding/CreateTeam/page/Form/index.tsx
@@ -6,6 +6,7 @@ import { Checkbox } from "@/components/Checkbox";
 import { DecoratedButton } from "@/components/DecoratedButton";
 import { Input } from "@/components/Input";
 import { TYPOGRAPHY, Typography } from "@/components/Typography";
+import { teamNameSchema } from "@/lib/schema";
 import { urls } from "@/lib/urls";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -23,12 +24,7 @@ export const Form = (props: { hasUser: boolean }) => {
     () =>
       yup.object({
         hasUser: yup.boolean().default(props.hasUser),
-        teamName: yup
-          .string()
-          .required("Please enter a team name")
-          .max(128, "Team name must be 128 characters or less")
-          .matches(/^[^<>]*$/, "Team name cannot contain < or > characters"),
-
+        teamName: teamNameSchema,
         termsAndConditions: yup.boolean().when("hasUser", {
           is: false,
           then: (schema) =>
