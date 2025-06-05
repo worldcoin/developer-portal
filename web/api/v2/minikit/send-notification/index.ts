2b8a7ff17c3d7092e12f4433f40ae0cfa8e3481b
Michał Struck <98230431+michalstruck@users.noreply.github.com>
2025-06-05 13:03:26 +0200

    improve send-notification validation (#1449)
    * improve send-notification validation
    
    * Update web/api/v2/minikit/send-notification/index.ts
    
    Co-authored-by: Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andy Wang <41224501+andy-t-wang@users.noreply.github.com>

--- web/api/v2/minikit/send-notification/index.ts
+++ web/api/v2/minikit/send-notification/index.ts
@@ -17,28 +17,57 @@ import {
   getSdk as createNotificationLogSdk,
 } from "./graphql/create-notification-log.generated";
 import { getSdk as fetchMetadataSdk } from "./graphql/fetch-metadata.generated";
+const USERNAME_SPECIAL_STRING = "${username}";
 
-const sendNotificationBodySchema = yup.object({
-  app_id: yup.string().strict().required(),
-  wallet_addresses: yup
-    .array()
-    .of(yup.string())
-    .min(1)
-    .max(1000)
-    .required("wallet_addresses is required"),
-  message: notificationMessageSchema,
-  title: yup
-    .string()
-    .strict()
-    .optional()
-    .max(30)
-    .test(
-      "valid-title-with-emojis",
-      "Title can only contain letters, numbers, punctuation, emojis, and spaces",
-      allowTitleAndEmojisRegex.test,
-    ),
-  mini_app_path: yup.string().strict().required(),
-});
+const sendNotificationBodySchema = yup
+  .object({
+    app_id: yup.string().strict().required(),
+    wallet_addresses: yup
+      .array()
+      .of(yup.string().length(42))
+      .min(1)
+      .max(1000)
+      .required("wallet_addresses is required"),
+    message: notificationMessageSchema,
+    title: yup
+      .string()
+      .strict()
+      .optional()
+      .max(30)
+      .test(
+        "valid-title-with-emojis",
+        "Title can only contain letters, numbers, punctuation, emojis, and spaces",
+        allowTitleAndEmojisRegex.test,
+      ),
+    mini_app_path: yup
+      .string()
+      .strict()
+      .required()
+      .test(
+        "contains-app-id",
+        "mini_app_path must include the app_id and be a valid WorldApp deeplink",
+        function (value) {
+          const { app_id } = this.parent;
+          return value.startsWith(`worldapp://mini-app?app_id=${app_id}`);
+        },
+      ),
+  })
+  .test(
+    "title-length",
+    "Title with substituted username cannot exceed 16 characters.",
+    (value) => {
+      // title can be 30 chars long max, username can be 14 chars long max
+      if (value?.title?.includes(USERNAME_SPECIAL_STRING)) {
+        const titleWithoutUsername = value.title.replace(
+          USERNAME_SPECIAL_STRING,
+          "",
+        );
+
+        return titleWithoutUsername.length <= 16;
+      }
+      return true;
+    },
+  );
 
 type NotificationResult = {
   walletAddress: string;
