5c45eff94562d5e99bc5832ef30e1d663f5b80c5
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-06-10 13:29:40 +0200

    Improve localisation parsing

--- web/lib/languages.ts
+++ web/lib/languages.ts
@@ -266,8 +266,9 @@ export const formLanguagesList = supportedLanguages;
 
 // Since Android isn't using a standardized list of locales to return we need to normalize the locale
 export const parseLocale = (locale: string) => {
-  const major_locale = locale.split("-")[0];
-  const secondary_locale = locale.split("-")[1];
+  // Split on either - or _
+  const major_locale = locale.split(/[-_]/)[0];
+  const secondary_locale = locale.split(/[-_]/)?.[1];
 
   const language = supportedLanguages.find(
     (lang) => lang.value === major_locale,
