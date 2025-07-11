353940e9754acbf63e2be1456fd374cd6abda024
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-07-11 14:53:23 -0400

    explicit

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/Configuration/BasicInformation/form-schema.ts
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/Configuration/BasicInformation/form-schema.ts
@@ -1,15 +1,17 @@
 import { appNameSchema } from "@/lib/schema";
 import * as yup from "yup";
 
-export const schema = yup.object({
-  name: appNameSchema,
-  integration_url: yup
-    .string()
-    .url("Must be a valid https:// URL")
-    .matches(/^https:\/\/(\w+-)*\w+(\.\w+)+([\/\w\-._/?%&#=]*)?$/, {
-      message: "Link must be a valid HTTPS URL",
-      excludeEmptyString: true,
-    })
-    .required("This field is required"),
-});
+export const schema = yup
+  .object({
+    name: appNameSchema,
+    integration_url: yup
+      .string()
+      .url("Must be a valid https:// URL")
+      .matches(/^https:\/\/(\w+-)*\w+(\.\w+)+([\/\w\-._/?%&#=]*)?$/, {
+        message: "Link must be a valid HTTPS URL",
+        excludeEmptyString: true,
+      })
+      .required("This field is required"),
+  })
+  .noUnknown(true);
 export type BasicInformationFormValues = yup.Asserts<typeof schema>;
