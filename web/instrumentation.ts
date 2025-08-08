24d7bcf36b02da83dcb75b778aae69909b46c86a
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-08-08 18:49:44 +0200

    Revert "feat: Add dd-trace setup (#1566)"
    This reverts commit 7bfe9d3c25be9ae49c377b600cb646b0f2d2ee7f.

--- web/instrumentation.ts
+++ web/instrumentation.ts
@@ -6,43 +6,6 @@ export async function register() {
       typeof window === "undefined" &&
       process.env.NEXT_RUNTIME === "nodejs"
     ) {
-      console.log("Registering dd-trace");
-
-      const ddTrace = await import("dd-trace");
-
-      const tracer = ddTrace.default.init();
-
-      // Monitor HTTP requests
-      tracer.use("http", {
-        // Should block requests to Next internal endpoints
-        blocklist: [/\/_next\//],
-        hooks: {
-          request(span, req, res) {
-            if (span && req) {
-              const urlString = "path" in req ? req.path : req.url;
-
-              if (urlString) {
-                const url = new URL(urlString, "http://localhost");
-                const resourceGroup = url.pathname;
-                const method = req.method;
-
-                span.setTag(
-                  "resource.name",
-                  method ? `${method} ${resourceGroup}` : resourceGroup,
-                );
-              }
-            }
-          },
-        },
-      });
-
-      // Disable the Next.js instrumentation as it creates duplicate spans
-      tracer.use("next", {
-        enabled: false,
-      });
-
-      console.log("dd-trace registered");
-
       const { ParameterStore } = await import("./lib/parameter-store");
       global.ParameterStore = new ParameterStore("developer-portal");
 
