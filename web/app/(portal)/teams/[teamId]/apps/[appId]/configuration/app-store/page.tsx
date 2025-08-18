ce34e68701ccf4dbfa506c7aa0bd531df963b554
Michał Struck <98230431+michalstruck@users.noreply.github.com>
2025-08-18 14:58:03 +0200

    remove /app-store path

--- web/app/(portal)/teams/[teamId]/apps/[appId]/configuration/app-store/page.tsx
+++
@@ -1,9 +0,0 @@
-import { generateMetaTitle } from "@/lib/genarate-title";
-import { AppProfileGalleryPage } from "@/scenes/Portal/Teams/TeamId/Apps/AppId/Configuration/AppStore/page";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: generateMetaTitle({ left: "Mini Apps" }),
-};
-
-export default AppProfileGalleryPage;
