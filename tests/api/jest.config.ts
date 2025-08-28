48a63504cf33799e636157b976a828140f679dae
Dmitrii Bormotov <dmitrii.bormotov@toolsforhumanity.com>
2025-08-28 18:03:56 +0200

    Add jest-expect-message

--- tests/api/jest.config.ts
+++ tests/api/jest.config.ts
@@ -13,9 +13,11 @@ const config: Config = {
     "^.+\\.(js|ts)$": "babel-jest",
   },
   transformIgnorePatterns: ["node_modules/auth0/(?!(nextjs-auth0)/)"],
+  setupFilesAfterEnv: ["jest-expect-message"],
   testMatch: ["<rootDir>/specs/**/*.spec.ts"],
   testTimeout: 30000,
-  moduleDirectories: ["node_modules", "<rootDir>/../../web"],
+  moduleDirectories: ["node_modules", "<rootDir>", "<rootDir>/../../web"],
+  modulePaths: ["./"],
   verbose: true,
 };
 
