cecac03a02a86a25a6a36ebe370651af468d803d
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-09-01 19:35:16 +0200

    feat: Introduce team soft-deletion (#1607)
    * Introduce team soft-delete
    
    * Add missing permissions checks for actions and nullifier
    
    * Fix action permissions
    
    * Fix action permissions
    
    * Fix empty action

--- web/graphql/graphql.ts
+++ web/graphql/graphql.ts
@@ -9657,6 +9657,7 @@ export type Team = {
   /** An aggregate relationship */
   apps_aggregate: App_Aggregate;
   created_at: Scalars["timestamptz"]["output"];
+  deleted_at?: Maybe<Scalars["timestamptz"]["output"]>;
   id: Scalars["String"]["output"];
   /** An array relationship */
   memberships: Array<Membership>;
@@ -9768,6 +9769,7 @@ export type Team_Bool_Exp = {
   apps?: InputMaybe<App_Bool_Exp>;
   apps_aggregate?: InputMaybe<App_Aggregate_Bool_Exp>;
   created_at?: InputMaybe<Timestamptz_Comparison_Exp>;
+  deleted_at?: InputMaybe<Timestamptz_Comparison_Exp>;
   id?: InputMaybe<String_Comparison_Exp>;
   memberships?: InputMaybe<Membership_Bool_Exp>;
   memberships_aggregate?: InputMaybe<Membership_Aggregate_Bool_Exp>;
@@ -9787,6 +9789,7 @@ export type Team_Insert_Input = {
   api_keys?: InputMaybe<Api_Key_Arr_Rel_Insert_Input>;
   apps?: InputMaybe<App_Arr_Rel_Insert_Input>;
   created_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
+  deleted_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
   id?: InputMaybe<Scalars["String"]["input"]>;
   memberships?: InputMaybe<Membership_Arr_Rel_Insert_Input>;
   name?: InputMaybe<Scalars["String"]["input"]>;
@@ -9797,6 +9800,7 @@ export type Team_Insert_Input = {
 export type Team_Max_Fields = {
   __typename?: "team_max_fields";
   created_at?: Maybe<Scalars["timestamptz"]["output"]>;
+  deleted_at?: Maybe<Scalars["timestamptz"]["output"]>;
   id?: Maybe<Scalars["String"]["output"]>;
   name?: Maybe<Scalars["String"]["output"]>;
   /** A computed field that returns a quantity of team owners */
@@ -9808,6 +9812,7 @@ export type Team_Max_Fields = {
 export type Team_Min_Fields = {
   __typename?: "team_min_fields";
   created_at?: Maybe<Scalars["timestamptz"]["output"]>;
+  deleted_at?: Maybe<Scalars["timestamptz"]["output"]>;
   id?: Maybe<Scalars["String"]["output"]>;
   name?: Maybe<Scalars["String"]["output"]>;
   /** A computed field that returns a quantity of team owners */
@@ -9843,6 +9848,7 @@ export type Team_Order_By = {
   api_keys_aggregate?: InputMaybe<Api_Key_Aggregate_Order_By>;
   apps_aggregate?: InputMaybe<App_Aggregate_Order_By>;
   created_at?: InputMaybe<Order_By>;
+  deleted_at?: InputMaybe<Order_By>;
   id?: InputMaybe<Order_By>;
   memberships_aggregate?: InputMaybe<Membership_Aggregate_Order_By>;
   name?: InputMaybe<Order_By>;
@@ -9860,6 +9866,8 @@ export enum Team_Select_Column {
   /** column name */
   CreatedAt = "created_at",
   /** column name */
+  DeletedAt = "deleted_at",
+  /** column name */
   Id = "id",
   /** column name */
   Name = "name",
@@ -9870,6 +9878,7 @@ export enum Team_Select_Column {
 /** input type for updating data in table "team" */
 export type Team_Set_Input = {
   created_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
+  deleted_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
   id?: InputMaybe<Scalars["String"]["input"]>;
   name?: InputMaybe<Scalars["String"]["input"]>;
   updated_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
@@ -9907,6 +9916,7 @@ export type Team_Stream_Cursor_Input = {
 /** Initial value of the column from where the streaming should start */
 export type Team_Stream_Cursor_Value_Input = {
   created_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
+  deleted_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
   id?: InputMaybe<Scalars["String"]["input"]>;
   name?: InputMaybe<Scalars["String"]["input"]>;
   updated_at?: InputMaybe<Scalars["timestamptz"]["input"]>;
@@ -9923,6 +9933,8 @@ export type Team_Sum_Fields = {
 export enum Team_Update_Column {
   /** column name */
   CreatedAt = "created_at",
+  /** column name */
+  DeletedAt = "deleted_at",
   /** column name */
   Id = "id",
   /** column name */
