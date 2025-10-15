e6c514a742713adce880b0f3e5529576b5bbb873
Eugene <evgeniy.sabynin@toolsforhumanity.com>
2025-10-15 18:40:53 +0500

    created routes for other pages

---
+++ web/scenes/Portal/Teams/TeamId/Team/AffiliateProgram/HowItWorks/page/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+import {SizingWrapper} from "@/components/SizingWrapper";
+
+type PageProps = {
+  params: {
+    teamId: string;
+  };
+};
+
+export const HowItWorksPage = (props: PageProps) => {
+  const { params } = props;
+  const teamId = params?.teamId;
+
+  return (
+    <>
+      <SizingWrapper
+        gridClassName="order-2 grow"
+        className="flex flex-col">
+        How it works page
+      </SizingWrapper>
+    </>
+  );
+};
