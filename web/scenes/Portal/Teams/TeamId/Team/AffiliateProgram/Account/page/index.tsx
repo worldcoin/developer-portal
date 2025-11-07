864ec67eaa245ecbc14b2939d0bb7f21828ce581
Eugene <evgeniy.sabynin@toolsforhumanity.com>
2025-11-07 17:55:50 +0500

    removed skeleton for account

--- web/scenes/Portal/Teams/TeamId/Team/AffiliateProgram/Account/page/index.tsx
+++ web/scenes/Portal/Teams/TeamId/Team/AffiliateProgram/Account/page/index.tsx
@@ -5,12 +5,9 @@ import { QuickAction } from "@/components/QuickAction";
 import { useAtom } from "jotai/index";
 import { WalletDialog, walletDialogAtom } from "./WalletDialog";
 import { EmailDialog, emailDialogAtom } from "./EmailDialog";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { Auth0SessionUser } from "@/lib/types";
 import { GmailIcon } from "@/components/Icons/GmailIcon";
 import { WalletIcon } from "@/components/Icons/WalletIcon";
 import { formatWalletAddress } from "@/lib/utils";
-import Skeleton from "react-loading-skeleton";
 import { useGetAffiliateMetadata } from "@/scenes/Portal/Teams/TeamId/Team/AffiliateProgram/Overview/page/hooks/use-get-affiliate-metadata";
 
 type PageProps = {
@@ -20,58 +17,45 @@ type PageProps = {
 };
 
 export const AffiliateAccountPage = (props: PageProps) => {
-  const { user, isLoading: isEmailLoading } = useUser() as Auth0SessionUser;
-  const { data: metadata, loading: isWalletLoading } =
-    useGetAffiliateMetadata();
+  const { data: metadata } = useGetAffiliateMetadata();
 
   const [, setIsWalletDialogOpened] = useAtom(walletDialogAtom);
   const [, setIsEmailDialogOpened] = useAtom(emailDialogAtom);
 
   return (
-    <>
-      <SizingWrapper
-        gridClassName="order-2 grow mt-6 md:mt-10"
-        className="flex flex-col"
-      >
-        <EmailDialog email={user?.email || ""} />
-        <WalletDialog walletAddress={metadata?.withdrawalWallet || ""} />
-        <div className="grid gap-y-8">
-          <Typography variant={TYPOGRAPHY.H6}>Account</Typography>
+    <SizingWrapper
+      gridClassName="order-2 grow mt-6 md:mt-10"
+      className="flex flex-col"
+    >
+      <EmailDialog email={metadata?.email || ""} />
+      <WalletDialog walletAddress={metadata?.withdrawalWallet || ""} />
+      <div className="grid gap-y-8">
+        <Typography variant={TYPOGRAPHY.H6}>Account</Typography>
 
-          <div className="grid gap-x-6 gap-y-4 md:grid-cols-2">
-            {isEmailLoading || isWalletLoading ? (
-              <>
-                <Skeleton height={98} className="rounded-2xl" />
-                <Skeleton height={98} className="rounded-2xl" />
-              </>
-            ) : (
-              <>
-                {metadata?.email && (
-                  <QuickAction
-                    type="button"
-                    icon={<GmailIcon className="size-5.5" />}
-                    title="Email address"
-                    description={metadata?.email || ""}
-                    onClick={() => setIsEmailDialogOpened(true)}
-                  />
-                )}
+        <div className="grid gap-x-6 gap-y-4 md:grid-cols-2">
+          {metadata?.email && (
+            <QuickAction
+              type="button"
+              icon={<GmailIcon className="size-5.5" />}
+              title="Email address"
+              description={metadata?.email || ""}
+              onClick={() => setIsEmailDialogOpened(true)}
+            />
+          )}
 
-                {metadata?.withdrawalWallet && (
-                  <QuickAction
-                    type="button"
-                    icon={<WalletIcon className="size-5.5" />}
-                    title="Wallet address"
-                    description={formatWalletAddress(
-                      metadata?.withdrawalWallet || "",
-                    )}
-                    onClick={() => setIsWalletDialogOpened(true)}
-                  />
-                )}
-              </>
-            )}
-          </div>
+          {metadata?.withdrawalWallet && (
+            <QuickAction
+              type="button"
+              icon={<WalletIcon className="size-5.5" />}
+              title="Wallet address"
+              description={formatWalletAddress(
+                metadata?.withdrawalWallet || "",
+              )}
+              onClick={() => setIsWalletDialogOpened(true)}
+            />
+          )}
         </div>
-      </SizingWrapper>
-    </>
+      </div>
+    </SizingWrapper>
   );
 };
