d32d445216ed495deddcd2fc41ee50d9a8df8b03
Ahmed Zahran <ahmed@toolsforhumanity.com>
2026-01-27 20:49:46 -0800

    using shared layout for Enable World ID 4.0 and Configure Signer Key flows

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/layout/AppIdChrome.tsx
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/layout/AppIdChrome.tsx
@@ -119,7 +119,10 @@ export const AppIdChrome = ({
       {children}
 
       <BottomBar>
-        <BottomBar.Link href={`/teams/${teamId}/apps/${appId}`} segment={null}>
+        <BottomBar.Link
+          href={`/teams/${teamId}/apps/${appId}`}
+          segment={null}
+        >
           <DashboardSquareIcon className="size-7" />
         </BottomBar.Link>
 
