f839ce65ae79e42f5663c6e451d7400232c38da0
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2026-02-27 19:42:36 +0100

    fix: Add team checks for transactions tab (#1806)
    * Add team checks for transactions tab
    
    * Fix formatting
    
    * Fix capital letter

--- web/scenes/Portal/Teams/TeamId/Apps/AppId/layout/index.tsx
+++ web/scenes/Portal/Teams/TeamId/Apps/AppId/layout/index.tsx
@@ -1,7 +1,8 @@
 import { getAPIServiceGraphqlClient } from "@/api/helpers/graphql";
 import { ErrorPage } from "@/components/ErrorPage";
 import { isWorldId40EnabledServer } from "@/lib/feature-flags/world-id-4-0/server";
-import { EngineType } from "@/lib/types";
+import { Auth0SessionUser, EngineType } from "@/lib/types";
+import { getSession } from "@auth0/nextjs-auth0";
 import { ReactNode } from "react";
 import { AppIdChrome } from "./AppIdChrome";
 import { getSdk as getAppEnv } from "./graphql/server/fetch-app-env.generated";
@@ -18,6 +19,17 @@ type AppIdLayoutProps = {
 
 export const AppIdLayout = async (props: AppIdLayoutProps) => {
   const params = props.params;
+
+  const session = await getSession();
+  const user = session?.user as Auth0SessionUser["user"];
+  const isTeamMember = user?.hasura?.memberships?.some(
+    (membership) => membership.team?.id === params.teamId,
+  );
+
+  if (!isTeamMember) {
+    return <ErrorPage statusCode={404} title="App not found" />;
+  }
+
   const client = await getAPIServiceGraphqlClient();
   const { app, action } = await getAppEnv(client).FetchAppEnv({
     id: params.appId ?? "",
