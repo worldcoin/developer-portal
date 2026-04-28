593d89fa774f1cc778fdcbbd95a4b0b2539c0b83
Eugene <evgeniy.sabynin@toolsforhumanity.com>
2026-04-28 21:11:32 +0800

    allow kyc request

--- web/scenes/Portal/Teams/TeamId/Team/AffiliateProgram/Overview/server/getIdentityVerificationLink.ts
+++ web/scenes/Portal/Teams/TeamId/Team/AffiliateProgram/Overview/server/getIdentityVerificationLink.ts
@@ -11,11 +11,11 @@ import { createSignedFetcher } from "aws-sigv4-fetch";
 import { validateAffiliateRequest } from "../../common/server/validate-affiliate-request";
 
 export const getIdentityVerificationLink = async ({
+  type,
   redirectUri,
-}: Pick<
-  GetIdentityVerificationLinkRequest,
-  "redirectUri"
->): Promise<FormActionResult> => {
+}: Pick<GetIdentityVerificationLinkRequest, "type" | "redirectUri">): Promise<
+  FormActionResult
+> => {
   let teamId: string | undefined;
 
   try {
@@ -36,7 +36,7 @@ export const getIdentityVerificationLink = async ({
     }
     const url = `${process.env.NEXT_SERVER_APP_BACKEND_BASE_URL}/internal/v1/affiliate/identity-verification/verification-link`;
     // NOTE: set kyb because app backend doesn't if it's kyc or kyb
-    const requestBody = { type: "kyb", redirectUri };
+    const requestBody = { type, redirectUri };
 
     const response = await signedFetch(url, {
       method: "POST",
