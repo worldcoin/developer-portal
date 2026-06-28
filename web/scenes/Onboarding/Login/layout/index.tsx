0059fac0d0b79f766d6d804efb3ad2391d36d1ed
Kartike Chawla <kchawla2005@ucla.edu>
2026-06-28 15:11:48 -0700

    change log in to sign in

--- web/scenes/Onboarding/Login/layout/index.tsx
+++ web/scenes/Onboarding/Login/layout/index.tsx
@@ -26,7 +26,7 @@ export const LoginLayout = (props: { children: ReactNode }) => {
               className="group h-12 rounded-full border-black bg-transparent px-5 py-0 text-base text-black hover:border-black hover:bg-transparent hover:text-black"
               testId="log-in"
             >
-              Log in
+              Sign in
               <span className="-ml-2 inline-flex w-0 items-center justify-center overflow-hidden transition-[width,margin-left] duration-300 ease-out group-hover:ml-0 group-hover:w-6 motion-reduce:transition-none">
                 <span className="inline-flex scale-0 opacity-0 transition-[transform,opacity] duration-300 ease-out group-hover:scale-100 group-hover:opacity-100 motion-reduce:scale-100 motion-reduce:opacity-100 motion-reduce:transition-none">
                   <WorldIcon className="size-6 shrink-0 group-hover:animate-[spin_2.75s_linear_infinite] motion-reduce:animate-none [&_path]:fill-black" />
