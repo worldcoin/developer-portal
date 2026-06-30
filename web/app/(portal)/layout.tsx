69b46f0835a3b9a3264046d058e6c8d5b32177a1
Soam Desai <soam.desai@toolsforhumanity.com>
2026-06-30 13:04:15 -0700

    style(portal-v3): collapse blank line in (portal) layout (prettier)

--- web/app/(portal)/layout.tsx
+++ web/app/(portal)/layout.tsx
@@ -3,7 +3,6 @@ import { PortalLayout as PortalLayoutV2 } from "@/scenes/Portal/layout";
 import { PortalLayout as PortalLayoutV3 } from "@/scenes/PortalV3/layout";
 import { ReactNode } from "react";
 
-
 export default async function PortalRootLayout(props: { children: ReactNode }) {
   return pickPortalVersion(
     () => <PortalLayoutV3>{props.children}</PortalLayoutV3>,
