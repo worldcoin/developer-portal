3eb3099f317bbded02466405058c34d2dd384fab
Soam Desai <soam.desai@toolsforhumanity.com>
2026-07-01 12:46:23 -0700

    feat(portal-v3): flip /world-id-actions/wia-actionid to pickPortalVersion

--- web/app/(portal)/teams/[teamId]/apps/[appId]/world-id-actions/[actionId]/page.tsx
+++ web/app/(portal)/teams/[teamId]/apps/[appId]/world-id-actions/[actionId]/page.tsx
@@ -1,3 +1,12 @@
+import { pickPortalVersion } from "@/lib/feature-flags/portal-v3/activation";
 import { WorldIdActionIdPage } from "@/scenes/Portal/Teams/TeamId/Apps/AppId/WorldIdActions/ActionId/page";
+import { WorldIdActionIdPage as WorldIdActionIdPageV3 } from "@/scenes/PortalV3/Teams/TeamId/Apps/AppId/WorldIdActions/ActionId/page";
 
-export default WorldIdActionIdPage;
+export default async function Page(props: {
+  params: Promise<Record<string, string>>;
+}) {
+  return pickPortalVersion(
+    () => <WorldIdActionIdPageV3 params={props.params} />,
+    () => <WorldIdActionIdPage params={props.params} />,
+  );
+}
